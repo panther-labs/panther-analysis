97d1a833f3191baffc67eccff45d9e45004cacef
Jack Naglieri <jack@runpanther.io>
2020-02-07 17:22:37 -0800

    Add references, fix a bug in RDS Instance HA policy

--- analysis/policies/managed/aws_rds_instance_has_high_availability.py
+++ analysis/policies/managed/aws_rds_instance_has_high_availability.py
@@ -1,3 +1,3 @@
 def policy(resource):
     # Explict check for True to avoid returning NoneType
-    return resource['MultiAZ'] is True
+    return resource['MultiAZ'] is True and resource['StorageType'] != 'aurora'
