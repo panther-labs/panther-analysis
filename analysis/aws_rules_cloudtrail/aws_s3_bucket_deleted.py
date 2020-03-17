904db9ee4eeca9f610f3e119f671e45c2346e36f
Jack Naglieri <jack@runpanther.io>
2020-03-16 17:36:53 -0700

    make fmt

--- analysis/aws_rules_cloudtrail/aws_s3_bucket_deleted.py
+++ analysis/aws_rules_cloudtrail/aws_s3_bucket_deleted.py
@@ -2,5 +2,6 @@ def rule(event):
     # Capture DeleteBucket, DeleteBucketPolicy, DeleteBucketWebsite
     return event.get('eventName').startswith('DeleteBucket')
 
+
 def dedup(event):
     return event.get('userIdentity', {}).get('arn')
--- analysis/aws_rules_cloudtrail/aws_update_credentials.py
+++ analysis/aws_rules_cloudtrail/aws_update_credentials.py
@@ -2,8 +2,10 @@ UPDATE_EVENTS = {
     'ChangePassword', 'CreateAccessKey', 'CreateLoginProfile', 'CreateUser'
 }
 
+
 def rule(event):
     return event.get('eventName') in UPDATE_EVENTS
+
 
 def dedup(event):
     return event.get('userIdentity', {}).get('userName')
