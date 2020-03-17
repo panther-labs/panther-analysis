7e73faf41703fa75bf660bd1066ae9eedcb721bd
Jack Naglieri <jack@runpanther.io>
2020-03-16 17:25:02 -0700

    [aws_rules_cloudtrail] new update cred rule

--- analysis/aws_rules_cloudtrail/aws_s3_bucket_deleted.py
+++ analysis/aws_rules_cloudtrail/aws_s3_bucket_deleted.py
@@ -1,3 +1,6 @@
 def rule(event):
     # Capture DeleteBucket, DeleteBucketPolicy, DeleteBucketWebsite
     return event.get('eventName').startswith('DeleteBucket')
+
+def dedup(event):
+    return event.get('userIdentity', {}).get('arn')
