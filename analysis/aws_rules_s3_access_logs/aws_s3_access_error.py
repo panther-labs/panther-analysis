0ac8d0f4a528d2c1ca16f3eb78ceff24962d602b
Jack Naglieri <jack@runpanther.io>
2020-03-18 16:50:43 -0700

    [aws_rules_s3] add title, dedup, and small improvements

--- analysis/aws_rules_s3_access_logs/aws_s3_access_error.py
+++ analysis/aws_rules_s3_access_logs/aws_s3_access_error.py
@@ -1,2 +1,10 @@
 def rule(event):
     return 'errorCode' in event
+
+
+def dedup(event):
+    return event.get('bucket')
+
+
+def title(event):
+    return 'Error When Accessing S3 Bucket {}'.format(event.get('bucket'))
--- analysis/aws_rules_s3_access_logs/aws_s3_insecure_access.py
+++ analysis/aws_rules_s3_access_logs/aws_s3_insecure_access.py
@@ -1,2 +1,16 @@
+SECURE_BUCKETS = {
+    # example-bucket-name,
+}
+
+
 def rule(event):
-    return 'ciphersuite' not in event or 'tlsVersion' not in event
+    return (event['bucket'] in SECURE_BUCKETS and 'ciphersuite' not in event or
+            'tlsVersion' not in event)
+
+
+def dedup(event):
+    return event.get('bucket')
+
+
+def title(event):
+    return 'Insecure AWS S3 Access on {}'.format(event.get('bucket'))
