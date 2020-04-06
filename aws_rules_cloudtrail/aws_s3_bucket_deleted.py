0712f9d0608f5925f3166e33ebcd28368d1f6d22
Jack Naglieri <jack@runpanther.io>
2020-04-05 20:41:10 -0700

    [aws_rules] deduping, helper func, titles

--- aws_rules_cloudtrail/aws_s3_bucket_deleted.py
+++ aws_rules_cloudtrail/aws_s3_bucket_deleted.py
@@ -4,13 +4,18 @@ def rule(event):
         'DeleteBucket') and not event.get('errorCode')
 
 
+def helper_strip_role_session_id(user_identity_arn):
+    # The Arn structure is arn:aws:sts::123456789012:assumed-role/RoleName/<sessionId>
+    arn_parts = user_identity_arn.split('/')
+    if arn_parts:
+        return '/'.join(arn_parts[:2])
+    return user_identity_arn
+
+
 def dedup(event):
     user_identity = event.get('userIdentity', {})
     if user_identity.get('type') == 'AssumedRole':
-        # arn:aws:sts::123456789012:assumed-role/RoleName/<sessionId>
-        arn_parts = user_identity.get('arn', '').split('/')
-        if arn_parts:
-            return '/'.join(arn_parts[:2])
+        return helper_strip_role_session_id(user_identity.get('arn', ''))
     return user_identity.get('arn')
 
 
