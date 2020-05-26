30b21c270504bf7c84f99207c9c6c2f6110843ae
Ben <bpruce@gmail.com>
2020-05-26 12:18:58 -0400

    GH exposed key rule (#59)

---
+++ aws_cloudtrail_rules/aws_key_compromised.py
@@ -0,0 +1,14 @@
+def rule(event):
+    # Capture PutUserPolicy
+    return event.get(
+        'eventName') == 'PutUserPolicy' and event['requestParameters'].get(
+            'policyName') == 'AWSExposedCredentialPolicy_DO_NOT_REMOVE'
+
+
+def dedup(event):
+    return event['userIdentity'].get('userName')
+
+
+def title(event):
+    return '{} {} key was uploaded to public github repo'.format(
+        event['userIdentity'].get('accessKeyId'), dedup(event))
