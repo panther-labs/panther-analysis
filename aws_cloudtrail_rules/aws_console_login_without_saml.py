ffd4f8b7a76569d56aacf630288cf8e7a01dd4ce
Jack Naglieri <jack@runpanther.io>
2020-06-12 11:23:08 -0700

    [cloudtrail] logins without saml

---
+++ aws_cloudtrail_rules/aws_console_login_without_saml.py
@@ -0,0 +1,17 @@
+from panther import lookup_aws_account_name  # pylint: disable=import-error
+
+
+def rule(event):
+    additional_event_data = event.get('additionalEventData', {})
+    return (event['eventName'] == 'ConsoleLogin' and
+            event['userIdentity'].get('type') != 'AssumedRole' and
+            not additional_event_data.get('SamlProviderArn'))
+
+
+def dedup(event):
+    return event['recipientAccountId']
+
+
+def title(event):
+    return 'AWS logins without SAML in account [{}]'.format(
+        lookup_aws_account_name(event['recipientAccountId']))
