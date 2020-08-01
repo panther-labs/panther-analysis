a4210e3aaf96827ad15fadd794b96bde34768c7d
Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
2020-07-31 20:19:52 -0700

    fixed  formatting

--- gsuite_reports_rules/gsuite_doc_ownership_transfer.py
+++ gsuite_reports_rules/gsuite_doc_ownership_transfer.py
@@ -4,6 +4,7 @@ ORG_DOMAINS = {
     '@example.com',
 }
 
+
 def rule(event):
     if event['id'].get('applicationName') != 'admin':
         return False
@@ -12,6 +13,7 @@ def rule(event):
         if (details.get('type') == 'DOCS_SETTINGS' and
                 details.get('name') == 'TRANSFER_DOCUMENT_OWNERSHIP'):
             new_owner = param_lookup(details.get('parameters', {}), 'NEW_VALUE')
-            return bool(new_owner) and not any([new_owner.endswith(x) for x in ORG_DOMAINS])
+            return bool(new_owner) and not any(
+                [new_owner.endswith(x) for x in ORG_DOMAINS])
 
     return False
