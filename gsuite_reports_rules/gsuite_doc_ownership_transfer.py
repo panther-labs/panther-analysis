96a4ad50b6da2ec28d7f5d58c31112ae3997cf91
Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
2020-07-31 20:19:02 -0700

    final rules for now

---
+++ gsuite_reports_rules/gsuite_doc_ownership_transfer.py
@@ -0,0 +1,17 @@
+from panther_base_helpers import gsuite_parameter_lookup as param_lookup
+
+ORG_DOMAINS = {
+    '@example.com',
+}
+
+def rule(event):
+    if event['id'].get('applicationName') != 'admin':
+        return False
+
+    for details in event.get('events', [{}]):
+        if (details.get('type') == 'DOCS_SETTINGS' and
+                details.get('name') == 'TRANSFER_DOCUMENT_OWNERSHIP'):
+            new_owner = param_lookup(details.get('parameters', {}), 'NEW_VALUE')
+            return bool(new_owner) and not any([new_owner.endswith(x) for x in ORG_DOMAINS])
+
+    return False
