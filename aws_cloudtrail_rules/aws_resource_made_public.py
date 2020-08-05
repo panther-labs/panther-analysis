031fc45d7eb387f439d64e3c53f15e4c33675101
Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
2020-08-05 12:05:09 -0700

    some minor fixes for issues I noticed while deploying

--- aws_cloudtrail_rules/aws_resource_made_public.py
+++ aws_cloudtrail_rules/aws_resource_made_public.py
@@ -1,29 +1,6 @@
 import json
 from policyuniverse.policy import Policy
 
-PARAMETER_LOOKUP_MAPPING = {
-    'PutBucketPolicy':
-        lambda x: x.get('bucketPolicy'),
-    'SetVaultAccessPolicy':
-        lambda x: json.loads(x.get('policy', {}).get('policy', '{}')),
-    'SetQueueAttributes':
-        lambda x: json.loads(x.get('attributes', {}).get('Policy', '{}')),
-    'CreateTopic':
-        lambda x: json.loads(x.get('attributes', {}).get('Policy', '{}')),
-    'SetRepositoryPolicy':
-        lambda x: json.loads(x.get('policyText', {})),
-    'CreateElasticsearchDomain':
-        lambda x: json.loads(x.get('accessPolicies', {})),
-    'UpdateElasticsearchDomainConfig':
-        lambda x: json.loads(x.get('accessPolicies', {})),
-    'CreateKey':
-        lambda x: json.loads(x.get('policy', {})),
-    'PutKeyPolicy':
-        lambda x: json.loads(x.get('policy', {})),
-    'PutResourcePolicy':
-        lambda x: json.loads(x.get('resourcePolicy', {})),
-}
-
 
 # This is a helper function so that the logic determining whether this is an
 # acceptable policy can be updated easily.
@@ -34,19 +11,54 @@ def policy_is_not_acceptable(json_policy):
     return Policy(json_policy).is_internet_accessible()
 
 
+# Normally this check helps avoid overly complex functions that are doing too many things,
+# but in this case we explicitly want to handle 10 different cases in 10 different ways. Any
+# solution that avoids too many return statements only increases the complexity of this rule.
+# pylint: disable=too-many-return-statements
 def rule(event):
     parameters = event.get('requestParameters', {})
-    parameter_lookup = PARAMETER_LOOKUP_MAPPING.get(event['eventName'])
 
-    if parameter_lookup:
-        policy = parameter_lookup(parameters)
-        return policy_is_not_acceptable(policy)
+    # S3
+    if event['eventName'] == 'PutBucketPolicy':
+        return policy_is_not_acceptable(parameters.get('bucketPolicy', None))
 
-    # SNS special case
+    # ECR
+    if event['eventName'] == 'SetRepositoryPolicy':
+        policy = parameters.get('policyText', '{}')
+        return policy_is_not_acceptable(json.loads(policy))
+
+    # Elasticsearch
+    if event['eventName'] in [
+            'CreateElasticsearchDomain', 'UpdateElasticsearchDomainConfig'
+    ]:
+        policy = parameters.get('accessPolicies', '{}')
+        return policy_is_not_acceptable(json.loads(policy))
+
+    # KMS
+    if event['eventName'] in ['CreateKey', 'PutKeyPolicy']:
+        policy = parameters.get('policy', '{}')
+        return policy_is_not_acceptable(json.loads(policy))
+
+    # S3 Glacier
+    if event['eventName'] == 'SetVaultAccessPolicy':
+        policy = parameters.get('policy', {}).get('policy', '{}')
+        return policy_is_not_acceptable(json.loads(policy))
+
+    # SNS & SQS
+    if event['eventName'] in ['SetQueueAttributes', 'CreateTopic']:
+        policy = parameters.get('attributes', {}).get('Policy', '{}')
+        return policy_is_not_acceptable(json.loads(policy))
+
+    # SNS
     if event['eventName'] == 'SetTopicAttributes':
         if parameters.get('attributeName') == 'Policy':
             policy = parameters.get('attributeValue', '{}')
             return policy_is_not_acceptable(json.loads(policy))
         return False
+
+    # SecretsManager
+    if event['eventName'] == 'PutResourcePolicy':
+        policy = parameters.get('resourcePolicy', '{}')
+        return policy_is_not_acceptable(json.loads(policy))
 
     return False
