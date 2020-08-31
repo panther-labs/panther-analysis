60cf4ad4f1ff335db553af3b8448ccc8da6f8344
Austin Byers <austin.byers@runpanther.io>
2020-08-31 15:34:56 -0700

    Remove broken runbook links, ignore Panther stacks in stack drift policy (#98)

--- aws_cloudformation_policies/aws_cloudformation_stack_drifted.py
+++ aws_cloudformation_policies/aws_cloudformation_stack_drifted.py
@@ -1,2 +1,19 @@
+# CloudFormation stacks tagged with "STACK=(name)" will be marked as passing.
+IGNORE_STACK_TAGS = {
+    'panther-bootstrap-gateway', 'panther-cloud-security', 'panther-core',
+    'panther-log-analysis'
+}
+
+
 def policy(resource):
-    return resource['DriftInformation']['StackDriftStatus'] != "DRIFTED"
+    if resource['DriftInformation']['StackDriftStatus'] != "DRIFTED":
+        return True
+
+    # Some of Panther's own stacks contain Lambda functions which will always show as "drifted."
+    # Panther stacks have a fixed "Stack" tag, even though the real stack name is dynamic.
+    tags = resource['Tags']
+    if tags.get('Application') == 'Panther' and tags.get(
+            'Stack') in IGNORE_STACK_TAGS:
+        return True
+
+    return False
