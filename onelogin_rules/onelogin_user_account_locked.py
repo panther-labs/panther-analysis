ee6aa3ed6da55cc383bc8f4a3ff0becb24e6d8f8
lindsey-w <lindsey.whitehurst@runpanther.io>
2020-09-11 09:25:00 -0700

    merging similar rules

--- onelogin_rules/onelogin_user_account_locked.py
+++ onelogin_rules/onelogin_user_account_locked.py
@@ -2,8 +2,9 @@ def rule(event):
 
     # check for a user locked event
     # event 531 and 553 are user lock events via api
-    return event.get('event_type_id') in [531, 553]
+    # event 551 is user suspended via api
+    return event.get('event_type_id') in [531, 553, 551]
 
 
 def title(event):
-    return 'A user [{}] was locked by the api'.format(event.get('user_name'))
+    return 'A user [{}] was locked or suspended via api call'.format(event.get('user_name'))
