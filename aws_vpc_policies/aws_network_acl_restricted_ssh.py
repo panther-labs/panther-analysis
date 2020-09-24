8bc0c12e3f90cf7e6445ebbf84e7e178bf99c61c
Nick Angelou <nick.angelou@runpanther.io>
2020-09-24 16:50:18 -0500

    fix: rule checks for allowed inbound only (#116)
    * fix: rule checks for allowed inbound only
    
    * chore: lint

--- aws_vpc_policies/aws_network_acl_restricted_ssh.py
+++ aws_vpc_policies/aws_network_acl_restricted_ssh.py
@@ -3,7 +3,8 @@ def policy(resource):
         # Look for ingress rules from any IP.
         # This could be modified in the future to inspect the size
         # of the source network with the ipaddress.ip_network.num_addresses call.
-        if not entry['Egress'] and entry['CidrBlock'] == '0.0.0.0/0':
+        if not entry['Egress'] and entry['CidrBlock'] == '0.0.0.0/0' and entry[
+                'RuleAction'] == 'allow':
             # Check within a range of ports, normally the From/To would be set to 22,
             # but this covers the case where it could be 0-1024.
             if ('PortRange' not in entry or not entry['PortRange'] or
