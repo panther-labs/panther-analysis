5f48b494dd1255ce3b3a6a91244214b392427bc3
Lindsey Whitehurst <43453975+lindsey-w@users.noreply.github.com>
2020-11-04 16:15:30 -0800

    box helpers - working with the sdk (#137)
    * additional box rules and boxsdk helpers
    
    * fixing field types in tests
    
    * fmt + lint fixes
    
    * disable logger
    
    * couple of edits looking at PR
    
    * fix comment
    
    * more typos
    
    * prevent None access when item is unshared in between the share event and this lookup
    
    * rename
    
    * final typo fix :)
    
    * disable rule that relies on boxsdk by default
    
    * addressing pr comments
    
    * fixing fmt
    
    * addressing more PR comments
    
    * addressing more PR commentsv2
    
    * rename aws sm
    
    * print the set of missing keys rather than all keys
    
    * fmt
    
    * address pr comment
    
    * fmt

---
+++ box_rules/box_event_triggered_externally.py
@@ -0,0 +1,21 @@
+DOMAINS = {
+    '@example.com',
+}
+
+
+def rule(event):
+    # Check that all events are triggered by internal users
+    if event.get('event_type') not in ('FAILED_LOGIN', 'SHIELD_ALERT'):
+        user = event.get('created_by', {})
+        # user id 2 indicates an anonymous user
+        if user.get('id', '') == '2':
+            return True
+        return user.get('login') and not any(
+            user.get('login').endswith(x) for x in DOMAINS)
+    return False
+
+
+def title(event):
+    message = ('External user [{}] triggered a box event.')
+    return message.format(
+        event.get('created_by', {}).get('login', '<UNKNOWN_USER>'))
