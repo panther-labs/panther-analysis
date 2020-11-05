c785d18499be60c345e0fff5578775ad595d45b4
Lindsey Whitehurst <43453975+lindsey-w@users.noreply.github.com>
2020-11-05 09:51:36 -0800

    gsuite perms delegated (#140)
    * update title verbiage and extract proper fields
    
    * lint fix

--- gsuite_reports_rules/gsuite_permissions_delegated.py
+++ gsuite_reports_rules/gsuite_permissions_delegated.py
@@ -1,3 +1,6 @@
+from panther_base_helpers import gsuite_parameter_lookup as param_lookup  # pylint: disable=import-error
+
+
 def rule(event):
     if event['id'].get('applicationName') != 'admin':
         return False
@@ -11,5 +14,13 @@ def rule(event):
 
 
 def title(event):
-    return 'User [{}] was delegated new administrator privileges'.format(
-        event.get('actor', {}).get('email'))
+    role = '<UNKNOWN_ROLE>'
+    user = '<UKNNOWN_USER>'
+    for details in event.get('events', [{}]):
+        if (details.get('type') == 'DELEGATED_ADMIN_SETTINGS' and
+                details.get('name') == 'ASSIGN_ROLE'):
+            role = param_lookup(details.get('parameters', {}), 'ROLE_NAME')
+            user = param_lookup(details.get('parameters', {}), 'USER_EMAIL')
+            break
+    return 'User [{}] delegated new administrator privileges [{}] to [{}]'.format(
+        event.get('actor', {}).get('email'), role, user)
