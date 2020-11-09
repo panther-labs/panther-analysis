dee60810f73f4075daa393e7296f6d08bee816c9
Jack Naglieri <jack@runpanther.io>
2020-11-09 14:49:44 -0800

    [update again]

--- test_scenarios/send_data.py
+++ test_scenarios/send_data.py
@@ -5,50 +5,76 @@ import json
 from os import path
 import yaml
 
-REGION = 'us-east-2'
-ACCOUNT_ID = '185324533555'
-QUEUE_NAME = 'panther-source-e9fa3b38-45c8-40ba-a1b2-e06d8468722f'
-QUEUE_URL = 'https://sqs.us-east-2.amazonaws.com/{AccountID}/{QueueName}'
+QUEUE_URL = 'https://sqs.{Region}.amazonaws.com/{AccountID}/{QueueName}'
+FLOW_LOG_HEADER = 'version account-id interface-id srcaddr dstaddr srcport dstport protocol packets bytes start end action log-status'
 
 
-def send_message(client, body):
-    return client.send_message(
-        QueueUrl=QUEUE_URL.format(AccountID=ACCOUNT_ID, QueueName=QUEUE_NAME),
-        MessageBody=json.dumps(body)
-    )
+def send_message(client, queue_url, message, message_format):
+    if message_format == 'raw':
+        message_str = message
+    elif message_format == 'json':
+        message_str = json.dumps(message)
+    return client.send_message(QueueUrl=queue_url, MessageBody=message_str)
 
 
-def main(filename):
-    if not path.exists(filename):
-        logging.error('File does not exist: %s', filename)
+def process_file(client, queue_url, logs, log_type, message_format):
+    if log_type == 'AWS.CloudTrail':
+        logging.info('Sending %d CloudTrail logs...', len(logs))
+        for indx, log in enumerate(logs):
+            # Wrap the CloudTrail in a 'Records' top-level key
+            resp = send_message(client, queue_url, {'Records': [log]},
+                                message_format)
+            logging.debug('Message [%d] response: %s', indx + 1,
+                          resp['ResponseMetadata']['HTTPStatusCode'])
+        return
+
+    if log_type == 'AWS.VPCFlow':
+        logging.debug('Sending VPC Flow log header')
+        resp = send_message(client, queue_url, FLOW_LOG_HEADER, 'raw')
+        logging.debug('Message [1] response: %s',
+                      resp['ResponseMetadata']['HTTPStatusCode'])
+
+    logging.info('Sending %d %s logs...', len(logs), log_type)
+    for indx, log in enumerate(logs):
+        resp = send_message(client, queue_url, log, message_format)
+        logging.debug('Message [%d] response: %s', indx + 1,
+                      resp['ResponseMetadata']['HTTPStatusCode'])
+
+
+def main(args):
+    if not path.exists(args.file):
+        logging.error('File does not exist: %s', args.file)
         return False
 
-    client = boto3.client('sqs', region_name=REGION)
-
-    with open(filename) as file:
+    with open(args.file) as file:
         data = yaml.load(file, Loader=yaml.FullLoader)
 
-    if data['Type'] == 'AWS.CloudTrail':
-        logging.info('Sending %d CloudTrail logs...', len(data['Logs']))
-        for indx, log in enumerate(data['Logs']):
-            # Wrap the CloudTrail in a 'Records' top-level key
-            resp = send_message(client, {'Records': [log]})
-            logging.info('\t%d: %s', indx+1, resp['ResponseMetadata']['HTTPStatusCode'])
-    else:
-        logging.info('Sending %d %s logs...', len(data['Logs']), data['Type'])
-        for indx, log in enumerate(data['Logs']):
-            resp = send_message(client, log)
-            logging.info('%d: %s', indx+1, resp['ResponseMetadata']['HTTPStatusCode'])
-    return True
+    process_file(
+        boto3.client('sqs', region_name=args.region),
+        QUEUE_URL.format(AccountID=args.account_id, QueueName=args.queue_name),
+        data.get('Logs', []), data.get('LogType', ''),
+        data.get('Format', 'json'))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Send test data to Panther.')
-    parser.add_argument('--file', help='the yml file with test data')
+    parser.add_argument('--file',
+                        help='the yml file with test data',
+                        required=True)
+    parser.add_argument('--account-id',
+                        help='the AWS account ID of the Panther deployment',
+                        required=True)
+    parser.add_argument('--queue-name',
+                        help='the SQS Queue of the Panther source',
+                        required=True)
+    parser.add_argument(
+        '--region',
+        help='the region of the SQS Queue of the Panther source',
+        required=True)
     args = parser.parse_args()
 
-    logging.basicConfig(
-        format='[%(asctime)s %(levelname)-8s] %(message)s',
-        level=logging.INFO,
-        datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(format='[%(asctime)s %(levelname)-8s] %(message)s',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
 
-    main(args.file)
+    main(args)
