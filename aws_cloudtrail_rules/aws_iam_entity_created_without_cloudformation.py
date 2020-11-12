55947b13c52d5a65cd70de7f543e86bfac8d38e1
Weyland <weyland.chiang@gmail.com>
2020-11-12 18:04:33 -0500

    Fixed line formatting

--- aws_cloudtrail_rules/aws_iam_entity_created_without_cloudformation.py
+++ aws_cloudtrail_rules/aws_iam_entity_created_without_cloudformation.py
@@ -9,6 +9,7 @@ IAM_ADMIN_ROLES = {
 IAM_ADMIN_ROLE_PATTERNS = { 
     'arn:aws:iam::[0-9]+:role/IdentityCFNServiceRole'
 }
+
 # API calls that are indicative of IAM entity creation
 IAM_ENTITY_CREATION_EVENTS = { 
     'BatchCreateUser',
@@ -20,13 +21,17 @@ IAM_ENTITY_CREATION_EVENTS = {
     'CreateServiceLinkedRole',
     'CreateUser',
 }
+
+
 def rule(event):
     # Check if this event is in scope
     if event['eventName'] not in IAM_ENTITY_CREATION_EVENTS:
         return False
+
     # All IAM changes MUST go through CloudFormation
     if event['userIdentity'].get('invokedBy') != 'cloudformation.amazonaws.com':
         return True
+
     # Only approved IAM Roles can make IAM Changes
     for admin_role_pattern in IAM_ADMIN_ROLES:
         # Check if the arn matches any role patterns, returns False (whitelisting it) if there is a match
