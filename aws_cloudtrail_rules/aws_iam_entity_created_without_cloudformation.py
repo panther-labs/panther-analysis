3d282af1f8cd8ebb1b8814ad1cb1b9db4a25d5c2
Weyland <weyland.chiang@gmail.com>
2020-11-12 18:02:37 -0500

    Added example of regex checking for IAM arns

--- aws_cloudtrail_rules/aws_iam_entity_created_without_cloudformation.py
+++ aws_cloudtrail_rules/aws_iam_entity_created_without_cloudformation.py
@@ -1,10 +1,16 @@
+import re
+
 # The role dedicated for IAM administration
-IAM_ADMIN_ROLES = {
+IAM_ADMIN_ROLES = { 
     'arn:aws:iam::123456789012:role/IdentityCFNServiceRole',
 }
 
+# The role patterns dedicated for IAM Service Roles 
+IAM_ADMIN_ROLE_PATTERNS = { 
+    'arn:aws:iam::[0-9]+:role/IdentityCFNServiceRole'
+}
 # API calls that are indicative of IAM entity creation
-IAM_ENTITY_CREATION_EVENTS = {
+IAM_ENTITY_CREATION_EVENTS = { 
     'BatchCreateUser',
     'CreateGroup',
     'CreateInstanceProfile',
@@ -14,17 +20,18 @@ IAM_ENTITY_CREATION_EVENTS = {
     'CreateServiceLinkedRole',
     'CreateUser',
 }
-
-
 def rule(event):
     # Check if this event is in scope
     if event['eventName'] not in IAM_ENTITY_CREATION_EVENTS:
         return False
-
     # All IAM changes MUST go through CloudFormation
     if event['userIdentity'].get('invokedBy') != 'cloudformation.amazonaws.com':
         return True
-
     # Only approved IAM Roles can make IAM Changes
-    return event['userIdentity']['sessionContext']['sessionIssuer'][
-        'arn'] not in IAM_ADMIN_ROLES
+    for admin_role_pattern in IAM_ADMIN_ROLES:
+        # Check if the arn matches any role patterns, returns False (whitelisting it) if there is a match
+        if len(re.findall(admin_role_pattern, event['userIdentity']['sessionContext']['sessionIssuer']['arn'])) > 0 : 
+            return False
+
+    return event['userIdentity']['sessionContext']['sessionIssuer']['arn'] not in IAM_ADMIN_ROLES
+
