8d8dbb02b43b9d0576335b65c63ed58585adba8e
lindsey-w <lindsey.whitehurst@runpanther.io>
2020-12-10 11:12:34 -0500

    moved files

---
+++ standard_rules/brute_force_by_ip.py
@@ -0,0 +1,14 @@
+import panther_event_type_helpers as event_type
+
+
+def rule(event):
+    # filter events on unified data model field
+    return bool(event.udm('event_type')) and event.udm(
+        'event_type') == event_type.FAILED_LOGIN
+
+
+def title(event):
+    # use unified data model field in title
+    return '{}: User [{}] from IP [{}] has exceeded the failed logins threshold'.format(
+        event.get('p_log_type'), event.udm('actor_user'),
+        event.udm('source_ip'))
