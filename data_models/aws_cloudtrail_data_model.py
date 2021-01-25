5101c77788ee8e11e82abf56fd4299a0e195cb0a
Jack Naglieri <jack@runpanther.io>
2021-01-24 17:06:52 -0800

    [models] update cloudtrail model to search for IPs

--- data_models/aws_cloudtrail_data_model.py
+++ data_models/aws_cloudtrail_data_model.py
@@ -1,3 +1,4 @@
+import ipaddress
 import panther_event_type_helpers as event_type
 from panther_base_helpers import deep_get
 
@@ -6,8 +7,26 @@ def get_event_type(event):
     # currently, only tracking a few event types
     if event.get('eventName') == 'ConsoleLogin' and deep_get(
             event, 'userIdentity', 'type') == 'IAMUser':
-        if event.get('responseElements', {}).get('ConsoleLogin') == 'Failure':
+        if deep_get(event, 'responseElements', 'ConsoleLogin') == 'Failure':
             return event_type.FAILED_LOGIN
-        if event.get('responseElements', {}).get('ConsoleLogin') == 'Success':
+        if deep_get(event, 'responseElements', 'ConsoleLogin') == 'Success':
             return event_type.SUCCESSFUL_LOGIN
     return None
+
+
+def load_ip_address(event):
+    '''
+    CloudTrail occassionally sets non-IPs in the sourceIPAddress field.
+    This method ensures that either an IPv4 or IPv6 address is always returned.
+    '''
+    source_ip = event.get('sourceIPAddress')
+    if not source_ip:
+        return None
+    try:
+        ip = ipaddress.IPv4Address(source_ip)
+    except ipaddress.AddressValueError:
+        try:
+            ip = ipaddress.IPv6Address(source_ip)
+        except ipaddress.AddressValueError:
+            return None
+    return ip
