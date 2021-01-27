385bc2b1791c602a03d72b450046868f3df99a6d
Weyland <71197790+wey-chiang@users.noreply.github.com>
2021-01-27 13:58:00 -0500

    Removed the evaluate threshold to get the rule running properly

--- gsuite_reports_rules/gsuite_brute_force_login.py
+++ gsuite_reports_rules/gsuite_brute_force_login.py
@@ -1,5 +1,4 @@
 from panther_base_helpers import deep_get, gsuite_details_lookup as details_lookup
-from panther_oss_helpers import evaluate_threshold
 '''
 SELECT *
 FROM panther_logs.public.gsuite_reports
@@ -7,25 +6,13 @@ WHERE id:applicationName = 'login'
 AND events[0]:name = 'login_failure' LIMIT 10;
 '''
 
-# TODO change to native thresholding once support is added
-# tentatively slated for 1.7
-THRESH = 10
-THRESH_TTL = 600  # 10 minutes
-
-
 def rule(event):
     # Filter login events
     if deep_get(event, 'id', 'applicationName') != 'login':
         return False
 
     # Pattern match this event to the recon actions
-    details = details_lookup('login', ['login_failure'], event)
-    return bool(details) and evaluate_threshold(
-        '{}-GSuiteLoginFailedCounter'.format(
-            deep_get(event, 'actor', 'email', default='<UNKNOWN_EMAIL>')),
-        THRESH,
-        THRESH_TTL,
-    )
+    return bool(details_lookup('login', ['login_failure'], event))
 
 
 def title(event):
