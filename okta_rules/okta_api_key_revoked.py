719bb3cb14f178b5b2c65b4ecf7e43c726d75f17
Don Magee <DonMagee@users.noreply.github.com>
2021-03-01 15:40:50 -0500

    feat: create okta api key rules (#195)
    * feat: create okta api key rules
    
    Creates two new rules for to detect the creation and revocation of Okta
    API keys.
    
    * style: fixed trailing whitespace
    
    * style: removed trailing whitespace
    
    * Update okta_rules/okta_api_key_created.py
    
    Co-authored-by: Weyland <71197790+wey-chiang@users.noreply.github.com>
    
    * Update okta_rules/okta_api_key_revoked.py
    
    Co-authored-by: Weyland <71197790+wey-chiang@users.noreply.github.com>
    
    * Fixed CI issues by formatting file causing issue with black
    
    * Fixed CI issues by formatting file causing issue with black
    
    Co-authored-by: Weyland <71197790+wey-chiang@users.noreply.github.com>
    Co-authored-by: Lindsey Whitehurst <43453975+lindsey-w@users.noreply.github.com>

---
+++ okta_rules/okta_api_key_revoked.py
@@ -0,0 +1,27 @@
+from panther_base_helpers import deep_get, okta_alert_context  # pylint: disable=import-error
+
+
+def rule(event):
+    return (
+        event.get("eventType", None) == "system.api_token.revoke"
+        and deep_get(event, "outcome", "result") == "SUCCESS"
+    )
+
+
+def title(event):
+    title_str = "{} <{}> revoked the API key [{}]"
+
+    target = event.get("target", [{}])
+    display_name = (
+        target[0].get("displayName", "MISSING DISPLAY NAME") if target else "MISSING TARGET"
+    )
+
+    return title_str.format(
+        deep_get(event, "actor", "displayName"),
+        deep_get(event, "actor", "alternateId"),
+        display_name,
+    )
+
+
+def alert_context(event):
+    return okta_alert_context(event)
