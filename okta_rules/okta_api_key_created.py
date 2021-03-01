fc7e851825b3b9dc8afd6f5cd6bbeb5b8c5d81d2
Weyland <71197790+wey-chiang@users.noreply.github.com>
2021-03-01 15:36:59 -0500

    Fixed CI issues by formatting file causing issue with black

--- okta_rules/okta_api_key_created.py
+++ okta_rules/okta_api_key_created.py
@@ -2,19 +2,25 @@ from panther_base_helpers import deep_get, okta_alert_context  # pylint: disable
 
 
 def rule(event):
-    return (event.get('eventType', None) == 'system.api_token.create' and
-            deep_get(event, 'outcome', 'result') == 'SUCCESS')
+    return (
+        event.get("eventType", None) == "system.api_token.create"
+        and deep_get(event, "outcome", "result") == "SUCCESS"
+    )
+
 
 def title(event):
-    title_str = '{} <{}> created a new API key [{}]'
+    title_str = "{} <{}> created a new API key [{}]"
 
-    target = event.get('target', [{}])
-    display_name = target[0].get('displayName', 'MISSING DISPLAY NAME') if target else 'MISSING TARGET'
+    target = event.get("target", [{}])
+    display_name = (
+        target[0].get("displayName", "MISSING DISPLAY NAME") if target else "MISSING TARGET"
+    )
 
     return title_str.format(
-        deep_get(event, 'actor', 'displayName'),
-        deep_get(event, 'actor', 'alternateId'),
-        display_name)
+        deep_get(event, "actor", "displayName"),
+        deep_get(event, "actor", "alternateId"),
+        display_name,
+    )
 
 
 def alert_context(event):
