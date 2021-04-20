307d50c06319c91bd67c88315464fe09719b191c
Weyland <71197790+wey-chiang@users.noreply.github.com>
2021-04-20 15:39:14 -0400

    Revamped S3 Bucket Lifecycle Config Policy to use deep_get, dictionary (#250)
    * Revamped S3 Bucket Lifecycle Config Policy to use deep_get, dictionary
    .get method and added check for int-None comparison
    
    * Added unit test for s3 bucket lifecycle null test
    
    * Added `resolve_timestamp_string` global helper
    
    * Replaced resolved_time_string broad except for specific Exceptions
    
    * Fixed linting issues, removed tzinfo replacement
    
    * Changed timestamp resolver global helper to use a list instead of a set
    
    * Optimized resolve_timestamp_string preprocessing and fixed epoch format resolution
    
    * Removed an extraneous empty line
    
    * Moved comment to a separate line for linting
    
    * Update aws_s3_policies/aws_s3_bucket_lifecycle_configuration.py
    
    Co-authored-by: Nick Hakmiller <49166439+nhakmiller@users.noreply.github.com>
    
    * Replaced a timestamp conversion to use the new global and simplified
    some logic for the S3 Policy for Lifecycle Configuration
    
    * Removed unused import
    
    Co-authored-by: Nick Hakmiller <49166439+nhakmiller@users.noreply.github.com>

--- aws_iam_policies/aws_access_key_unused.py
+++ aws_iam_policies/aws_access_key_unused.py
@@ -1,12 +1,17 @@
 import datetime
+from panther_oss_helpers import resolve_timestamp_string
 
 TIMEOUT_DAYS = datetime.timedelta(days=90)
-AWS_TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 DEFAULT_TIME = "0001-01-01T00:00:00Z"
 
 
 def aged_out(timestamp):
-    datetime_ts = datetime.datetime.strptime(timestamp, AWS_TIMESTAMP_FORMAT)
+    if not timestamp:
+        return False
+    datetime_ts = resolve_timestamp_string(timestamp)
+
+    if not datetime_ts:
+        return True
     return (datetime.datetime.now() - datetime_ts) > TIMEOUT_DAYS
 
 
@@ -14,26 +19,26 @@ def policy(resource):
     # If a user is less than 4 hours old, it may not have a credential report generated yet.
     # It will be re-scanned periodically until a credential report is found, at which point this
     # policy will be properly evaluated.
-    report = resource["CredentialReport"]
+    report = resource.get("CredentialReport")
     if not report:
         return True
 
-    if report["AccessKey1Active"]:
-        if report["AccessKey1LastUsedDate"] != DEFAULT_TIME and aged_out(
-            report["AccessKey1LastUsedDate"]
+    if report.get("AccessKey1Active"):
+        if report.get("AccessKey1LastUsedDate") != DEFAULT_TIME and aged_out(
+            report.get("AccessKey1LastUsedDate")
         ):
             return False
-        if report["AccessKey1LastUsedDate"] == DEFAULT_TIME and aged_out(
-            report["AccessKey1LastRotated"]
+        if report.get("AccessKey1LastUsedDate") == DEFAULT_TIME and aged_out(
+            report.get("AccessKey1LastRotated")
         ):
             return False
-    if report["AccessKey2Active"]:
-        if report["AccessKey2LastUsedDate"] != DEFAULT_TIME and aged_out(
-            report["AccessKey2LastUsedDate"]
+    if report.get("AccessKey2Active"):
+        if report.get("AccessKey2LastUsedDate") != DEFAULT_TIME and aged_out(
+            report.get("AccessKey2LastUsedDate")
         ):
             return False
-        if report["AccessKey2LastUsedDate"] == DEFAULT_TIME and aged_out(
-            report["AccessKey2LastRotated"]
+        if report.get("AccessKey2LastUsedDate") == DEFAULT_TIME and aged_out(
+            report.get("AccessKey2LastRotated")
         ):
             return False
 
--- global_helpers/panther_oss_helpers.py
+++ global_helpers/panther_oss_helpers.py
@@ -1,13 +1,74 @@
 """Utility functions provided to policies and rules during execution."""
+import re
 import os
 import time
-from typing import Any, Dict, Sequence, Set, Union
+from datetime import datetime
+from typing import Any, Dict, Sequence, Set, Union, Optional
 
 import boto3
 
 _RESOURCE_TABLE = None  # boto3.Table resource, lazily constructed
 FIPS_ENABLED = os.getenv("ENABLE_FIPS", "").lower() == "true"
 FIPS_SUFFIX = "-fips." + os.getenv("AWS_REGION", "") + ".amazonaws.com"
+
+# Auto Time Resolution Parameters
+EPOCH_REGEX = r"([0-9]{9,12}(\.\d+)?)"
+TIME_FORMATS = [
+    "%Y-%m-%dT%H:%M:%SZ",  # AWS Timestamp
+    "%Y-%m-%dT%H:%M:%S.%fZ",  # Panther Timestamp
+    "%Y-%m-%dT%H:%M:%S*%f%z",
+    "%Y %b %d %H:%M:%S.%f %Z",
+    "%b %d %H:%M:%S %z %Y",
+    "%d/%b/%Y:%H:%M:%S %z",
+    "%b %d, %Y %I:%M:%S %p",
+    "%b %d %Y %H:%M:%S",
+    "%b %d %H:%M:%S %Y",
+    "%b %d %H:%M:%S %z",
+    "%b %d %H:%M:%S",
+    "%Y-%m-%dT%H:%M:%S%z",
+    "%Y-%m-%dT%H:%M:%S.%f%z",
+    "%Y-%m-%d %H:%M:%S %z",
+    "%Y-%m-%d %H:%M:%S%z",
+    "%Y-%m-%d %H:%M:%S,%f",
+    "%Y/%m/%d*%H:%M:%S",
+    "%Y %b %d %H:%M:%S.%f*%Z",
+    "%Y %b %d %H:%M:%S.%f",
+    "%Y-%m-%d %H:%M:%S,%f%z",
+    "%Y-%m-%d %H:%M:%S.%f",
+    "%Y-%m-%d %H:%M:%S.%f%z",
+    "%Y-%m-%dT%H:%M:%S.%f",
+    "%Y-%m-%dT%H:%M:%S",
+    "%Y-%m-%dT%H:%M:%S%z",
+    "%Y-%m-%dT%H:%M:%S.%f",
+    "%Y-%m-%dT%H:%M:%S",
+    "%Y-%m-%d*%H:%M:%S:%f",
+    "%Y-%m-%d*%H:%M:%S",
+    "%y-%m-%d %H:%M:%S,%f %z",
+    "%y-%m-%d %H:%M:%S,%f",
+    "%y-%m-%d %H:%M:%S",
+    "%y/%m/%d %H:%M:%S",
+    "%y%m%d %H:%M:%S",
+    "%Y%m%d %H:%M:%S.%f",
+    "%m/%d/%y*%H:%M:%S",
+    "%m/%d/%Y*%H:%M:%S",
+    "%m/%d/%Y*%H:%M:%S*%f",
+    "%m/%d/%y %H:%M:%S %z",
+    "%m/%d/%Y %H:%M:%S %z",
+    "%H:%M:%S",
+    "%H:%M:%S.%f",
+    "%H:%M:%S,%f",
+    "%d/%b %H:%M:%S,%f",
+    "%d/%b/%Y:%H:%M:%S",
+    "%d/%b/%Y %H:%M:%S",
+    "%d-%b-%Y %H:%M:%S",
+    "%d-%b-%Y %H:%M:%S.%f",
+    "%d %b %Y %H:%M:%S",
+    "%d %b %Y %H:%M:%S*%f",
+    "%m%d_%H:%M:%S",
+    "%m%d_%H:%M:%S.%f",
+    "%m/%d/%Y %I:%M:%S %p:%f",
+    "%m/%d/%Y %I:%M:%S %p",
+]
 
 
 class BadLookup(Exception):
@@ -16,6 +77,32 @@ class BadLookup(Exception):
 
 class PantherBadInput(Exception):
     """Error returned when a Panther helper function is provided bad input."""
+
+
+def resolve_timestamp_string(timestamp: str) -> Optional[datetime]:
+    """Auto Time Resolution"""
+    if not timestamp:
+        return None
+
+    # Removes weird single-quotes used in some timestamp formats
+    ts_format = timestamp.replace("\'", "")
+    # Attempt to resolve timestamp format
+    for each_format in TIME_FORMATS:
+        try:
+            return datetime.strptime(ts_format, each_format)
+        except (ValueError, TypeError):
+            continue
+
+    # Attempt to resolve epoch format
+    # Since datetime.utcfromtimestamp supports 9 through 12 digit epoch timestamps
+    # and we only want the first 12 digits.
+    match = re.match(EPOCH_REGEX, timestamp)
+    if match.group(0) != "":
+        try:
+            return datetime.utcfromtimestamp(float(match.group(0)))
+        except (ValueError, TypeError):
+            return None
+    return None
 
 
 def get_s3_arn_by_name(name: str) -> str:
