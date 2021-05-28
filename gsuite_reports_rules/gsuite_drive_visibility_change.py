327801a779d8633a1877f925b7da0c9e084139dc
glerb <56281590+glerb@users.noreply.github.com>
2021-05-27 18:28:45 -0700

    INCOMPLETE More exact specification of changes

--- gsuite_reports_rules/gsuite_drive_visibility_change.py
+++ gsuite_reports_rules/gsuite_drive_visibility_change.py
@@ -1,6 +1,13 @@
 from panther_base_helpers import deep_get
 from panther_base_helpers import gsuite_parameter_lookup as param_lookup
 
+#
+VISIBILITY = [
+    "people_with_link",
+    "public_on_the_web",
+    "shared_externally",
+    "unknown"
+    ]
 
 def rule(event):
     if deep_get(event, "id", "applicationName") != "drive":
@@ -10,6 +17,7 @@ def rule(event):
         if (
             details.get("type") == "acl_change"
             and param_lookup(details.get("parameters", {}), "visibility_change") == "external"
+            and param_lookup(details.get("parameters", {}), "visibility") in VISIBILITY
         ):
             return True
 
@@ -35,8 +43,16 @@ def title(event):
                 doc_title = param_lookup(detail.get("parameters", {}), "doc_title")
             if param_lookup(detail.get("parameters", {}), "target_user"):
                 target_user_email = param_lookup(detail.get("parameters", {}), "target_user")
+            if param_lookup(detail.get("parameters", {}), "target_domain"):
+                target_domain = param_lookup(detail.get("parameters", {}), "target_domain")
             break
+    if target_user_email != "<EMAIL_UNKNOWN>":
+        sharing_scope = target_user_email
+    elif target_domain == "all":
+        sharing_scope = f"all domains"
+    else:
+        sharing_scope = target_domain
     return (
         f"User [{deep_get(event, 'actor', 'email', default='<UNKNOWN_EMAIL>')}] made a document "
-        f"[{doc_title}] externally visible for the first time with [{target_user_email}]"
+        f"[{doc_title}] externally visible to [{sharing_scope}]"
     )
