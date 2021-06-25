5992f64ec432934a2ff868e6734da5bdeafcdce3
lindsey-w <lindsey.whitehurst@runpanther.io>
2021-06-25 14:42:19 -0700

    add org modification rule

---
+++ github_rules/github_org_modified.py
@@ -0,0 +1,17 @@
+def rule(event):
+    return (
+        event.get("action") == "org.add_member"
+        or event.get("action") == "org.remove_member"
+    )
+
+
+def title(event):
+    action = event.get("action")
+    if event.get("action") == "org.add_member":
+        action = "added"
+    elif event.get("action") == "org.remove_member":
+        action = "removed"
+    return (
+        f"GitHub.Audit: User [{event.udm('actor_user')}] {action} "
+        f"{event.get('user', '<UNKNOWN_USER>')} to org [{event.get('org','<UNKNOWN_ORG>')}]"
+    )
