9332672dab9111220f05b96aba7f06b8e82ac432
glerb <56281590+glerb@users.noreply.github.com>
2021-06-27 19:43:11 -0700

    Rework - still failing timedelta

--- standard_rules/unusual_login.py
+++ standard_rules/unusual_login.py
@@ -1,16 +1,22 @@
 import ast
 import json
 import logging
+from datetime import datetime
 
 import panther_event_type_helpers as event_type
-from panther_oss_helpers import geoinfo_from_ip, get_string_set, put_string_set
+from panther_oss_helpers import (
+    geoinfo_from_ip,
+    get_string_set,
+    put_string_set,
+    resolve_timestamp_string,
+)
 
 # number of unique geolocation city:region combinations retained in the
 # panther-kv-table in Dynamo to suppress alerts
 GEO_HISTORY_LENGTH = 5
-FINGERPRINT = {}
 GEO_INFO = {}
-UPDATED_GEO_INFO = 
+GEO_HISTORY = set()
+
 
 def rule(event):
     # GEO_INFO is mocked as a string in unit tests and redeclared as a dict
@@ -89,13 +95,14 @@ def rule(event):
                 oldest_login = geo
         logging.debug("updated_geo_logins before removing oldest entry:")
         logging.debug(updated_geo_logins)
-        GEO_HISTORY = updated_geo_logins
         updated_geo_logins.pop(oldest_login)
         logging.debug("updated_geo_logins after removing oldest entry:")
         logging.debug(updated_geo_logins)
 
     # Mocked during unit testing
     put_string_set(event_key, [json.dumps(updated_geo_logins)])
+    global GEO_HISTORY  # pylint: disable=global-statement
+    GEO_HISTORY = updated_geo_logins
 
     return True
 
@@ -113,24 +120,21 @@ def title(event):
         f" (not in last [{GEO_HISTORY_LENGTH}] login locations)"
     )
 
-'''
+
 def alert_context(event):
     # round to days:hours:minutes
-    # need to truncate p_event_time to microseconds for strptime
-    # not sure how resolve_timestamp_string() in oss helpers would work
-    # with the current p_event_time in nanoseconds
-    parts_event_time = p_event_time.split('.')
-    # next line doesn't work but it should
-    event_seconds_truncated = '{:06d}'.format(int(parts_event_time[-1])) 
-    event_time_truncated = '.'.join(parts[:-1],event_seconds_truncated)
-
-    parts_parse_time = p_parse_time.split('.')
-    # next line doesn't work but it should
-    parse_seconds_truncated = '{:06d}'.format(int(parts_parse_time[-1])) 
-    parse_time_truncated = '.'.join(parts[:-1],parse_seconds_truncated)
-    time_delta = resolve_timestamp_string(parse_time_truncated') - resolve_timestamp_string(event_time_truncated')
-    return ({
+    event_time_truncated = nano_to_micro(event.get("p_event_time"))
+    parse_time_truncated = nano_to_micro(event.get("p_parse_time"))
+    time_delta = resolve_timestamp_string(parse_time_truncated) - resolve_timestamp_string(
+        event_time_truncated
+    )
+    return {
         "loginHistory": f"{json.dumps(GEO_HISTORY)}",
-        "logEventParsingDelay": f"{time_delta.strftime('%d:%H:%M')}"
-})
-'''
+        "logEventParsingDelay": f"{datetime.strftime(time_delta, '%d:%H:%M')}",
+    }
+
+
+def nano_to_micro(time_str: str) -> str:
+    parts = time_str.split(":")
+    parts[-1] = "{:06f}".format(float(parts[-1]))
+    return ":".join(parts)
