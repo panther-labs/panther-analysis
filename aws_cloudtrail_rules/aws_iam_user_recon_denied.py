d6b8f5861d4ac4a84d7bceabca36484bd6dd91e4
glerb <56281590+glerb@users.noreply.github.com>
2021-07-15 17:25:37 -0700

    Remove extra library

--- aws_cloudtrail_rules/aws_iam_user_recon_denied.py
+++ aws_cloudtrail_rules/aws_iam_user_recon_denied.py
@@ -1,4 +1,3 @@
-from fnmatch import fnmatch
 from ipaddress import ip_address
 
 from panther import lookup_aws_account_name
@@ -6,11 +5,11 @@ from panther_base_helpers import deep_get
 
 # service/event patterns to monitor
 RECON_ACTIONS = {
-    "dynamodb": ["List*", "Describe*", "Get*"],
-    "ec2": ["Describe*", "Get*"],
-    "iam": ["List*", "Get*"],
-    "s3": ["List*", "Get*"],
-    "rds": ["Describe*", "List*"],
+    "dynamodb": ["List", "Describe", "Get"],
+    "ec2": ["Describe", "Get"],
+    "iam": ["List", "Get"],
+    "s3": ["List", "Get"],
+    "rds": ["Describe", "List"],
 }
 
 
@@ -30,7 +29,7 @@ def rule(event):
     # Pattern match this event to the recon actions
     for event_source, event_patterns in RECON_ACTIONS.items():
         if event.get("eventSource", "").startswith(event_source) and any(
-            fnmatch(event.get("eventName", ""), event_pattern) for event_pattern in event_patterns
+            event.get("eventName", "").startswith(event_pattern) for event_pattern in event_patterns
         ):
             return True
     return False
