a74e3481dfac940b48ed776d01b2f17172f5042d
lindsey-w <lindsey.whitehurst@runpanther.io>
2021-07-19 16:15:01 -0500

    rename; check kms key first; try to use mock

---
+++ aws_cloudwatch_policies/aws_cloudwatch_loggroup_sensitive_encrypted.py
@@ -0,0 +1,14 @@
+from fnmatch import fnmatch
+
+# replace the log groups ARN regex with the
+# log groups that should be encrypted with your CMK
+SENSITIVE_LOG_GROUP_ARN_REGEXS = {}
+
+
+def policy(resource):
+    if resource["KmsKeyId"] is None:
+        if SENSITIVE_LOG_GROUP_ARN_REGEXS and any(
+            fnmatch(resource.get("Arn"), group_arn) for group_arn in SENSITIVE_LOG_GROUP_ARN_REGEXS
+        ):
+            return False
+    return True
