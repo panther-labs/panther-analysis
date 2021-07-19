cc5ca91f1bd10d1485dfc7f46707fb5919893c21
lindsey-w <lindsey.whitehurst@runpanther.io>
2021-07-19 15:28:06 -0500

    refine

--- aws_cloudwatch_policies/aws_cloudwatch_sensitive_loggroup_encrypted.py
+++ aws_cloudwatch_policies/aws_cloudwatch_sensitive_loggroup_encrypted.py
@@ -1,19 +1,13 @@
 from fnmatch import fnmatch
 
-# replace the log groups regex with the
-# log groups you have encrypted with your CMK
-SENSITIVE_LOG_GROUP_NAME_REGEXS = {"LogGroup-2*"}
-
-SENSITIVE_LOG_GROUP_ARN_REGEXS = {}
+# replace the log groups ARN regex with the
+# log groups that should be encrypted with your CMK
+SENSITIVE_LOG_GROUP_ARN_REGEXS = {"*LogGroup-2*"}
 
 
 def policy(resource):
-    if SENSITIVE_LOG_GROUP_NAME_REGEXS and not any(
-        fnmatch(resource.get("Name"), group_name) for group_name in SENSITIVE_LOG_GROUP_NAME_REGEXS
-    ):
-        return True
     if SENSITIVE_LOG_GROUP_ARN_REGEXS and not any(
-        fnmatch(resource.get("arn"), group_arn) for group_arn in SENSITIVE_LOG_GROUP_ARN_REGEXS
+        fnmatch(resource.get("Arn"), group_arn) for group_arn in SENSITIVE_LOG_GROUP_ARN_REGEXS
     ):
         return True
     return resource["KmsKeyId"] is not None
