ab11044a5ebff91f13496b9b1f6dc2313acbea2e
Lindsey Whitehurst <43453975+lindsey-w@users.noreply.github.com>
2021-08-18 14:54:39 -0700

    first pass: scheduled rules + queries (#257)
    * add templates
    
    * adding queries
    
    * adding initial round of scheduled rules
    
    * add missing field
    
    * fix error
    
    * auto-disable queries; update pack yml
    
    * move to new dir
    
    * fmt"
    
    * add some additional queries
    
    * update pat
    
    * disable
    
    * fixing up with testing
    
    * PR feedbakc
    
    * removing query packs for now
    
    * update rate to cron
    
    * renaming and such
    
    * more cleanup/renaming
    
    * chore:lint
    
    * minor updates + add a couple custom titles
    
    * update scheduled rule default
    
    * adding additional snowflake queries
    
    * PR feedback
    
    * PR feedback
    
    * make github happy
    
    * fix missing param
    
    * add default athena query (no supported) for snowflake monitoring
    
    * add default athena queries
    
    * update queries
    
    * fix spacing
    
    * fixing brute force scheduled query
    
    Co-authored-by: Nick Hakmiller <49166439+nhakmiller@users.noreply.github.com>
    Co-authored-by: Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>

---
+++ snowflake_queries/snowflake_user_enabled.py
@@ -0,0 +1,8 @@
+def rule(_):
+    return True
+
+def title(event):
+    query_text = event.get('query_text', '').split(' ')
+    if len(query_text) > 2:
+        return f"Snowflake user [{query_text[2]}] enabled by [{event.get('user_name','<UNKNOWN_ADMIN>')}]"
+    return f"Snowflake user [<UNKNOWN_USER>] enabled by [{event.get('user_name','<UNKNOWN_ADMIN>')}]"
