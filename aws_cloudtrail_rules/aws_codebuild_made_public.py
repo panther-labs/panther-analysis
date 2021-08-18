8703d15358301c35ae0b02a88ce766a3b324c6b4
Dakota <dakota@stedi.com>
2021-08-18 11:44:06 -0400

    feat: add detection for codebuild public projects

---
+++ aws_cloudtrail_rules/aws_codebuild_made_public.py
@@ -0,0 +1,13 @@
+from panther import lookup_aws_account_name
+from panther_base_helpers import deep_get
+
+
+def rule(event):
+    return (event['eventName'] == 'UpdateProjectVisibility' and deep_get(event, 'requestParameters', 'projectVisibility') == 'PUBLIC_READ')
+
+
+def title(event):
+    return 'AWS CodeBuild Project made Public by {} in account {}'.format(
+        deep_get(event, 'userIdentity', 'arn'),
+        lookup_aws_account_name(deep_get(event, 'recipientAccountId'))
+    )
