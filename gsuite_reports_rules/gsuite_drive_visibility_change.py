b8a1050817ee4a7bd12ca02c0ca30552575ae3f2
glerb <56281590+glerb@users.noreply.github.com>
2021-08-26 10:04:01 -0700

    Whitelist user domains

--- gsuite_reports_rules/gsuite_drive_visibility_change.py
+++ gsuite_reports_rules/gsuite_drive_visibility_change.py
@@ -3,7 +3,13 @@ from panther_base_helpers import gsuite_parameter_lookup as param_lookup
 
 EXCLUDED_DOMAINS = {"example.com"}
 
-VISIBILITY = {"people_with_link", "public_on_the_web", "shared_externally", "unknown"}
+VISIBILITY = {
+    "people_with_link",
+    "people_within_domain_with_link",
+    "public_on_the_web",
+    "shared_externally",
+    "unknown",
+}
 
 ALERT_DETAILS = {}
 
@@ -20,9 +26,17 @@ def init_alert_details(log):
         "ACCESS_SCOPE": "<UNKNOWN_ACCESS_SCOPE>",
         "DOC_TITLE": "<UNKNOWN_TITLE>",
         "NEW_VISIBILITY": "<UNKNOWN_VISIBILITY>",
-        "TARGET_USER_EMAILS": {"<UNKNOWN_USER>"},
+        "TARGET_USER_EMAILS": ["<UNKNOWN_USER>"],
         "TARGET_DOMAIN": "<UNKNOWN_DOMAIN>",
     }
+
+
+def user_is_external(target_user):
+    external = True
+    for domain in EXCLUDED_DOMAINS:
+        if domain in target_user:
+            external = False
+    return external
 
 
 def rule(event):
@@ -83,6 +97,8 @@ def rule(event):
     #########
     # for visibility changes that apply to a user
     # there is a change_user_access event for each user
+    # change_user_access and change_document_visibility events are
+    # not found in the same report
     change_user_access = False
 
     for details in event.get("events", [{}]):
@@ -90,33 +106,33 @@ def rule(event):
             details.get("type") == "acl_change"
             and details.get("name") == "change_user_access"
             and param_lookup(details.get("parameters", {}), "new_value") != ["none"]
+            and user_is_external(param_lookup(details.get("parameters", {}), "target_user"))
         ):
-            if ALERT_DETAILS[log]["TARGET_USER_EMAILS"] != {"<UNKNOWN_USER>"}:
-                ALERT_DETAILS[log]["TARGET_USER_EMAILS"].add(
+            if ALERT_DETAILS[log]["TARGET_USER_EMAILS"] != ["<UNKNOWN_USER>"]:
+                ALERT_DETAILS[log]["TARGET_USER_EMAILS"].append(
                     param_lookup(details.get("parameters", {}), "target_user")
                 )
             else:
-                ALERT_DETAILS[log]["TARGET_USER_EMAILS"] = {
+                ALERT_DETAILS[log]["TARGET_USER_EMAILS"] = [
                     param_lookup(details.get("parameters", {}), "target_user")
-                }
-            ALERT_DETAILS[log]["DOC_TITLE"] = param_lookup(
-                details.get("parameters", {}), "doc_title"
-            )
-            ALERT_DETAILS[log]["ACCESS_SCOPE"] = param_lookup(
-                details.get("parameters", {}), "new_value"
-            )
+                ]
+                ALERT_DETAILS[log]["DOC_TITLE"] = param_lookup(
+                    details.get("parameters", {}), "doc_title"
+                )
+                ALERT_DETAILS[log]["ACCESS_SCOPE"] = param_lookup(
+                    details.get("parameters", {}), "new_value"
+                )
 
             change_user_access = True
 
-    if change_user_access:
-        return True
-
-    return False
+    return change_user_access
 
 
 def alert_context(event):
     log = event.get("p_row_id")
-    return {"target users": list(ALERT_DETAILS[log]["TARGET_USER_EMAILS"])}
+    if ALERT_DETAILS[log]["TARGET_USER_EMAILS"] != ["<UNKNOWN_USER>"]:
+        return {"target users": ALERT_DETAILS[log]["TARGET_USER_EMAILS"]}
+    return {}
 
 
 def dedup(event):
@@ -126,9 +142,9 @@ def dedup(event):
 
 def title(event):
     log = event.get("p_row_id")
-    if ALERT_DETAILS[log]["TARGET_USER_EMAILS"] != {"<UNKNOWN_USER>"}:
+    if ALERT_DETAILS[log]["TARGET_USER_EMAILS"] != ["<UNKNOWN_USER>"]:
         if len(ALERT_DETAILS[log]["TARGET_USER_EMAILS"]) == 1:
-            sharing_scope = ALERT_DETAILS[log]["TARGET_USER_EMAILS"]
+            sharing_scope = ALERT_DETAILS[log]["TARGET_USER_EMAILS"][0]
         else:
             sharing_scope = "multiple users"
         if ALERT_DETAILS[log]["NEW_VISIBILITY"] == "shared_externally":
@@ -140,7 +156,7 @@ def title(event):
         elif ALERT_DETAILS[log]["NEW_VISIBILITY"] == "public_on_the_web":
             sharing_scope += " (link not required)"
     else:
-        sharing_scope = f"the {ALERT_DETAILS['TARGET_DOMAIN']} domain"
+        sharing_scope = f"the {ALERT_DETAILS[log]['TARGET_DOMAIN']} domain"
         if ALERT_DETAILS[log]["NEW_VISIBILITY"] == "people_within_domain_with_link":
             sharing_scope += f" (anyone in {ALERT_DETAILS['TARGET_DOMAIN']} with the link)"
         elif ALERT_DETAILS[log]["NEW_VISIBILITY"] == "public_in_the_domain":
