ff766dc6f46099312c7ffe1703bbc72e3f98729b
Ben Sebastian <ben.sebastian@protonmail.com>
2021-08-30 13:25:25 -0400

    More testing fixes and feedback

--- indicator_creation_rules/new_account_logging.py
+++ indicator_creation_rules/new_account_logging.py
@@ -1,30 +1,27 @@
-from datetime import (
-    datetime,
-    timedelta,
-)
+from datetime import timedelta
+
 from panther_oss_helpers import (
     put_string_set,
     set_key_expiration,
-    check_new_user,
+    resolve_timestamp_string,
 )
 import panther_event_type_helpers as event_type
 
 # Days an account is considered new
 TTL = timedelta(days=3)
-PANTHER_TIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
 
 
 def rule(event):
-
     if event.udm("event_type") != event_type.ACCOUNT_CREATED:
         return False
 
     event_id = f'new_user_{event.get("p_row_id")}'
     new_user = event.udm("user")
-    event_time = datetime.strptime(event.get("p_event_time"), PANTHER_TIME_FORMAT)
+    event_time = resolve_timestamp_string(event.get("p_event_time"))
     expiry_time = event_time + TTL
 
-    put_string_set(new_user, event_id)
+    put_string_set(new_user, [
+        event_id])
 
     set_key_expiration(new_user, expiry_time.strftime("%s"))
     return True
