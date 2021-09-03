77e7b56a451f71786488d153951a00f9bf7c79e5
Ben Sebastian <ben.sebastian@protonmail.com>
2021-09-03 10:40:37 -0400

    Format Newly Ported Rules

--- gsuite_activityevent_rules/gsuite_low_severity_rule.py
+++ gsuite_activityevent_rules/gsuite_low_severity_rule.py
@@ -1,11 +1,12 @@
 from panther_base_helpers import deep_get
 
+
 def rule(event):
 
     if deep_get(event, "id", "applicationName") != "rules":
         return False
 
-    if not(
+    if not (
         deep_get(event, "parameters", "triggered_actions")
         and deep_get(event, "parameters", "severity") == "LOW"
     ):
--- gsuite_activityevent_rules/gsuite_medium_severity_rule.py
+++ gsuite_activityevent_rules/gsuite_medium_severity_rule.py
@@ -5,7 +5,7 @@ def rule(event):
     if deep_get(event, "id", "applicationName") != "rules":
         return False
 
-    if not(
+    if not (
         deep_get(event, "parameters", "triggered_actions")
         and deep_get(event, "parameters", "severity") == "MEDIUM"
     ):
