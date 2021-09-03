e0d0638be276dd81ffc4d334a46945cf5fc4a9dd
Ben Sebastian <ben.sebastian@protonmail.com>
2021-09-03 10:21:57 -0400

    Port Rules to Gsuite-ActivityEvent

---
+++ gsuite_activityevent_rules/gsuite_high_severity_rule.py
@@ -0,0 +1,13 @@
+from panther_base_helpers import deep_get
+
+def rule(event):
+
+    if deep_get(event, "id", "applicationName") != "rules":
+        return False
+
+    if not(
+        deep_get(event, "parameters", "triggered_actions")
+        and deep_get(event, "parameters", "severity") == "HIGH"
+    ):
+        return False
+    return True
