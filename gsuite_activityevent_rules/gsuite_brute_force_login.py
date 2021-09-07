b354ca2a1ae8e82c41ad310c9892fce5f212124b
Patrick Hagan <56281590+glerb@users.noreply.github.com>
2021-09-06 21:35:57 -0700

    make fmt (#317)

--- gsuite_activityevent_rules/gsuite_brute_force_login.py
+++ gsuite_activityevent_rules/gsuite_brute_force_login.py
@@ -1,12 +1,13 @@
 from panther_base_helpers import deep_get
 
+
 def rule(event):
     # Filter login events
-    if event.get('type') != 'login':
+    if event.get("type") != "login":
         return False
 
     # Pattern match this event to the recon actions
-    return bool(event.get('name') == 'login_failure')
+    return bool(event.get("name") == "login_failure")
 
 
 def title(event):
