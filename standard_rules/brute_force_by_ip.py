f1d3be816fdf03b09ddc80e7704f1962878a329a
Patrick Hagan <56281590+glerb@users.noreply.github.com>
2021-09-06 21:43:36 -0700

    Add geoloc to failed logins title (#313)

--- standard_rules/brute_force_by_ip.py
+++ standard_rules/brute_force_by_ip.py
@@ -8,6 +8,7 @@ AND events[0]:name = 'login_failure' LIMIT 10;
 """
 
 import panther_event_type_helpers as event_type
+from panther_oss_helpers import add_parse_delay, geoinfo_from_ip_formatted
 
 
 def rule(event):
@@ -19,5 +20,10 @@ def title(event):
     # use unified data model field in title
     return (
         f"{event.get('p_log_type')}: User [{event.udm('actor_user')}] "
-        f"from IP [{event.udm('source_ip')}] has exceeded the failed logins threshold"
+        f"from IP [{geoinfo_from_ip_formatted(event.udm('source_ip'))}] "
+        f"has exceeded the failed logins threshold"
     )
+
+
+def alert_context(event):
+    return add_parse_delay(event, {})
