d9d26ba6e43484990dfe491d1331038674c511db
Ben Sebastian <bseb@users.noreply.github.com>
2021-09-13 11:24:52 -0400

    Replace Black/White list with Allow/Block list (#324)
    * Replace Black/White list with Allow/Block list
    
    * Replace Black/White list with Allow/Block list
    
    * Replace Black/White list with Allow/Block list
    
    * Replace Black/White list with Allow/Block list
    
    Co-authored-by: Ben Sebastian <ben.sebastian@protonmail.com>

--- aws_iam_policies/aws_iam_policy_blocklist.py
+++ aws_iam_policies/aws_iam_policy_blocklist.py
@@ -1,8 +1,8 @@
 # IAM policy ARN's list in this variable will be checked against the policies assigned to all IAM
-# resources (users, groups, roles) and this rule will fail if any policy ARN in the blacklist is
+# resources (users, groups, roles) and this rule will fail if any policy ARN in the blocklist is
 # assigned to a user
-IAM_POLICY_ARN_BLACKLIST = [
-    "TEST_BLACKLISTED_ARN",
+IAM_POLICY_ARN_BLOCKLIST = [
+    "TEST_BLOCKLISTED_ARN",
 ]
 
 
@@ -11,8 +11,8 @@ def policy(resource):
     if resource["ManagedPolicyNames"] is None:
         return True
 
-    # Iterate through the blacklist and return true if the resource is in violation
-    for iam_policy in IAM_POLICY_ARN_BLACKLIST:
+    # Iterate through the blocklist and return true if the resource is in violation
+    for iam_policy in IAM_POLICY_ARN_BLOCKLIST:
         if iam_policy in resource["ManagedPolicyNames"]:
             return False
 
