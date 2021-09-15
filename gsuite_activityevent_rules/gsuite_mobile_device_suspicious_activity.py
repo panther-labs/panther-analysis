32e79ede775e3661007959bc79a5fb1c7703d9a9
Ben Sebastian <ben.sebastian@protonmail.com>
2021-09-15 17:45:41 -0400

    aPort rules to Gsuite Activity Event

--- gsuite_activityevent_rules/gsuite_mobile_device_suspicious_activity.py
+++ gsuite_activityevent_rules/gsuite_mobile_device_suspicious_activity.py
@@ -1,12 +1,11 @@
 from panther_base_helpers import deep_get
-from panther_base_helpers import gsuite_details_lookup as details_lookup
 
 
 def rule(event):
     if deep_get(event, "id", "applicationName") != "mobile":
         return False
 
-    return bool(details_lookup("suspicious_activity", ["SUSPICIOUS_ACTIVITY_EVENT"], event))
+    return bool(event.get("name") == "SUSPICIOUS_ACTIVITY_EVENT")
 
 
 def title(event):
