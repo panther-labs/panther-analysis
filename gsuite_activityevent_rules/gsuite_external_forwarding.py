a04e7f9b2ea2897891f5d68cfe9bb765c35ab188
Ben Sebastian <ben.sebastian@protonmail.com>
2021-09-20 17:55:23 -0400

    Create rule for external email forwarding

---
+++ gsuite_activityevent_rules/gsuite_external_forwarding.py
@@ -0,0 +1,22 @@
+from panther_base_helpers import deep_get
+
+ALLOWED_DOMAINS = ["example.com"]  # List of external domains that are allowed to be forwarded to
+
+
+def rule(event):
+    if deep_get(event, "id", "applicationName") != "user_accounts":
+        return False
+
+    if event.get("name") == "email_forwarding_out_of_domain":
+        domain = deep_get(event, "parameters", "email_forwarding_destination_address").split("@")[1]
+        if domain not in ALLOWED_DOMAINS:
+            return True
+
+    return False
+
+
+def title(event):
+    external_address = deep_get(event, "parameters", "email_forwarding_destination_address")
+    user = deep_get(event, "actor", "email")
+
+    return f"An email forwarding rule was created by {user} to {external_address}"
