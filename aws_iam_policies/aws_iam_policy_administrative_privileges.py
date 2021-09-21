845d46c102e84a206a9c60207d9ee7a9975f8fdd
lindsey-w <lindsey.whitehurst@runpanther.io>
2021-09-21 11:43:32 -0700

    fmt

--- aws_iam_policies/aws_iam_policy_administrative_privileges.py
+++ aws_iam_policies/aws_iam_policy_administrative_privileges.py
@@ -1,5 +1,7 @@
 import json
+
 from panther_oss_helpers import listify
+
 
 def policy(resource):
     iam_policy = json.loads(resource["PolicyDocument"])
@@ -7,7 +9,6 @@ def policy(resource):
     for state in statements:
         actions = listify(state.get("Action", []))
         resources = listify(state.get("Resource", []))
-
         if state["Effect"] == "Allow" and "*" in actions and "*" in resources:
             return False
     return True
