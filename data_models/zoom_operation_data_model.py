a170af95ab4f549ff7b0c3c1be4f09905df37d7a
Ben Sebastian <ben.sebastian@protonmail.com>
2021-11-24 15:10:46 -0500

    Add admin role to data model

--- data_models/zoom_operation_data_model.py
+++ data_models/zoom_operation_data_model.py
@@ -3,11 +3,14 @@ import panther_event_type_helpers as event_type
 
 def get_event_type(event):
     # pylint: disable=too-many-return-statements
+    # pylint: disable=too-many-branches
     if event.get("category_type") == "User":
         if event.get("action") == "Add":
             return event_type.USER_ACCOUNT_CREATED
         if event.get("action") == "Delete":
             return event_type.USER_ACCOUNT_DELETED
+        if event.get("action") == "Update" and "to Admin" in event.get("operation_detail"):
+            return event_type.ADMIN_ROLE_ASSIGNED
         if event.get("action") == "Update":
             return event_type.USER_ACCOUNT_MODIFIED
 
