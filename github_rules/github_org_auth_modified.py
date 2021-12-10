d7f42f739503648504dd93874baca09328fda18c
Ben Sebastian <ben.sebastian@protonmail.com>
2021-12-10 14:55:07 -0500

    New GitHub Org Rules

---
+++ github_rules/github_org_auth_modified.py
@@ -0,0 +1,19 @@
+def rule(event):
+    auth_change_events = [
+        "org.saml_disabled",
+        "org.saml_enabled",
+        "org.disable_two_factor_requirement",
+        "org.enable_two_factor_requirement",
+        "org.update_saml_provider_settings",
+        "org.enable_oauth_app_restrictions",
+        "org.disable_oauth_app_restrictions",
+    ]
+
+    if not event.get("action").startswith("org."):
+        return False
+
+    return event.get("action") in auth_change_events
+
+
+def title(event):
+    return f"GitHub auth configuration was changed by {event.get('actor', '<UNKNOWN USER')}"
---
+++ github_rules/github_org_ip_allowlist.py
@@ -0,0 +1,18 @@
+def rule(event):
+    if not event.get("action").startswith("ip_allow_list"):
+        return False
+
+    allowlist_actions = [
+        "ip_allow_list.enable",
+        "ip_allow_list.disable",
+        "ip_allow_list.enable_for_installed_apps",
+        "ip_allow_list.disable_for_installed_apps",
+        "ip_allow_list_entry.create",
+        "ip_allow_list_entry.update",
+        "ip_allow_list_entry.destroy",
+    ]
+    return event.get("action") in allowlist_actions
+
+
+def title(event):
+    return f"GitHub Org IP Allow list modified by {event.get('actor')}."
