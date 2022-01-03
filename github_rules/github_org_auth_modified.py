71bdd347fb26689414e4a749158b2e799bc4b946
Ben Sebastian <ben.sebastian@protonmail.com>
2022-01-03 16:51:29 -0500

    Address PR Feedback

--- github_rules/github_org_auth_modified.py
+++ github_rules/github_org_auth_modified.py
@@ -1,18 +1,20 @@
+AUTH_CHANGE_EVENTS = [
+    "org.saml_disabled",
+    "org.saml_enabled",
+    "org.disable_two_factor_requirement",
+    "org.enable_two_factor_requirement",
+    "org.update_saml_provider_settings",
+    "org.enable_oauth_app_restrictions",
+    "org.disable_oauth_app_restrictions",
+]
+
+
 def rule(event):
-    auth_change_events = [
-        "org.saml_disabled",
-        "org.saml_enabled",
-        "org.disable_two_factor_requirement",
-        "org.enable_two_factor_requirement",
-        "org.update_saml_provider_settings",
-        "org.enable_oauth_app_restrictions",
-        "org.disable_oauth_app_restrictions",
-    ]
 
     if not event.get("action").startswith("org."):
         return False
 
-    return event.get("action") in auth_change_events
+    return event.get("action") in AUTH_CHANGE_EVENTS
 
 
 def title(event):
--- github_rules/github_org_ip_allowlist.py
+++ github_rules/github_org_ip_allowlist.py
@@ -1,14 +1,18 @@
+ALLOWLIST_ACTIONS = [
+    "ip_allow_list.enable",
+    "ip_allow_list.disable",
+    "ip_allow_list.enable_for_installed_apps",
+    "ip_allow_list.disable_for_installed_apps",
+    "ip_allow_list_entry.create",
+    "ip_allow_list_entry.update",
+    "ip_allow_list_entry.destroy",
+]
+
+
 def rule(event):
-    allowlist_actions = [
-        "ip_allow_list.enable",
-        "ip_allow_list.disable",
-        "ip_allow_list.enable_for_installed_apps",
-        "ip_allow_list.disable_for_installed_apps",
-        "ip_allow_list_entry.create",
-        "ip_allow_list_entry.update",
-        "ip_allow_list_entry.destroy",
-    ]
-    return event.get("action").startswith("ip_allow_list") and event.get("action") in allowlist_actions
+    return (
+        event.get("action").startswith("ip_allow_list") and event.get("action") in ALLOWLIST_ACTIONS
+    )
 
 
 def title(event):
