f6bae85763f8826569be6addf81f956af0aaf9a8
Ben Sebastian <ben.sebastian@protonmail.com>
2022-01-03 17:22:14 -0500

    Fix Syntax and update PAT

--- data_models/atlassian_data_model.py
+++ data_models/atlassian_data_model.py
@@ -1,7 +1,7 @@
 import panther_event_type_helpers as event_type
 from panther_base_helpers import deep_get
 
- auditLogTypeMap = {
+audit_log_type_map = {
     "user_login": event_type.SUCCESSFUL_LOGIN,
     "user_logout": event_type.SUCCESSFUL_LOGOUT,
     "user_created": event_type.USER_ACCOUNT_CREATED,
@@ -9,16 +9,16 @@ from panther_base_helpers import deep_get
     "group_created": event_type.USER_GROUP_CREATED,
     "group_deleted": event_type.USER_GROUP_DELETED,
     "user_granted_role": event_type.USER_ROLE_MODIFIED,
-    "user_revoked_role": event_type.USER_ROLE_DELETED   
+    "user_revoked_role": event_type.USER_ROLE_DELETED
 }
 
 def get_event_type(event):
-    auditLogType = deep_get(event, "AuditLog", "Type")
-    matched = auditLogTypeMap.get(auditLogType)
+    audit_log_type = deep_get(event, "AuditLog", "Type")
+    matched = audit_log_type_map.get(audit_log_type)
     if matched is not None:
         return matched
 
-    if auditLogType in ("added_org_admin", "group_granted_admin_access"):
+    if audit_log_type in ("added_org_admin", "group_granted_admin_access"):
         return event_type.ADMIN_ROLE_ASSIGNED
 
-    return None 
+    return None
