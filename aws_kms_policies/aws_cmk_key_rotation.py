3688be6a3b243bc986161a600d19e3649d2d59eb
Ben Sebastian <ben.sebastian@protonmail.com>
2022-02-01 14:17:07 -0500

    Only enforce policy on AWS managed keys

--- aws_kms_policies/aws_cmk_key_rotation.py
+++ aws_kms_policies/aws_cmk_key_rotation.py
@@ -1,9 +1,13 @@
 def policy(resource):
+    # per AWS Docs automatic rotation is only supported on managed symmetric keys
+    # These are of Origin AWS_KMS
+    if resource.get("Origin") != "AWS_KMS":
+        return True
     return (  # Ignore AWS managed keys
-            resource.get("KeyManager") != "CUSTOMER" # Check that the KeyRotation exists
-            # Explicit True check to avoid returning NoneType
-            or (resource.get("KeyRotationEnabled") is True and resource.get(
-                "KeyState") == "Enabled"))
+        resource.get("KeyManager") != "CUSTOMER"  # Check that the KeyRotation exists
+        # Explicit True check to avoid returning NoneType
+        or (resource.get("KeyRotationEnabled") is True and resource.get("KeyState") == "Enabled")
+    )
 
 
 def dedup(resource):
