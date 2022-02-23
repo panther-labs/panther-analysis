0d46afe11973f2b7f728520ff99a0095c8641572
Ben Sebastian <ben.sebastian@protonmail.com>
2022-02-23 17:50:20 -0500

    Add debugging to test

--- greynoise_test_rules/greynoise_noise_test.py
+++ greynoise_test_rules/greynoise_noise_test.py
@@ -20,3 +20,28 @@ def rule(event):
         and basic_noise.actor("srcAddr") == "unknown"
         and basic_noise.url("srcAddr") == "www.greynoise.io/viz/ip/4.4.4.4"
     )
+
+def alert_context(event):
+    # if our test returns False this will print out a dict of all the items tested and their values
+    advanced_noise = GreyNoiseAdvanced(event)
+    basic_noise = GreyNoiseBasic(event)
+
+    context={
+        "advanced": {},
+        "basic": {}
+    }
+    # Build Advanced Output Dictionary
+    context["advanced"]["ip_address"] = advanced_noise.ip_address("srcAddr")
+    context["advanced"]["classification"] = advanced_noise.classification("srcAddr") == "malicious"
+    context["advanced"]["actor"] = advanced_noise.actor("srcAddr")
+    context["advanced"]["url"] = advanced_noise.url("srcAddr")
+    context["advanced"]["country"] = advanced_noise.metadata("srcAddr").get("country")
+    context["advanced"]["cve_string"] = advanced_noise.cve_string("srcAddr")
+    context["advanced"]["vpn_service"] = advanced_noise.vpn_service("srcAddr")
+    # Build Basic Output Dictionary
+    context["basic"]["ip_address"] = basic_noise.ip_address("srcAddr")
+    context["basic"]["classification"] = basic_noise.classification("srcAddr") == "malicious"
+    context["basic"]["actor"] = basic_noise.actor("srcAddr")
+    context["basic"]["url"] = basic_noise.url("srcAddr")
+
+    return context
