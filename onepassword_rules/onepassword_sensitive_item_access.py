fc8b28cf859be0bbba923eddae09f8d398dd9de4
Ben Sebastian <ben.sebastian@protonmail.com>
2022-02-23 16:21:07 -0500

    1Password specific detections

---
+++ onepassword_rules/onepassword_sensitive_item_access.py
@@ -0,0 +1,28 @@
+"""
+This rule detects access to high sensitivity items in your 1Password account. 1Password references
+these items by their UUID so the SENSITIVE_ITEM_WATCHLIST below allows for the mapping of UUID to
+meaningful name.
+"""
+
+from panther_base_helpers import deep_get
+
+SENSITIVE_ITEM_WATCHLIST = {"ecd1d435c26440dc930ddfbbef201a11": "demo_item"}
+
+
+def rule(event):
+    return event.get("item_uuid") in SENSITIVE_ITEM_WATCHLIST.keys()
+
+
+def title(event):
+    return f"A Sensitive 1Password Item was Accessed by user {deep_get(event,'user', 'name')}"
+
+
+def alert_context(event):
+    context = {}
+    context["user"] = deep_get(event, "user", "name")
+    context["item_name"] = SENSITIVE_ITEM_WATCHLIST.get(event.get("item_uuid"))
+    context["client"] = deep_get(event, "client", "app_name")
+    context["ip_address"] = event.udm("source_ip")
+    context["event_time"] = event.get("timestamp")
+
+    return context
