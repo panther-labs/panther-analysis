06e2d4a8c8f88cbd366d104c4b57ebf4bcfcd740
Ben Sebastian <bseb@users.noreply.github.com>
2022-03-21 10:03:55 -0700

    Tune Amazon SSOv2 False Postives) (#389)
    * Tune Amazon SSOv2 False Postives)
    
    * lint/fmt

--- aws_cloudtrail_rules/aws_console_login_without_mfa.py
+++ aws_cloudtrail_rules/aws_console_login_without_mfa.py
@@ -10,9 +10,16 @@ def rule(event):
     if event.get("eventName") != "ConsoleLogin":
         return False
 
+    # Extract some nested JSON structure
     additional_event_data = event.get("additionalEventData", {})
-    session_context = deep_get(event, "userIdentity", "sessionContext", default={})
     response_elements = event.get("responseElements", {})
+
+    # If using AWS SSOv2 or other SAML provider return False
+    if (
+        "AWSReservedSSO" in deep_get(event, "userIdentity", "arn")
+        or additional_event_data.get("SamlProviderArn") is not None
+    ):
+        return False
 
     # If Account is less than 3 days old do not alert
     # This functionality is not enabled by default, in order to start logging new user creations
@@ -37,18 +44,8 @@ def rule(event):
         return False
 
     return (
-        # Only alert on successful logins
         response_elements.get("ConsoleLogin") == "Success"
-        and
-        # Where MFA is not in use
-        additional_event_data.get("MFAUsed") == "No"
-        and
-        # Ignoring SSO login events
-        not additional_event_data.get("SamlProviderArn")
-        and
-        # And ignoring logins that were authenticated via a session that was itself
-        # authenticated with MFA
-        deep_get(session_context, "attributes", "mfaAuthenticated") != "true"
+        and additional_event_data.get("MFAUsed") == "No"
     )
 
 
