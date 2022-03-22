6d0e93d2440bebbd240095a8fe379845cb4fa4cc
Ben Sebastian <ben.sebastian@protonmail.com>
2022-03-22 14:29:54 -0400

    Update Okta datamodel to support mfa disable

--- data_models/okta_data_model.py
+++ data_models/okta_data_model.py
@@ -12,4 +12,10 @@ def get_event_type(event):
         and event.get("outcome", {}).get("result") == "SUCCESS"
     ):
         return event_type.SUCCESSFUL_LOGIN
+    if (
+        event.get("eventType") in ["user.mfa.factor.deactivate", "user.mfa.factor.suspend"]
+    ):
+        return event_type.MFA_DISABLED
+    
+    
     return None
