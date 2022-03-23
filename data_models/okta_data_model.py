91a9ab7e3f4410c2a8cf66e115748f70fbf346d3
Ben Sebastian <ben.sebastian@protonmail.com>
2022-03-23 12:05:12 -0400

    PR Feedback, update event type helper and data model

--- data_models/okta_data_model.py
+++ data_models/okta_data_model.py
@@ -15,4 +15,10 @@ def get_event_type(event):
     if event.get("eventType") in ["user.mfa.factor.deactivate", "user.mfa.factor.suspend"]:
         return event_type.MFA_DISABLED
 
+    if event.get("eventType") in [
+        "user.mfa.factor.activate",
+        "user.mfa.factor.unsuspend",
+        "user.mfa.factor.update ",
+    ]:
+        return event_type.MFA_ENABLED
     return None
