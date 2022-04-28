fa3a8f1fdddc6fec8b7ed360a0fb22d7adae25a3
Ben Sebastian <ben.sebastian@panther.io>
2022-04-28 12:52:04 -0400

    More Rules

--- panther_audit_rules/panther_sensitive_role_created.py
+++ panther_audit_rules/panther_sensitive_role_created.py
@@ -17,7 +17,21 @@ def rule(event):
     if event.udm(event_type) not in PANTHER_ROLE_ACTIONS:
         return False
     role_permissions= set(deep_get(event, 'action_params', 'input', 'permissions'))
-    
+
     return (
-        len(set(PANTHER_ADMIN_PERMISSIONS).intersection(role_permissions)) > 0
+        len(set(PANTHER_ADMIN_PERMISSIONS).intersection(role_permissions)) > 0 and
+        event.get("actionResult") == "SUCCEEDED"
     )
+
+def title(event):
+    return ( 
+        f"Role with Admin Permissions created by {event.udm('actor_user')}"
+        f"Role Name: {deep_get(event, 'actionParams', 'name')}"
+        )
+
+def alert_context(event):
+    return {
+        'user': event.udm('actor_user'),
+        'role_name': deep_get(event, 'actionParams', 'name'),
+        'ip': event.udm("source_ip"),
+    }
