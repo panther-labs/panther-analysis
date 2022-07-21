89f95f878ad1cb38c7117ca66ffa97426053e63a
Kyle Bailey <kyle.o.bailey@gmail.com>
2022-07-21 16:08:50 -0500

    fix: S3 malicious access allow panther roles from AWS

--- aws_cloudtrail_rules/aws_s3_activity_greynoise.py
+++ aws_cloudtrail_rules/aws_s3_activity_greynoise.py
@@ -3,11 +3,21 @@ from ipaddress import ip_address
 from panther_base_helpers import deep_get, pattern_match_list
 from panther_greynoise_helpers import GetGreyNoiseObject, GetGreyNoiseRiotObject
 
+
 # pylint: disable=too-many-return-statements,invalid-name,unused-argument,global-at-module-level,global-variable-undefined
 
 # Monitor for GetObject events from S3.
 # Also check ListBucket to reveal object enumeration.
-_S3_EVENT_LIST = ("ListBucket*", "GetObject*")
+_S3_EVENT_LIST = (
+    "ListBucket*", 
+    "GetObject*",
+)
+
+# Some AWS IP addresses listed as "malicious" are stale, this allows for allowing specific roles where this may occur
+_ALLOWED_ROLES = (
+    "*PantherAuditRole-*",
+    "*PantherLogProcessingRole-*",
+)
 
 
 def rule(event):
@@ -23,7 +33,7 @@ def rule(event):
     # Filter: Non "Get" events
     if not pattern_match_list(event.get("eventName"), _S3_EVENT_LIST):
         return False
-
+    
     # Validate the IP is actually an IP (sometimes it's a string)
     try:
         ip_address(event.get("sourceIPAddress"))
@@ -41,7 +51,18 @@ def rule(event):
 
     # Check that the IP is classified as 'malicious'
     if NOISE.classification("sourceIPAddress") == "malicious":
+
+        # Filter: Roles that generate FP's if used from AWS IP Space
+        if pattern_match_list(deep_get(event, "userIdentity", "arn"), _ALLOWED_ROLES):
+            # Only Greynoise advanced provides AS organization info
+            if NOISE.subscription_level() == 'advanced':
+                if NOISE.organization == 'Amazon.com, Inc.':
+                    return False
+            # return false if the role is seen and we are not able to valide the AS organization
+            else:
+                return False
         return True
+    
     return False
 
 
