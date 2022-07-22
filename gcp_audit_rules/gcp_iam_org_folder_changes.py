6c3791b4aec466867f31bd1d1b08ec610f8dc894
kbroughton <kesten.broughton@gmail.com>
2022-07-22 13:47:44 -0500

    GCP.IAM.OrgFolderPolicyChanges

---
+++ gcp_audit_rules/gcp_iam_org_folder_changes.py
@@ -0,0 +1,27 @@
+from panther_base_helpers import deep_get
+
+def rule(event):
+    # Return True to match the log event and trigger an alert.
+    logname = deep_get(event, "logName")
+    if deep_get(event, "protoPayload", "methodName") == "SetIamPolicy" and \
+     (logname.startswith("organizations") or \
+         logname.startswith("folder") ) and \
+        logname.endswith("/logs/cloudaudit.googleapis.com%2Factivity") and \
+     deep_get(event, "protoPayload", "requestMetadata", "callerSuppliedUserAgent").lower().find('terraform') == -1:
+        return True
+    else:
+        return False
+
+def title(event):
+    # use unified data model field in title
+    return (
+        f"{event.get('p_log_type')}: [{event.udm('actor_user')}] made manual changes to Org policy"
+    )
+
+def alert_context(event):
+    return {
+        "actor": event.udm("actor_user"),
+        "policy_change": deep_get(event,  "protoPayload", "serviceData", "policyDelta"),
+        "caller_ip": deep_get(event, "protoPayload", "requestMetadata", "callerIP"),
+        "user_agent": deep_get(event, "protoPayload", "requestMetadata", "callerSuppliedUserAgent")
+    }
