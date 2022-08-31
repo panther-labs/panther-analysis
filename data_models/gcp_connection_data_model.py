7a50bb91c2f34aed8edc9651078c61db67a6807c
kbroughton <kesten.broughton@gmail.com>
2022-08-31 18:53:13 -0500

    SEC-1003-gcp-connection-data-model

---
+++ data_models/gcp_connection_data_model.py
@@ -0,0 +1,25 @@
+from panther_base_helpers import deep_get
+
+
+def get_resource_tags(event):
+    """
+    Returns a list of resource tags like pod, cluster, namespace
+
+    Suppressions could allow exceptions for if a string matches any tag
+    """
+    resource_tags = {}
+    for path in [
+        ['resource', 'labels', 'subnetwork_name'],
+        ['resource', 'labels', 'subnetwork_id'],
+        ['resource', 'labels', 'gateway_name'],
+        ['resource', 'labels', 'router_id'],
+        ['resource', 'labels', 'project_id'],
+        ['resource', 'labels', 'region'],
+        ['resource', 'labels', 'location'],
+        ['resource', 'type']
+        ]:
+        value = deep_get(event, *path)
+        if value:
+            resource_tags[".".join(path)] = value
+    
+    return resource_tags
