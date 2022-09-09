a901d942358f581652ba3eda7f880269330e9f3c
Kyle Bailey <kyle.o.bailey@gmail.com>
2022-09-09 13:21:43 -0500

    add bypass; fix review comments

--- rules/github_rules/github_advanced_security_change.py
+++ rules/github_rules/github_advanced_security_change.py
@@ -1,16 +1,16 @@
 from panther_base_helpers import github_alert_context
 
-# pylint: disable=line-too-long
 
 # {GitHub Action: Alert Severity}
 ADV_SEC_ACTIONS = {
-    "dependabot_alerts.disable": "CRITICAL",  # An enterprise owner or site administrator disabled Dependabot alerts for all existing repositories.
-    "dependabot_alerts_new_repos.disable": "HIGH",  # An enterprise owner or site administrator disabled Dependabot alerts for all new repositories.
-    "dependabot_security_updates.disable": "CRITICAL",  # An enterprise owner or site administrator disabled Dependabot security updates for all existing repositories.
-    "dependabot_security_updates_new_repos.disable": "HIGH",  # An enterprise owner or site administrator disabled Dependabot security updates for all new repositories.
-    "repository_secret_scanning_push_protection.disable": "HIGH",  # A repository owner or administrator disabled secret scanning for a repository.
-    "secret_scanning.disable": "CRITICAL",  # An organization owner disabled secret scanning for all existing repositories.
-    "secret_scanning_new_repos.disable": "HIGH",  # An organization owner disabled secret scanning for all new repositories.
+    "dependabot_alerts.disable": "CRITICAL",
+    "dependabot_alerts_new_repos.disable": "HIGH",
+    "dependabot_security_updates.disable": "CRITICAL",
+    "dependabot_security_updates_new_repos.disable": "HIGH",
+    "repository_secret_scanning_push_protection.disable": "HIGH",
+    "secret_scanning.disable": "CRITICAL",
+    "secret_scanning_new_repos.disable": "HIGH",
+    "bypass": "MEDIUM",  # Bypass secret scanner push protection for a detected secret.
 }
 
 
@@ -28,4 +28,4 @@ def alert_context(event):
 
 # Use the per action severity configured above
 def severity(event):
-    return ADV_SEC_ACTIONS.get(event.get("action"))
+    return ADV_SEC_ACTIONS.get(event.get("action", ""), "Low")
