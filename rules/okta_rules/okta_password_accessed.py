f17fd0f73872806127dca7b6bb44ded4a8618bf8
Carrie Pascale <carrie.pascale@panther.io>
2022-09-22 19:27:05 -0400

    fixes linting errors, makes formatting changes, small logic edits

--- rules/okta_rules/okta_password_accessed.py
+++ rules/okta_rules/okta_password_accessed.py
@@ -1,39 +1,39 @@
-from panther_base_helpers import deep_get, lst_iter
+from panther_base_helpers import deep_get, get_val_from_list
+
+# pylint: disable=global-variable-undefined
 
 
 def rule(event):
-    global target_user
-    global target_app_names
-    # event['target'] = [{...}, {...}, {...}]
-    target_user = lst_iter(
-        event.get("target", [{}]), 'alternateId', 'Type', 'AppUser')
-    target_app_names = target_app_names = lst_iter(
-        event.get("target", [{}]), 'alternateId', 'Type', 'AppInstance')
+    global TARGET_USERS
+    global TARGET_APP_NAMES
 
-    if (
-        event.get("eventType") != "application.user_membership.show_password"
-        or not event.udm("actor_user")
-    ):
+    if event.get("eventType") != "application.user_membership.show_password":
         return False
 
-    if deep_get(event, 'actor', 'alternateId') not in target_user:
+    # event['target'] = [{...}, {...}, {...}]
+    TARGET_USERS = get_val_from_list(event.get("target", [{}]), "alternateId", "Type", "AppUser")
+    TARGET_APP_NAMES = get_val_from_list(
+        event.get("target", [{}]), "alternateId", "Type", "AppInstance"
+    )
+
+    if deep_get(event, "actor", "alternateId") not in TARGET_USERS:
         return True
     return False
 
 
 def dedup(event):
-    dedup_str = deep_get(event, 'actor', 'alternateId')
+    dedup_str = deep_get(event, "actor", "alternateId")
 
-    if target_user:
-        dedup_str += ":" + str(target_user)
-    if target_app_names:
-        dedup_str += ":" + str(target_app_names)
-    return dedup_str or ''
+    if TARGET_USERS:
+        dedup_str += ":" + str(TARGET_USERS)
+    if TARGET_APP_NAMES:
+        dedup_str += ":" + str(TARGET_APP_NAMES)
+    return dedup_str or ""
 
 
 def title(event):
     return (
         f"A user {deep_get(event, 'actor', 'alternateId')} accessed another user's "
-        f"{target_user} "
-        f"{target_app_names} password"
+        f"{TARGET_USERS} "
+        f"{TARGET_APP_NAMES} password"
     )
