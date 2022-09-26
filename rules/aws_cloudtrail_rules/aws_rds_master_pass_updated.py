d1348789a712d290cc25a0f8de117691782e846f
Jack Naglieri <jack@panther.io>
2022-09-26 11:10:10 -0700

    fmt

--- rules/aws_cloudtrail_rules/aws_rds_master_pass_updated.py
+++ rules/aws_cloudtrail_rules/aws_rds_master_pass_updated.py
@@ -2,11 +2,17 @@ from panther_base_helpers import deep_get
 
 
 def rule(event):
-    return (event.get('eventName') == 'ModifyDBInstance' and
-            event.get('eventSource') == 'rds.amazonaws.com' and
-            bool(deep_get(event, 'responseElements', 'pendingModifiedValues', 'masterUserPassword')))
+    master_user_pass = deep_get(
+        event, "responseElements", "pendingModifiedValues", "masterUserPassword"
+    )
+    return (
+        event.get("eventName") == "ModifyDBInstance"
+        and event.get("eventSource") == "rds.amazonaws.com"
+        and bool(master_user_pass)
+    )
 
 
 def title(event):
-    return f"RDS Master Password Updated on [{deep_get(event, 'responseElements', 'dBInstanceArn')}]"
-
+    return (
+        f"RDS Master Password Updated on [{deep_get(event, 'responseElements', 'dBInstanceArn')}]"
+    )
