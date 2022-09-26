2289d0d4d6190010c462256239fa2563eba5e40e
Jack Naglieri <jack@panther.io>
2022-09-26 12:16:52 -0700

    [pr feedback] simplify rule and move name attr up

--- rules/aws_cloudtrail_rules/aws_rds_master_pass_updated.py
+++ rules/aws_cloudtrail_rules/aws_rds_master_pass_updated.py
@@ -2,13 +2,10 @@ from panther_base_helpers import deep_get
 
 
 def rule(event):
-    master_user_pass = deep_get(
-        event, "responseElements", "pendingModifiedValues", "masterUserPassword"
-    )
     return (
         event.get("eventName") == "ModifyDBInstance"
         and event.get("eventSource") == "rds.amazonaws.com"
-        and bool(master_user_pass)
+        and bool(deep_get(event, "responseElements", "pendingModifiedValues", "masterUserPassword"))
     )
 
 
