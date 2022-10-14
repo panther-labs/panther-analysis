67c5cf96f1ae3d584783b59b93994308c7fa4752
Ed Anderson <ed.anderson@panther.io>
2022-10-13 18:27:18 -0700

    feat: tweak the aws modify cloud compute infra rule to limit false-positives

--- rules/aws_cloudtrail_rules/aws_modify_cloud_compute_infrastructure.py
+++ rules/aws_cloudtrail_rules/aws_modify_cloud_compute_infrastructure.py
@@ -1,17 +1,78 @@
 from panther_base_helpers import deep_get
 
+EC2_CRUD_ACTIONS = {
+    "AssociateIamInstanceProfile",
+    "AssociateInstanceEventWindow",
+    "BundleInstance",
+    "CancelSpotInstanceRequests",
+    "ConfirmProductInstance",
+    "CreateInstanceEventWindow",
+    "CreateInstanceExportTask",
+    "DeleteInstanceEventWindow",
+    "DeregisterInstanceEventNotificationAttributes",
+    "DisassociateIamInstanceProfile",
+    "DisassociateInstanceEventWindow",
+    "ImportInstance",
+    "ModifyInstanceAttribute",
+    "ModifyInstanceCapacityReservationAttributes",
+    "ModifyInstanceCreditSpecification",
+    "ModifyInstanceEventStartTime",
+    "ModifyInstanceEventWindow",
+    "ModifyInstanceMaintenanceOptions",
+    "ModifyInstanceMetadataOptions",
+    "ModifyInstancePlacement",
+    "MonitorInstances",
+    "RegisterInstanceEventNotificationAttributes",
+    "ReportInstanceStatus",
+    "RequestSpotInstances",
+    "ResetInstanceAttribute",
+    "RunInstances",
+    "RunScheduledInstances",
+    "StartInstances",
+    "StopInstances",
+    "TerminateInstances",
+    "UnmonitorInstances",
+}
+
+
 def rule(event):
-    return event.get('eventName') == 'TerminateInstances'
+    # Disqualify any eventSource that is not ec2
+    if event.get("eventSource", "") != "ec2.amazonaws.com":
+        return False
+    # Disqualify AWS Service-Service operations, which can appear in a variety of forms
+    if (
+        # Web-Console initiated EC2:* events have a sourceIPAddress of 'AWS Internal'
+        #event.get("sourceIPAddress", "") == "AWS Internal"
+        event.get("sourceIPAddress", "").endswith(".amazonaws.com")
+        or deep_get(event, "userIdentity", "type", default="") == "AWSService"
+        or deep_get("userIdentity", "invokedBy", default="") == 'AWS Internal'
+        or deep_get("userIdentity", "invokedBy", default="").endswith(".amazonaws.com")
+    ):
+        return False
+    # Dry run operations get logged as SES Internal in the sourceIPAddress
+    #  but not in the invokedBy field
+    if event.get("errorCode", "") == "Client.DryRunOperation":
+        return False
+    # Disqualify any eventNames that do not Include instance
+    # and events that have readOnly set to false
+    if event.get("eventName", "") in EC2_CRUD_ACTIONS:
+        return True
+    return False
+
 
 def title(event):
-    items = deep_get(event, 'requestParameters', 'instancesSet', 'items')
-    return f" AWS Event [{event.get('eventName')}] Instance ID [{items[0].get('instanceId')}] AWS Account ID [{event.get('recipientAccountId')}]"
+    items = deep_get(event, "requestParameters", "instancesSet", "items")
+    return (
+        f"AWS Event [{event.get('eventName')}] Instance ID "
+        f"[{items[0].get('instanceId')}] AWS Account ID [{event.get('recipientAccountId')}]"
+    )
+
 
 def alert_context(event):
-    items = deep_get(event, 'requestParameters', 'instancesSet', 'items')
+    items = deep_get(event, "requestParameters", "instancesSet", "items")
     return {
-        "awsRegion": event.get('awsRegion'),
-        "eventName": event.get('eventName'),
-        "recipientAccountId": event.get('recipientAccountId'),
-        "instanceId": items[0].get('instanceId')
+        "awsRegion": event.get("awsRegion"),
+        "eventName": event.get("eventName"),
+        "recipientAccountId": event.get("recipientAccountId"),
+        "instanceId": items[0].get("instanceId"),
     }
