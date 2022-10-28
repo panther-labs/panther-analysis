6eeab7d471dae0503e5cf746455fa08d9b84ed02
Joe Stanulis <46797963+jstan12@users.noreply.github.com>
2022-10-28 14:14:34 -0400

    Update aws_ec2_monitoring.py

--- rules/aws_cloudtrail_rules/aws_ec2_monitoring.py
+++ rules/aws_cloudtrail_rules/aws_ec2_monitoring.py
@@ -1,39 +1,37 @@
 from panther_base_helpers import deep_get
 
 # AWS CloudTrail API eventNames for EC2 Instance Creation or Start
-ec2_instance = [
-    'RunInstances',
-    'RunScheduledInstances',
-    'StartInstances'
-]
+ec2_instance = ["RunInstances", "RunScheduledInstances", "StartInstances"]
 
 # AWS CloudTrail API eventNames for EC2 Image Creation
 ec2_image = [
-    'CopyFpgaImage',
-    'CopyImage',
-    'CreateFpgaImage',
-    'CreateImage',
-    'CreateRestoreImageTask',
-    'CreateStoreImageTask',
-    'ImportImage'
+    "CopyFpgaImage",
+    "CopyImage",
+    "CreateFpgaImage",
+    "CreateImage",
+    "CreateRestoreImageTask",
+    "CreateStoreImageTask",
+    "ImportImage",
 ]
+
 
 def rule(event):
 
     # Exclude automation and dry run operations
-    if deep_get(event, 'userIdentity', 'invokedBy') == 'autoscaling.amazonaws.com':
+    if deep_get(event, "userIdentity", "invokedBy") == "autoscaling.amazonaws.com":
         return False
-    
-    if event.get('errorCode') == 'Client.DryRunOperation':
+
+    if event.get("errorCode") == "Client.DryRunOperation":
         return False
 
     # Check for Instance Creation or Start
-    if event.get('eventName') in ec2_instance:
+    if event.get("eventName") in ec2_instance:
         return True
 
     # Check for Image Creation
-    if event.get('eventName') in ec2_image:
+    if event.get("eventName") in ec2_image:
         return True
+
 
 def title(event):
     return f"{deep_get(event, 'userIdentity', 'sessionContext', 'sessionIssuer', 'userName')} triggered a [{event.get('eventName')}] event within AWS Account ID: [{event.get('recipientAccountId')}]"
