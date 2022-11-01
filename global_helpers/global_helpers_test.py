c252eed68df39ac40a81d13964c19f4eb0fb2294
Ed⁦ <ed.anderson@panther.io>
2022-11-01 14:05:59 -0700

    fix: get_val_from_list would raise an error if the comparison key was not in the input item of the list (#547)
    * fix: get_val_from_list would raise an error if the comparison key was not in the input item of the list

--- global_helpers/global_helpers_test.py
+++ global_helpers/global_helpers_test.py
@@ -14,6 +14,29 @@ import panther_base_helpers as p_b_h  # pylint: disable=C0413
 import panther_tor_helpers as p_tor_h  # pylint: disable=C0413
 
 
+class TestGetValFromList(unittest.TestCase):
+    def setUp(self):
+        self.input = [
+            {"actor": 1, "one": 1, "select": "me"},
+            {"actor": 2, "two": 2, "select": "me"},
+            {"actor": 3, "three": 3, "select": "not_me"},
+        ]
+
+    def test_input_key_exists(self):
+        response = p_b_h.get_val_from_list(self.input, "actor", "select", "me")
+        should_be = set()
+        should_be.add(1)
+        should_be.add(2)
+        self.assertCountEqual(response, should_be)
+
+    # This test case validate that get_val_from_list will return the
+    # empty set when the comparison key is not found
+    def test_input_notdict(self):
+        response = p_b_h.get_val_from_list(self.input, "actor", "doesnotexist", "noExceptionRaised")
+        should_be = set()
+        self.assertCountEqual(response, should_be)
+
+
 class TestBoxParseAdditionalDetails(unittest.TestCase):
     def setUp(self):
         self.initial_dict = {"t": 10, "a": [{"b": 1, "c": 2}], "d": {"e": {"f": True}}}
