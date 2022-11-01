b28ce0931cc62c33a3a019d25ccb76292e43c99c
nkulig <nick.kuligoski@runpanther.io>
2022-11-01 17:33:41 -0400

    Updating EC2 Image Monitoring rule for MITRE TA002:T1204 based on feedback from Ed

--- rules/aws_cloudtrail_rules/aws_ec2_monitoring.py
+++ rules/aws_cloudtrail_rules/aws_ec2_monitoring.py
@@ -1,43 +1,44 @@
 from panther_base_helpers import deep_get
 
-# AWS CloudTrail API eventNames for EC2 Instance Creation or Start
-ec2_instance = ["RunInstances", "RunScheduledInstances", "StartInstances"]
-
-# AWS CloudTrail API eventNames for EC2 Image Creation
-ec2_image = {
-    "CopyFpgaImage",
-    "CopyImage",
-    "CreateFpgaImage",
-    "CreateImage",
-    "CreateRestoreImageTask",
-    "CreateStoreImageTask",
-    "ImportImage",
-}
-
+# AWS CloudTrail API eventNames for EC2 Image Actions
+EC2_IMAGE_ACTIONS = [
+    'CopyFpgaImage',
+    'CopyImage',
+    'CreateFpgaImage',
+    'CreateImage',
+    'CreateRestoreImageTask',
+    'CreateStoreImageTask',
+    'ImportImage'
+]
 
 def rule(event):
 
-    # Exclude automation and dry run operations
-    if deep_get(event, "userIdentity", "invokedBy") == "autoscaling.amazonaws.com":
+    # Disqualify any eventSource that is not ec2
+    if event.get("eventSource", "") != "ec2.amazonaws.com":
         return False
-
-    if event.get("errorCode") == "Client.DryRunOperation":
+    # Disqualify AWS Service-Service operations, which can appear in a variety of forms
+    if (
+        # FYI there is a weird quirk in the sourceIPAddress field of CloudTrail
+        #  events with ec2.amazonaws.com as the source name where users of the
+        #  web-console will have their sourceIPAddress recorded as "AWS Internal"
+        #  though their userIdentity will be more normal.
+        #  Example cloudtrail event in the "Terminate instance From WebUI with assumedRole" test
+        event.get("sourceIPAddress", "").endswith(".amazonaws.com")
+        or deep_get(event, "userIdentity", "type", default="") == "AWSService"
+        or deep_get("userIdentity", "invokedBy", default="") == "AWS Internal"
+        or deep_get("userIdentity", "invokedBy", default="").endswith(".amazonaws.com")
+    ):
         return False
-
-    # Check for Instance Creation or Start
-    if event.get("eventName") in ec2_instance:
-        return True
-
-    # Check for Image Creation
-    if event.get("eventName") in ec2_image:
+    # Dry run operations get logged as SES Internal in the sourceIPAddress
+    #  but not in the invokedBy field
+    if event.get("errorCode", "") == "Client.DryRunOperation":
+        return False
+    # Disqualify any eventNames that do not Include Image Actions
+    # and events that have readOnly set to false
+    if event.get("eventName", "") in EC2_IMAGE_ACTIONS:
         return True
 
     return False
 
-
 def title(event):
-    return (
-        f"{deep_get(event, 'userIdentity', 'sessionContext', 'sessionIssuer', 'userName')} "
-        f"triggered a [{event.get('eventName')}] event within AWS Account ID: "
-        f"[{event.get('recipientAccountId')}]"
-    )
+    return f"[{deep_get(event, 'userIdentity', 'sessionContext', 'sessionIssuer', 'userName')}] triggered a CloudTrail action [{event.get('eventName')}] within AWS Account ID: [{event.get('recipientAccountId')}]"
