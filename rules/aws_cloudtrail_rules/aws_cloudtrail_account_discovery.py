fea6b2db83952a5f4f78fa6d28f24fa7440a99c4
Nicholas Kuligoski <nkulig@Nicholass-MBP.localdomain>
2022-11-28 14:07:23 -0500

    black command to lint

--- rules/aws_cloudtrail_rules/aws_cloudtrail_account_discovery.py
+++ rules/aws_cloudtrail_rules/aws_cloudtrail_account_discovery.py
@@ -1,15 +1,17 @@
-from panther_base_helpers import deep_get 
+from panther_base_helpers import deep_get
 
 DISCOVERY_EVENTS = [
-    'GetAlternateContact', 
-    'GetContactInformation', 
-    'PutAlternateContact', 
-    'PutContactInformation', 
-    'DescribeAccount'
+    "GetAlternateContact",
+    "GetContactInformation",
+    "PutAlternateContact",
+    "PutContactInformation",
+    "DescribeAccount",
 ]
 
+
 def rule(event):
-    return event.get('eventName') in DISCOVERY_EVENTS
+    return event.get("eventName") in DISCOVERY_EVENTS
+
 
 def title(event):
     return f"User [{deep_get(event, 'userIdentity', 'principalId')}] performed a [{event.get('eventName')}] action in AWS account [{event.get('recipientAccountId')}]."
