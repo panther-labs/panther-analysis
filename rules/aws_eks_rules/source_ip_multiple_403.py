6ff77ae088772e353f0be31ae65af117f35ec649
Ed⁦ <ed.anderson@panther.io>
2022-12-01 16:59:50 -0800

    feat: Detections for EKS Audit logs (#571)

---
+++ rules/aws_eks_rules/source_ip_multiple_403.py
@@ -0,0 +1,44 @@
+from ipaddress import ip_address
+
+from panther_base_helpers import eks_panther_obj_ref
+
+
+# Alert if
+#   state is ResponseComplete
+#   sourceIPs[0] is a Public Address
+#   responseStatus:code == 403
+def rule(event):
+    if event.get("stage", "") != "ResponseComplete":
+        return False
+    # We include only 403
+    if event.get("responseStatus", {}).get("code", 0) != 403:
+        return False
+    p_eks = eks_panther_obj_ref(event)
+    if ip_address(p_eks.get("sourceIPs")[0]).is_private:
+        return False
+    return True
+
+
+# If not defined, defaults to the rule display name or rule ID.
+def title(event):
+    p_eks = eks_panther_obj_ref(event)
+    return (
+        f"[{p_eks.get('sourceIPs')[0]}] received [403] "
+        f"when executing [{p_eks.get('verb')}] "
+        f"for resource [{p_eks.get('resource')}] "
+        f"in ns [{p_eks.get('ns')}] on "
+        f"[{p_eks.get('p_source_label')}] as "
+        f"[{p_eks.get('actor')}]"
+    )
+
+
+def dedup(event):
+    p_eks = eks_panther_obj_ref(event)
+    return f"{p_eks.get('p_source_label')}_403_{p_eks.get('sourceIPs')[0]}"
+
+
+def alert_context(event):
+    p_eks = eks_panther_obj_ref(event)
+    mutable_event = event.to_dict()
+    mutable_event["p_eks"] = p_eks
+    return dict(mutable_event)
