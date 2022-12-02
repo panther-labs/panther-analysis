d97417fde3a9db264546855ceb91be720130cc2d
jpl5280 <joren.mcreynolds@runpanther.io>
2022-12-02 16:58:49 -0700

    lint warnings

--- rules/github_rules/github_secret_scanning_alert_created.py
+++ rules/github_rules/github_secret_scanning_alert_created.py
@@ -10,5 +10,6 @@ def alert_context(event):
     return {
         'repo': event.get('repo'),
         'alert #': event.get('number'),
-        'url': f"https://github.com/{event.get('repo')}/security/secret-scanning/{event.get('number')}",
+        'url': f"https://github.com/{event.get('repo')}/security/secret-scanning/"
+               f"{event.get('number')}",
     }
