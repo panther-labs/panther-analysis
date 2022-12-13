8adf3ac6de72fbe1410b22afd0319c5c77722525
calkim-panther <113376708+calkim-panther@users.noreply.github.com>
2022-12-13 16:43:48 -0600

    Okta detections pt1 (#586)
    Okta Detections
    
    okta_app_refresh_access_token_reuse.py
    okta_app_unauthorized_access_attempt.py
    okta_group_admin_role_assigned.py
    okta_rate_limits.py

--- global_helpers/panther_base_helpers.py
+++ global_helpers/panther_base_helpers.py
@@ -200,10 +200,17 @@ def box_parse_additional_details(event: dict):
 def okta_alert_context(event: dict):
     """Returns common context for automation of Okta alerts"""
     return {
+        "event_type": event.get("eventtype", ""),
+        "severity": event.get("severity", ""),
+        "actor": event.get("actor", {}),
+        "client": event.get("client", {}),
+        "request": event.get("request", {}),
+        "outcome": event.get("outcome", {}),
+        "target": event.get("target", []),
+        "debug_context": event.get("debugcontext", {}),
+        "authentication_context": event.get("authenticationcontext", {}),
+        "security_context": event.get("securitycontext", {}),
         "ips": event.get("p_any_ip_addresses", []),
-        "actor": event.get("actor", ""),
-        "target": event.get("target", ""),
-        "client": event.get("client", ""),
     }
 
 
