dfa66d664160775d8eda75083e67409eac4838c8
jpl5280 <joren.mcreynolds@runpanther.io>
2022-12-12 21:32:04 -0700

    format

--- rules/duo_rules/duo_user_action_fraudulent.py
+++ rules/duo_rules/duo_user_action_fraudulent.py
@@ -2,16 +2,17 @@ from panther_base_helpers import deep_get
 
 
 def rule(event):
-    return event.get('result') == 'fraud'
+    return event.get("result") == "fraud"
+
 
 def title(event):
-    return 'A DUO action was marked as fraudulent.'
+    return f"A DUO action was marked as fraudulent by {deep_get(event, 'user', 'name')}"
 
 
 def alert_context(event):
     return {
-        "factor": event.get('factor'),
-        "reason": event.get('reason'),
+        "factor": event.get("factor"),
+        "reason": event.get("reason"),
         "user": deep_get(event, "user", "name"),
         "os": deep_get(event, "access_device", "os"),
         "ip_access": deep_get(event, "access_device", "ip"),
