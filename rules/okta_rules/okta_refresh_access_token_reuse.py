9323989c18a04e4baaa2ff8be0a27ea8ab879d64
Calvin Kim <calvin.kim@panther.com>
2022-12-13 12:34:39 -0600

    okta reuse token rule

---
+++ rules/okta_rules/okta_refresh_access_token_reuse.py
@@ -0,0 +1,16 @@
+from panther_base_helpers import okta_alert_context
+
+
+def rule(event):
+    return event.get("eventtype") in (
+        "app.oauth2.as.token.detect_reuse",
+        "app.oauth2.token.detect_reuse",
+    )
+
+
+def title(event):
+    return "Okta Access Token Reuse Attempted by " f"[{event.get('client', {}).get('ipAddress')}]"
+
+
+def alert_context(event):
+    return okta_alert_context(event)
