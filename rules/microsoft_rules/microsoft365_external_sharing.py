6a0215708555387b85dc3d805a53e2acda7d167e
calkim-panther <113376708+calkim-panther@users.noreply.github.com>
2022-12-13 16:39:05 -0600

    Msf365 detections (#583)
    msft 365 detections: external forwarding, disabling mfa, brute force

---
+++ rules/microsoft_rules/microsoft365_external_sharing.py
@@ -0,0 +1,46 @@
+import re
+from fnmatch import fnmatch
+
+from panther_base_helpers import m365_alert_context
+
+email_regex = re.compile(r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+")
+
+ALLOWED_DOMAINS = ["mycompany.com", "alloweddomain.com"]  # should be in lowercase
+
+ALLOWED_USERS = ["exception@outsider.com"]  # should be in lowercase
+
+ALLOWED_PATHS = ["*/External/*", "External/*"]
+
+
+def allowed_path(relative_url):
+    for path in ALLOWED_PATHS:
+        if fnmatch(relative_url, path):
+            return True
+    return False
+
+
+def rule(event):
+    if event.get("Operation", "") == "AnonymousLinkCreated":
+        return not allowed_path(event.get("SourceRelativeUrl"))
+    if event.get("Operation", "") == "AddedToSecureLink":
+        if allowed_path(event.get("SourceRelativeUrl")):
+            return False
+        target = event.get("TargetUserOrGroupName", "")
+        if target.lower() in ALLOWED_USERS:
+            return False
+        if re.fullmatch(email_regex, target):
+            if target.split("@")[1].lower() not in ALLOWED_DOMAINS:
+                return True
+    return False
+
+
+def title(event):
+    return (
+        f"Microsoft365: [{event.get('SourceRelativeUrl')}] "
+        "has been shared with external users by "
+        f"[{event.get('UserId', '<user-not-found>')}]"
+    )
+
+
+def alert_context(event):
+    return m365_alert_context(event)
