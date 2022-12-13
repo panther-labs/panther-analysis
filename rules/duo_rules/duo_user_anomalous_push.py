2fc7508f4edf124093cfe3a061645f7b37850c49
Ryan Doyle <ryan.doyle@panther.com>
2022-12-13 13:46:21 -0800

    Line is barely too long, I think it's readable

--- rules/duo_rules/duo_user_anomalous_push.py
+++ rules/duo_rules/duo_user_anomalous_push.py
@@ -6,6 +6,7 @@ def rule(event):
 
 
 def title(event):
+    # pylint: disable=line-too-long
     return f"DUO Auth denied due to an anomalous 2FA push for {deep_get(event, 'user', 'name', default='<Unknown>')}"
 
 
