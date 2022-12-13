1f5d3531c04124cc4c24038f276c616f2d0ce23a
Calvin Kim <calvin.kim@panther.com>
2022-12-13 14:44:06 -0600

    security threat detection

---
+++ rules/okta_rules/okta_threatinsight_security_threat_detected.py
@@ -0,0 +1,12 @@
+from panther_base_helpers import okta_alert_context
+
+def rule(event):
+    return event.get('eventtype') == 'security.threat.detected'
+
+def title(event):
+    return (
+        f"Okta Security [{event.get('severity', '')}] Threat Detected "
+    )
+
+def alert_context(event):
+    return okta_alert_context(event)
