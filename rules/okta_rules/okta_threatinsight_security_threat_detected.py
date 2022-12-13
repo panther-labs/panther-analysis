4bfbd0d255b03f901d88425cc9b1a130edcd0212
Calvin Kim <calvin.kim@panther.com>
2022-12-13 16:55:14 -0600

    user lock out detection

--- rules/okta_rules/okta_threatinsight_security_threat_detected.py
+++ rules/okta_rules/okta_threatinsight_security_threat_detected.py
@@ -1,12 +1,13 @@
 from panther_base_helpers import okta_alert_context
 
+
 def rule(event):
-    return event.get('eventtype') == 'security.threat.detected'
+    return event.get("eventtype") == "security.threat.detected"
+
 
 def title(event):
-    return (
-        f"Okta Security [{event.get('severity', '')}] Threat Detected "
-    )
+    return f"Okta Security [{event.get('severity', '')}] Threat Detected "
+
 
 def alert_context(event):
     return okta_alert_context(event)
