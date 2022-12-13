9b60af080d42da9502e5a3e4d6bfe64acc3e4478
Ed Anderson <ed.anderson@panther.io>
2022-12-13 12:06:32 -0800

    fix: double negative was not ideal

--- rules/gsuite_activityevent_rules/gsuite_workspace_calendar_external_sharing.py
+++ rules/gsuite_activityevent_rules/gsuite_workspace_calendar_external_sharing.py
@@ -2,10 +2,11 @@ from panther_base_helpers import deep_get
 
 
 def rule(event):
-    if (
-        not event.get("name", "") != "CHANGE_CALENDAR_SETTING"
-        and not deep_get(event, "parameters", "SETTING_NAME", default="")
-        == "SHARING_OUTSIDE_DOMAIN"
+    if not all(
+        [
+            (event.get("name", "") == "CHANGE_CALENDAR_SETTING"),
+            (deep_get(event, "parameters", "SETTING_NAME", default="") == "SHARING_OUTSIDE_DOMAIN"),
+        ]
     ):
         return False
     return deep_get(event, "parameters", "NEW_VALUE", default="") in [
