2b900b12a3188d79dab5664944bef7d65da34fa8
andrea-youwakim <andrea.youwakim@panther.io>
2022-12-15 15:50:34 -0700

    cosmetic fixes

--- rules/gsuite_activityevent_rules/google_workspace_advanced_protection_program.py
+++ rules/gsuite_activityevent_rules/google_workspace_advanced_protection_program.py
@@ -8,7 +8,6 @@ def rule(event):
 
 
 def title(event):
-    # (Optional) Return a string which will be shown as the alert title.
     # If no 'dedup' function is defined, the return value of this
     # method will act as deduplication string.
     setting = event.get("parameters", {}).get("SETTING_NAME", "NO_SETTING_NAME")
@@ -17,13 +16,3 @@ def title(event):
         f"Google Workspace Advanced Protection Program settings have been updated to "
         f"[{setting_name}]"
     )
-
-
-# def dedup(event):
-#  (Optional) Return a string which will be used to deduplicate similar alerts.
-# return ''
-
-# def alert_context(event):
-# (Optional) Return a dictionary with additional data to be included in
-# the alert sent to the SNS/SQS/Webhook destination
-# return {'key':'value'}
