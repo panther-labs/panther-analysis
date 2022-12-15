fb3505a00c300351d53b4972763a37b41aee1b5d
andrea-youwakim <andrea.youwakim@panther.io>
2022-12-15 16:22:15 -0700

    added newly written rule to monitor when a new mobile app is installed in the google workspace apps directory

---
+++ rules/gsuite_activityevent_rules/google_workspace_apps_new_mobile_app_installed.py
@@ -0,0 +1,18 @@
+def rule(event):
+    # Return True to match the log event and trigger an alert.
+    return event.get("name", "") == "ADD_MOBILE_APPLICATION_TO_WHITELIST"
+
+
+def title(event):
+    # If no 'dedup' function is defined, the return value of
+    # this method will act as deduplication string.
+    mobile_app_pkg_id = event.get("parameters", {}).get(
+        "MOBILE_APP_PACKAGE_ID", "<NO_MOBILE_APP_PACKAGE_ID_FOUND>"
+    )
+    return (
+        f"Google Workspace User [{event.get('actor',{}).get('email','<NO_EMAIL_FOUND>')}] "
+        f"added a new mobile application "
+        f"[{mobile_app_pkg_id}] "
+        f"to your organization's mobile application whitelist for the device type "
+        f"[{event.get('parameters',{}).get('DEVICE_TYPE','<NO_DEVICE_TYPE_FOUND>')}]."
+    )
