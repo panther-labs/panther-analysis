4dd29dcef0253267c146d6ed36801609754bb342
Andrew Miotke <andrew.miotke@panther.io>
2022-12-15 16:41:03 -0800

    Detection: Linted using make fmt

--- rules/atlassian_rules/user_logged_in_as_user.py
+++ rules/atlassian_rules/user_logged_in_as_user.py
@@ -2,20 +2,17 @@ from panther_base_helpers import deep_get
 
 
 def rule(event):
-    return deep_get(
-        event,
-        "attributes",
-        "action",
-        default="<unknown-action>") == "user_logged_in_as_user"
+    return (
+        deep_get(event, "attributes", "action", default="<unknown-action>")
+        == "user_logged_in_as_user"
+    )
 
 
 def title(event):
     actor = deep_get(event, "attributes", "actor", "email", default="<unknown-email>")
     impersonated_user = deep_get(
-        event,
-        "attributes",
-        "context",
-        default="<unknown-user-attributes>")[0]["attributes"].get("email")
+        event, "attributes", "context", default="<unknown-user-attributes>"
+    )[0]["attributes"].get("email")
     return f"{actor} logged in as {impersonated_user}."
 
 
@@ -24,9 +21,7 @@ def alert_context(event):
         "Timestamp": deep_get(event, "attributes", "time", default="<unknown-time>"),
         "Actor": deep_get(event, "attributes", "actor", "email", default="<unknown-actor-email>"),
         "Impersonated user": deep_get(
-            event,
-            "attributes",
-            "context",
-            default="<unknown-user-attributes>")[0]["attributes"].get("email"),
+            event, "attributes", "context", default="<unknown-user-attributes>"
+        )[0]["attributes"].get("email"),
         "Event ID": event.get("id"),
     }
