95b36a74b255f41afadff88d56afc1dccbf3689d
andrea-youwakim <andrea.youwakim@panther.io>
2022-12-16 13:53:14 -0700

    fixed: linting issues

--- rules/duo_rules/duo_admin_bypass_code_created.py
+++ rules/duo_rules/duo_admin_bypass_code_created.py
@@ -2,6 +2,7 @@ def rule(event):
     # Return True to match the log event and trigger an alert.
     return event.get("action", "") == "bypass_create"
 
+
 def title(event):
     # If no 'dedup' function is defined, the return value of
     # this method will act as deduplication string.
