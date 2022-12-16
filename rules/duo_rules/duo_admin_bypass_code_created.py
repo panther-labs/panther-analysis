e85f1f8683b5b700738536701282664eb66ad274
andrea-youwakim <andrea.youwakim@panther.io>
2022-12-16 13:43:33 -0700

    Newly added duo rule monitoring and alerting on duo admin's creating mfa bypass codes for an application.

---
+++ rules/duo_rules/duo_admin_bypass_code_created.py
@@ -0,0 +1,11 @@
+def rule(event):
+    # Return True to match the log event and trigger an alert.
+    return event.get("action", "") == "bypass_create"
+
+def title(event):
+    # If no 'dedup' function is defined, the return value of
+    # this method will act as deduplication string.
+    return (
+        f"Duo: [{event.get('username', '<NO_USER_FOUND>')}] created "
+        f"a MFA bypass code for [{event.get('object', '<NO_OBJECT_FOUND>')}]"
+    )
