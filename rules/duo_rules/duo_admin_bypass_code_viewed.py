13774b559f2394567889efd1e8e59877b1711b55
andrea-youwakim <andrea.youwakim@panther.io>
2022-12-16 15:41:37 -0700

    fixed title

--- rules/duo_rules/duo_admin_bypass_code_viewed.py
+++ rules/duo_rules/duo_admin_bypass_code_viewed.py
@@ -7,6 +7,6 @@ def title(event):
     # If no 'dedup' function is defined, the return value
     # of this method will act as deduplication string.
     return (
-        f"Duo: [{event.get('username', '<NO_USER_FOUND>')}] created "
-        f"a MFA bypass code for [{event.get('object', '<NO_OBJECT_FOUND>')}]."
+        f"Duo: [{event.get('username', '<NO_USER_FOUND>')}] viewed "
+        f"an MFA bypass code for [{event.get('object', '<NO_OBJECT_FOUND>')}]."
     )
