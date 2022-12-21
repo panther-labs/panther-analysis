3fbe906ddb458a4798b88a7fa0f2a98914576439
andrea-youwakim <andrea.youwakim@panther.io>
2022-12-21 12:27:16 -0600

    adding an invalid json test case, and json loads to properly parse the description json

--- rules/duo_rules/duo_admin_lockout.py
+++ rules/duo_rules/duo_admin_lockout.py
@@ -1,3 +1,6 @@
+import json
+
+
 def rule(event):
     # Return True to match the log event and trigger an alert.
     return event.get("action", "") == "admin_lockout"
@@ -6,7 +9,12 @@ def rule(event):
 def title(event):
     # If no 'dedup' function is defined, the return value
     # of this method will act as deduplication string.
+    try:
+        desc = json.loads(event.get("description", {}))
+        message = desc.get("message", "<NO_MESSAGE_FOUND>")
+    except ValueError:
+        message = "Invalid Json"
     return (
-        f"Duo User [{event.get('username', '<NO_USER_FOUND>')}] is "
-        f"temporarily locked out due to too many passcode attempts."
+        f"Duo Admin [{event.get('username', '<NO_USER_FOUND>')}] is "
+        f"locked out. Reason: [{message}]."
     )
