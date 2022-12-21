0f3348ff4940775c922ff2d269163b88af1e07cb
andrea-youwakim <andrea.youwakim@panther.io>
2022-12-21 12:25:47 -0600

    Revert "fixing title and test case in yml"
    This reverts commit ad1c0bcc27a74f27aa116f52f74fc951f639d93d.

--- rules/duo_rules/duo_admin_lockout.py
+++ rules/duo_rules/duo_admin_lockout.py
@@ -8,6 +8,5 @@ def title(event):
     # of this method will act as deduplication string.
     return (
         f"Duo User [{event.get('username', '<NO_USER_FOUND>')}] is "
-        f"locked out. Reason: "
-        f"[{event.get('description', {}).get('message','<NO_MESSAGE_FOUND>')}]."
+        f"temporarily locked out due to too many passcode attempts."
     )
