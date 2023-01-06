ba8a99fabd8b0867316fff2f651480b83527eb9f
Nate Zemanek <natezemanek@US-ML40NMGH9Q.localdomain>
2023-01-06 14:46:22 -0500

    Linter fix #3

--- rules/aws_cloudtrail_rules/abnormally_high_event_volume.py
+++ rules/aws_cloudtrail_rules/abnormally_high_event_volume.py
@@ -7,64 +7,44 @@ from panther_oss_helpers import get_string_set, put_string_set, set_key_expirati
 ANOMALY_THRESHOLD = 10
 
 # MAX_LEDGER_COUNT enforces a cap on list items for the rolling ledger
-# If len(COUNT_LEDGER) == MAX_LEDGER_COUNT, the oldest list item is purged
+# If len(count_ledger) == MAX_LEDGER_COUNT, the oldest list item is purged
 MAX_LEDGER_COUNT = 15
 
-COUNT_LEDGER = None
-AVERAGE_COUNT = None
-
 def rule(event):
-    global COUNT_LEDGER
-    global AVERAGE_COUNT
-
     # Generate the DynamoDB key
     key = get_key(event)
 
+    # Store the current number of events
     num_logs = event.get("num_logs", 0)
 
     # Get the count ledger from DynamoDB (if there is one)
     # Example: [10, 15, 200, 20, 10....]
-    COUNT_LEDGER = get_count_ledger(key)
-
-    # Handle Unit Tests with mock overrides
-    if isinstance(COUNT_LEDGER, str):
-        COUNT_LEDGER = {'[40, 10, 20]'}
+    count_ledger = get_count_ledger(key)
 
     # If there is no count ledger, we start one and store it in DynamoDB
     # Example:
     #       num_logs = 10
-    #       Therefore, COUNT_LEDGER = [10]
-    if not COUNT_LEDGER:
+    #       Therefore, count_ledger = [10]
+    if not count_ledger:
         new_ledger = [num_logs]
         put_string_set(key, [str(new_ledger)])
         set_key_expiration(key, str((datetime.now() + timedelta(minutes=30)).timestamp()))
         return False
 
-    # Since DynamoDB returns a string set, we need to deserialize into a list using ast.literal
-    COUNT_LEDGER = ast.literal_eval(COUNT_LEDGER.pop())
-
     # Calculate an average of all previous log counts
-    AVERAGE_COUNT = mean(COUNT_LEDGER)
+    average_count = get_average_count(count_ledger)
 
     # If list length exceeds MAX_LEDGER_COUNT, then prune first item on the list
-    if len(COUNT_LEDGER) == MAX_LEDGER_COUNT:
-        COUNT_LEDGER.pop(0)
+    if len(count_ledger) == MAX_LEDGER_COUNT:
+        count_ledger.pop(0)
 
     # Append the current count to the list (after the average is calculated)
-    COUNT_LEDGER.append(num_logs)
+    count_ledger.append(num_logs)
 
     # Store the updated count ledger in DynamoDB
-    put_string_set(key, [str(COUNT_LEDGER)])
+    put_string_set(key, [str(count_ledger)])
 
-    return num_logs / AVERAGE_COUNT >= ANOMALY_THRESHOLD
-
-
-def get_count_ledger(key):
-    return get_string_set(key)
-
-
-def get_key(event):
-    return str(event.get("p_log_type")) + __name__
+    return num_logs / average_count >= ANOMALY_THRESHOLD
 
 
 def title(event):
@@ -73,9 +53,33 @@ def title(event):
 
 
 def alert_context(event):
+    key = get_key(event)
+    count_ledger = get_count_ledger(key)
+    average_count = get_average_count(count_ledger)
+
     context = {}
     context['Log Type'] = event.get('p_log_type')
-    context['Count Ledger'] = COUNT_LEDGER
-    context['Average'] = AVERAGE_COUNT
+    context['Count Ledger'] = count_ledger
+    context['Average'] = average_count
 
     return context
+
+
+def get_count_ledger(key):
+    count_ledger = get_string_set(key)
+
+    # Handle Unit Tests with mock overrides
+    if isinstance(count_ledger, str):
+        return [40, 10, 20]
+
+    # Since DynamoDB returns a string set, we need to deserialize into a list
+    else:
+        return ast.literal_eval(count_ledger.pop())
+
+
+def get_average_count(count_ledger):
+    return mean(count_ledger)
+
+
+def get_key(event):
+    return str(event.get("p_log_type")) + __name__
