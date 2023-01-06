3ed3bede743ad1c08bd6dc2a7a23d88be2745bc2
Nicholas Kuligoski <nkulig@US-K272KJ4452.localdomain>
2023-01-06 11:47:08 -0500

    adding test cases and including mocks

--- rules/aws_s3_rules/aws_s3_bucket_defacement_attempt.py
+++ rules/aws_s3_rules/aws_s3_bucket_defacement_attempt.py
@@ -1,4 +1,5 @@
 from fnmatch import fnmatch
+import ast
 
 # Ignore certain operations, users, user agents and specifc iam roles
 EXCLUDED_OPERATIONS = ["REST.GET.*", "REST.HEAD.*", "S3.EXPIRE.OBJECT"]
@@ -7,13 +8,20 @@ EXCLUDED_USERAGENTS = ["*aws-internal*"]
 EXCLUDED_ASSUMED_ROLES = ["*assumed-role/pan*"]
 
 # Optionally, explicitly monitor specific buckets and/or files
-INCLUDED_BUCKET_NAMES = []
-INCLUDED_S3_BUCKET_KEY = []
+# Defined as functions so that we may mock our testing
+def get_included_bucket_names():
+    included_bucket_names = []
+    return included_bucket_names
+
+
+def get_included_s3_bucket_keys():
+    included_s3_bucket_keys = []
+    return included_s3_bucket_keys
 
 
 def rule(event):
 
-    # Quick exit if event contains ignored value
+    # Exit if event contains ignored value
     for item in EXCLUDED_OPERATIONS:
         if fnmatch(event.get("operation"), item):
             return False
@@ -30,15 +38,26 @@ def rule(event):
         if fnmatch(event.get("requester"), item):
             return False
 
-    for item in INCLUDED_BUCKET_NAMES:
+    # Exit if event contains watched values
+    # with logic added to support mock tests
+    included_bucket_names = get_included_bucket_names()
+    included_s3_bucket_keys = get_included_s3_bucket_keys()
+
+    if isinstance(included_bucket_names, str):
+        included_bucket_names = ast.literal_eval(included_bucket_names)
+
+    if isinstance(included_s3_bucket_keys, str):
+        included_s3_bucket_keys = ast.literal_eval(included_s3_bucket_keys)
+
+    for item in included_bucket_names:
         if fnmatch(event.get("bucket"), item):
             return True
 
-    for item in INCLUDED_S3_BUCKET_KEY:
+    for item in included_s3_bucket_keys:
         if fnmatch(event.get("key"), item):
             return True
-    
-    return True
+
+    return False
 
 
 def title(event):
