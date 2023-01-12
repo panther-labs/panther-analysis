1910387ea69438fdf4b67a53c43f53b5d478a385
Ed⁦ <ed.anderson@panther.io>
2023-01-12 09:05:24 -0800

    chore: restructure the S3 bucket defacement rule to pass cylcocomplexity checks and restructure tests to logical boundaries (#619)

--- rules/aws_s3_rules/aws_s3_bucket_defacement_attempt.py
+++ rules/aws_s3_rules/aws_s3_bucket_defacement_attempt.py
@@ -1,61 +1,49 @@
+import json
 from fnmatch import fnmatch
-import ast
+from unittest.mock import MagicMock
 
 # Ignore certain operations, users, user agents and specifc iam roles
-EXCLUDED_OPERATIONS = ["REST.GET.*", "REST.HEAD.*", "S3.EXPIRE.OBJECT"]
-EXCLUDED_REQUESTERS = ["svc:*", "AmazonS3", "*:assumed-role/AWSServiceRole*", ""]
-EXCLUDED_USERAGENTS = ["*aws-internal*"]
-EXCLUDED_ASSUMED_ROLES = ["*assumed-role/pan*"]
+ALWAYS_IGNORE_SETTINGS = {
+    "operation": ["REST.GET.*", "REST.HEAD.*", "S3.EXPIRE.OBJECT"],
+    # requestor can also hold assumed-roles
+    "requester": ["svc:*", "AmazonS3", "*:assumed-role/AWSServiceRole*"],
+    # NOTE: S3 web console uses a userAgent that starts with S3Console and contains
+    #  "aws-internal" inside the userAgent string. it's important that we glob _after_
+    "userAgent": ["aws-internal*"],
+    # Set any buckets that you want to ignore in the bucket list
+    "bucket": [],
+}
 
-# Optionally, explicitly monitor specific buckets and/or files
-# Defined as functions so that we may mock our testing
-def get_included_bucket_names():
-    included_bucket_names = []
-    return included_bucket_names
-
-
-def get_included_s3_bucket_keys():
-    included_s3_bucket_keys = []
-    return included_s3_bucket_keys
+# Any items put in ALERT_SETTINGS will cause an alert to raise, provided
+# that they do not match the ignore conditions in ALWAYS_IGNORE_SETTINGS
+ALERT_SETTINGS = {
+    # If there are high-pri buckets, put their names here
+    "bucket": [],
+    # If there are high-pri bucket keys, put their names here
+    "keys": [],
+}
 
 
 def rule(event):
+    global ALWAYS_IGNORE_SETTINGS  # pylint: disable=global-statement
+    global ALERT_SETTINGS  # pylint: disable=global-statement
+    # First we exclude ignored items
+    if isinstance(ALWAYS_IGNORE_SETTINGS, MagicMock):
+        # pylint: disable=redefined-outer-name,not-callable
+        ALWAYS_IGNORE_SETTINGS = json.loads(ALWAYS_IGNORE_SETTINGS())
+    for event_key, ignore_values in ALWAYS_IGNORE_SETTINGS.items():
+        for ignore_value in ignore_values:
+            if fnmatch(event.get(event_key, ""), ignore_value):
+                return False
 
-    # Exit if event contains ignored value
-    for item in EXCLUDED_OPERATIONS:
-        if fnmatch(event.get("operation"), item):
-            return False
-
-    for item in EXCLUDED_REQUESTERS:
-        if fnmatch(event.get("requester"), item):
-            return False
-
-    for item in EXCLUDED_USERAGENTS:
-        if fnmatch(event.get("useragent"), item):
-            return False
-
-    for item in EXCLUDED_ASSUMED_ROLES:
-        if fnmatch(event.get("requester"), item):
-            return False
-
-    # Exit if event contains watched values
-    # with logic added to support mock tests
-    included_bucket_names = get_included_bucket_names()
-    included_s3_bucket_keys = get_included_s3_bucket_keys()
-
-    if isinstance(included_bucket_names, str):
-        included_bucket_names = ast.literal_eval(included_bucket_names)
-
-    if isinstance(included_s3_bucket_keys, str):
-        included_s3_bucket_keys = ast.literal_eval(included_s3_bucket_keys)
-
-    for item in included_bucket_names:
-        if fnmatch(event.get("bucket"), item):
-            return True
-
-    for item in included_s3_bucket_keys:
-        if fnmatch(event.get("key"), item):
-            return True
+    # Check for explicitly marked ALWAYS_ALERT_SETTINGS
+    if isinstance(ALERT_SETTINGS, MagicMock):
+        # pylint: disable=redefined-outer-name,not-callable
+        ALERT_SETTINGS = json.loads(ALERT_SETTINGS())
+    for event_key, alert_values in ALERT_SETTINGS.items():
+        for alert_value in alert_values:
+            if fnmatch(event.get(event_key, ""), alert_value):
+                return True
 
     return False
 
