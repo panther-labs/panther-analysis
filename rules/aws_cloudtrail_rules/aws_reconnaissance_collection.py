8e5f939d7414117ac8c13079961df9befc188560
hbenac10 <holly.benac@panther.io>
2023-01-13 14:28:45 -0500

    Fixing black command for MITRE T1119

--- rules/aws_cloudtrail_rules/aws_reconnaissance_collection.py
+++ rules/aws_cloudtrail_rules/aws_reconnaissance_collection.py
@@ -1,59 +1,56 @@
 from panther_base_helpers import deep_get, pattern_match_list, aws_rule_context
 
 events = {
-# Monitor for object level events
-    'PutObject',
-    'GetObject',
-    'DeleteObject',
-    'CopyObject',
-    'InitiateMultipartUpload',
-    'UploadPart',
-    'CompleteMultipartUpload',
-    'AbortMultipartUpload',
-    'ListMultipartUploadParts',
-
-# Monitor for bucket level events
-# Also check ListBucket to reveal object enumeration.
-    'ListBucket',
-    'CreateBucket',
-    'DeleteBucket',
-    'PutBucketAcl',
-    'PutBucketPolicy',
-    'PutBucketCors',
-    'PutBucketLifecycle',
-    'PutBucketReplication',
-    'PutBucketEncryption',
-    'PutBucketTagging',
-
-#Monitor for events associated with user creation, deletion, & modification
-    'CreateUser',
-    'CreateLoginProfile',
-    'DeleteUser',
-    'DeleteLoginProfile',
-    'RemoveUserFromGroup',
-    'UpdateUser',
-    'UpdateLoginProfile',
-    'PutUserPolicy',
-    'AddUserToGroup',
-    'RemoveUserFromGroup'
-
-#Monitor actions that can execute a script
-    'InvokeFunction',
-    'SendCommand',
-    'RunInstances',
-    'SubmitJob',
-    'TerminateJob',
-    'StartAutomationExecution',
-    'StopAutomationExecution'
-
+    # Monitor for object level events
+    "PutObject",
+    "GetObject",
+    "DeleteObject",
+    "CopyObject",
+    "InitiateMultipartUpload",
+    "UploadPart",
+    "CompleteMultipartUpload",
+    "AbortMultipartUpload",
+    "ListMultipartUploadParts",
+    # Monitor for bucket level events
+    # Also check ListBucket to reveal object enumeration.
+    "ListBucket",
+    "CreateBucket",
+    "DeleteBucket",
+    "PutBucketAcl",
+    "PutBucketPolicy",
+    "PutBucketCors",
+    "PutBucketLifecycle",
+    "PutBucketReplication",
+    "PutBucketEncryption",
+    "PutBucketTagging",
+    # Monitor for events associated with user creation, deletion, & modification
+    "CreateUser",
+    "CreateLoginProfile",
+    "DeleteUser",
+    "DeleteLoginProfile",
+    "RemoveUserFromGroup",
+    "UpdateUser",
+    "UpdateLoginProfile",
+    "PutUserPolicy",
+    "AddUserToGroup",
+    "RemoveUserFromGroup"
+    # Monitor actions that can execute a script
+    "InvokeFunction",
+    "SendCommand",
+    "RunInstances",
+    "SubmitJob",
+    "TerminateJob",
+    "StartAutomationExecution",
+    "StopAutomationExecution",
 }
 
 # Some AWS IP addresses listed as 'malicious' are stale
 # This enables allowing specific roles where this may occur
 _ALLOWED_ROLES = {
-    '*PantherAuditRole-*',
-    '*PantherLogProcessingRole-*',
+    "*PantherAuditRole-*",
+    "*PantherLogProcessingRole-*",
 }
+
 
 def rule(event):
     # Filter: Non-S3 events
@@ -80,6 +77,7 @@ def title(event):
         f"Suspicious events detected by [{deep_get(event, 'sourceIPAddress')}]"
         f"From [{deep_get(event, 'userIdentity', 'arn')}]"
     )
+
 
 def alert(event):
     return aws_rule_context(event)
