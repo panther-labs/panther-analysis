7d60fdf4fe4cbf04e7b6fb62130094b62f82e300
Calvin Kim <calvin.kim@panther.com>
2023-01-17 11:12:14 -0600

    create admin

---
+++ rules/duo_rules/duo_admin_create_admin.py
@@ -0,0 +1,22 @@
+from panther_duo_helpers import (
+    deserialize_administrator_log_event_description,
+    duo_alert_context,
+)
+
+
+def rule(event):
+    return event.get("action") == "admin_create"
+
+
+def title(event):
+    event_description = deserialize_administrator_log_event_description(event)
+    return (
+        f"Duo: [{event.get('username', '<username_not_found>')}] "
+        "created a new admin account for "
+        f"[{event_description.get('name', '<name_not_found>')}] "
+        f"[{event_description.get('email', '<email_not_found>')}]."
+    )
+
+
+def alert_context(event):
+    return duo_alert_context(event)
