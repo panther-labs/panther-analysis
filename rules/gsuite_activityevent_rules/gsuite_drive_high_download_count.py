5301b5f91c5ce290cea594eb1c2333ac9b16441b
Calvin Kim <calvin.kim@panther.com>
2023-01-20 14:14:02 -0600

    google drive download detection

---
+++ rules/gsuite_activityevent_rules/gsuite_drive_high_download_count.py
@@ -0,0 +1,12 @@
+def rule(event):
+    return True
+
+def title(event):
+    return (
+        f"GSuite: [{event.get('user', '<user_not_found>')}] "
+        f"downloaded [{event.get('download_count', '<count_not_found>')}] "
+        "files from Google Drive."
+    )
+
+def alert_context(event):
+    return event
