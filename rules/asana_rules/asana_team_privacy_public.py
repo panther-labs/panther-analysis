0962c44947eb069a9626e2545aa7e5c786a14c4c
Calvin Kim <calvin.kim@panther.com>
2023-02-01 14:29:30 -0600

    fmt

--- rules/asana_rules/asana_team_privacy_public.py
+++ rules/asana_rules/asana_team_privacy_public.py
@@ -12,12 +12,3 @@ def title(event):
     team = deep_get(event, "resource", "name", default="<TEAM_NOT_FOUND>")
     actor = deep_get(event, "actor", "email", default="<ACTOR_NOT_FOUND>")
     return f"Asana team [{team}] has been made public to the org by [{actor}]."
-
-
-# def dedup(event):
-#  (Optional) Return a string which will be used to deduplicate similar alerts.
-# return ''
-
-# def alert_context(event):
-#  (Optional) Return a dictionary with additional data to be included in the alert sent to the SNS/SQS/Webhook destination
-# return {'key':'value'}
