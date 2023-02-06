57af408b7eb238622e52a990d8fa276e434d1897
Calvin Kim <calvin.kim@panther.com>
2023-02-06 16:19:23 -0600

    modify cloudtrail policy for advanced selectors

--- policies/aws_cloudtrail_policies/aws_cloudtrail_enabled.py
+++ policies/aws_cloudtrail_policies/aws_cloudtrail_enabled.py
@@ -1,12 +1,24 @@
 def policy(resource):
-    if not resource["Trails"]:
+    if not (resource.get("Trails") or resource.get("TrailARN")):
         return False
 
-    if not resource["GlobalEventSelectors"]:
+    if not (resource.get("GlobalEventSelectors") or resource.get("GlobalAdvancedEventSelectors")):
         return False
 
-    for selector in resource["GlobalEventSelectors"]:
-        if selector["IncludeManagementEvents"] and selector["ReadWriteType"] == "All":
+    for selector in resource.get("GlobalEventSelectors", [{}]):
+        if selector.get("IncludeManagementEvents") and selector.get("ReadWriteType") == "All":
             return True
 
+    for advanced_selector in resource.get("GlobalAdvancedEventSelectors", [{}]):
+        management_present = False
+        readOnly_excluded = True
+        for field_selector in advanced_selector.get("FieldSelectors", [{}]):
+            if field_selector.get("Field") == "eventCategory":
+                eventCategories = field_selector.get("Equals", [])
+                if "Management" in eventCategories:
+                    management_present = True
+            if field_selector.get("Field") == "readOnly":
+                readOnly_excluded = False
+        if readOnly_excluded and management_present:
+            return True
     return False
