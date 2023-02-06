953dfd4a983bb352a4877bd074d1b31153fc14ee
Calvin Kim <calvin.kim@panther.com>
2023-02-06 16:24:30 -0600

    lint

--- policies/aws_cloudtrail_policies/aws_cloudtrail_enabled.py
+++ policies/aws_cloudtrail_policies/aws_cloudtrail_enabled.py
@@ -1,4 +1,5 @@
 def policy(resource):
+    #pylint: disable=R1260
     if not (resource.get("Trails") or resource.get("TrailARN")):
         return False
 
@@ -11,14 +12,14 @@ def policy(resource):
 
     for advanced_selector in resource.get("GlobalAdvancedEventSelectors", [{}]):
         management_present = False
-        readOnly_excluded = True
+        readonly_excluded = True
         for field_selector in advanced_selector.get("FieldSelectors", [{}]):
             if field_selector.get("Field") == "eventCategory":
-                eventCategories = field_selector.get("Equals", [])
-                if "Management" in eventCategories:
+                event_categories = field_selector.get("Equals", [])
+                if "Management" in event_categories:
                     management_present = True
             if field_selector.get("Field") == "readOnly":
-                readOnly_excluded = False
-        if readOnly_excluded and management_present:
+                readonly_excluded = False
+        if readonly_excluded and management_present:
             return True
     return False
