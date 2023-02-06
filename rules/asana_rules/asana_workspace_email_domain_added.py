80ee541a6b9e5affd0a04e2d8f8ed2019122d4a3
calkim-panther <113376708+calkim-panther@users.noreply.github.com>
2023-02-06 08:54:56 -0600

    Update rules/asana_rules/asana_workspace_email_domain_added.py

--- rules/asana_rules/asana_workspace_email_domain_added.py
+++ rules/asana_rules/asana_workspace_email_domain_added.py
@@ -9,4 +9,4 @@ def title(event):
     workspace = deep_get(event, "resource", "name", default="<WORKSPACE_NOT_FOUND>")
     domain = deep_get(event, "details", "new_value", default="<DOMAIN_NOT_FOUND>")
     actor = deep_get(event, "actor", "email", default="<ACTOR_NOT_FOUND>")
-    return f"Asana new email domain [{domain}] added to Workspace [{workspace}] " f"by [{actor}]."
+    return f"Asana new email domain [{domain}] added to Workspace [{workspace}] by [{actor}]."
