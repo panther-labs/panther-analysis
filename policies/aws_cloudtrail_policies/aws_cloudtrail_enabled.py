3c5a8a6e1719353dd839528494c76b0064e4a121
Calvin Kim <calvin.kim@panther.com>
2023-02-08 11:33:18 -0600

    variable wording

--- policies/aws_cloudtrail_policies/aws_cloudtrail_enabled.py
+++ policies/aws_cloudtrail_policies/aws_cloudtrail_enabled.py
@@ -14,14 +14,14 @@ def policy(resource):
 
     for advanced_selector in resource.get("GlobalAdvancedEventSelectors", [{}]):
         management_present = False
-        readonly_excluded = True
+        readonly_present = False
         for field_selector in advanced_selector.get("FieldSelectors", [{}]):
             if field_selector.get("Field") == "eventCategory":
                 event_categories = field_selector.get("Equals", [])
                 if "Management" in event_categories:
                     management_present = True
             if field_selector.get("Field") == "readOnly":
-                readonly_excluded = False
-        if readonly_excluded and management_present:
+                readonly_present = True
+        if all([management_present, not readonly_present]):
             return True
     return False
