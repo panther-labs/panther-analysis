b5dad84b7925310706246d2ac04f54c966c4c4a6
hbenac10 <holly.benac@panther.io>
2023-02-17 11:34:35 -0500

    Fixed black

--- rules/aws_cloudtrail_rules/aws_s3_reconnaissance.py
+++ rules/aws_cloudtrail_rules/aws_s3_reconnaissance.py
@@ -1,35 +1,33 @@
 from panther_base_helpers import deep_get, pattern_match_list, aws_rule_context
 
 events = {
-# Monitor for object level events
-    'PutObject',
-    'GetObject',
-    'DeleteObject',
-    'CopyObject',
-    'InitiateMultipartUpload',
-    'UploadPart',
-    'CompleteMultipartUpload',
-    'AbortMultipartUpload',
-    'ListMultipartUploadParts',
-
-# Monitor for bucket level events
-# Also check ListBucket to reveal object enumeration.
-    'ListBucket',
-    'CreateBucket',
-    'DeleteBucket',
-    'PutBucketAcl',
-    'PutBucketPolicy',
-    'PutBucketCors',
-    'PutBucketLifecycle',
-
-#Monitor actions that can execute a script
-    'RunInstances',
-    'SubmitJob',
-    'TerminateJob',
-    'StartAutomationExecution',
-    'StopAutomationExecution'
-
+    # Monitor for object level events
+    "PutObject",
+    "GetObject",
+    "DeleteObject",
+    "CopyObject",
+    "InitiateMultipartUpload",
+    "UploadPart",
+    "CompleteMultipartUpload",
+    "AbortMultipartUpload",
+    "ListMultipartUploadParts",
+    # Monitor for bucket level events
+    # Also check ListBucket to reveal object enumeration.
+    "ListBucket",
+    "CreateBucket",
+    "DeleteBucket",
+    "PutBucketAcl",
+    "PutBucketPolicy",
+    "PutBucketCors",
+    "PutBucketLifecycle",
+    # Monitor actions that can execute a script
+    "RunInstances",
+    "SubmitJob",
+    "TerminateJob",
+    "StartAutomationExecution",
+    "StopAutomationExecution",
 }
+
 
 def rule(event):
     # Filter: Non-S3 events
@@ -46,6 +44,7 @@ def rule(event):
         return False
     return True
 
+
 def title(event):
     return (
         f"IP Address [{event.get('sourceIPAddress')}]"
@@ -53,6 +52,7 @@ def title(event):
         f"Suspicious events detected by [{deep_get(event, 'sourceIPAddress')}]"
         f"From [{deep_get(event, 'userIdentity', 'arn')}]"
     )
+
 
 def alert(event):
     return aws_rule_context(event)
