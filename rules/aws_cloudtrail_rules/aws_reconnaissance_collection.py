79518ab4db572c655e41dc0ee64e9afbbff45d98
hbenac10 <holly.benac@panther.io>
2023-02-17 10:41:45 -0500

    Fixed detection logic

--- rules/aws_cloudtrail_rules/aws_reconnaissance_collection.py
+++ rules/aws_cloudtrail_rules/aws_reconnaissance_collection.py
@@ -1,74 +1,57 @@
-from panther_base_helpers import deep_get, pattern_match_list, aws_rule_context
+from ipaddress import ip_address
+from panther import aws_cloudtrail_success
+from panther_base_helpers import deep_get, aws_rule_context
 
-events = {
-    # Monitor for object level events
-    "PutObject",
-    "GetObject",
-    "DeleteObject",
-    "CopyObject",
-    "InitiateMultipartUpload",
-    "UploadPart",
-    "CompleteMultipartUpload",
-    "AbortMultipartUpload",
-    "ListMultipartUploadParts",
-    # Monitor for bucket level events
-    # Also check ListBucket to reveal object enumeration.
-    "ListBucket",
-    "CreateBucket",
-    "DeleteBucket",
-    "PutBucketAcl",
-    "PutBucketPolicy",
-    "PutBucketCors",
-    "PutBucketLifecycle",
-    "PutBucketReplication",
-    "PutBucketEncryption",
-    "PutBucketTagging",
-    # Monitor for events associated with user creation, deletion, & modification
-    "CreateUser",
-    "CreateLoginProfile",
-    "DeleteUser",
-    "DeleteLoginProfile",
-    "RemoveUserFromGroup",
-    "UpdateUser",
-    "UpdateLoginProfile",
-    "PutUserPolicy",
-    "AddUserToGroup",
-    "RemoveUserFromGroup"
-    # Monitor actions that can execute a script
-    "InvokeFunction",
-    "SendCommand",
-    "RunInstances",
-    "SubmitJob",
-    "TerminateJob",
-    "StartAutomationExecution",
-    "StopAutomationExecution",
+EVENTS = {
+# Monitor for object level events
+    'PutObject',
+    'GetObject',
+    'DeleteObject',
+    'CopyObject',
+    'InitiateMultipartUpload',
+    'UploadPart',
+    'CompleteMultipartUpload',
+    'AbortMultipartUpload',
+    'ListMultipartUploadParts',
+
+# Monitor for bucket level events
+# Also check ListBucket to reveal object enumeration.
+    'ListBucket',
+    'CreateBucket',
+    'DeleteBucket',
+    'PutBucketAcl',
+    'PutBucketPolicy',
+    'PutBucketCors',
+    'PutBucketLifecycle',
+
+#Monitor actions that can execute a script
+    'RunInstances',
+    'SubmitJob',
+    'TerminateJob',
+    'StartAutomationExecution',
+    'StopAutomationExecution'
+
 }
-
 
 def rule(event):
     # Filter: Non-S3 events
     if event.get("eventSource") != "s3.amazonaws.com":
         return False
     # Filter: Errors
-    if event.get("errorCode"):
+    if not aws_cloudtrail_success(event):
         return False
     # Filter: Internal AWS
     if deep_get(event, "userIdentity", "type") in ("AWSAccount", "AWSService"):
         return False
     # Filter: Non "Get" events
-    if not pattern_match_list(event.get("eventName", ""), events):
-        return False
-    return True
-
+    return event.get("eventName", "") in EVENTS
 
 def title(event):
     return (
-        f"IP Address [{event.get('sourceIPAddress')}]"
-        f"User [{deep_get(event, 'userIdentity', 'arn')}]"
-        f"Suspicious events detected by [{deep_get(event, 'sourceIPAddress')}]"
-        f"From [{deep_get(event, 'userIdentity', 'arn')}]"
+        f"AWS Identity [{deep_get(event, 'userIdentity', 'arn')}] " 
+        f"executed possible reconnaissance command [event.get('eventName', '')] "
+        f"in AWS Account [event.get('recipientAccountId', '')]"
     )
-
 
 def alert(event):
     return aws_rule_context(event)
