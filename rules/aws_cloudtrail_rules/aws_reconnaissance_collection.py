8e367d121a88b18b107d6e18bd79fa69d27a012c
hbenac10 <holly.benac@panther.io>
2023-02-17 11:07:13 -0500

    Fixed black

--- rules/aws_cloudtrail_rules/aws_reconnaissance_collection.py
+++ rules/aws_cloudtrail_rules/aws_reconnaissance_collection.py
@@ -2,35 +2,33 @@ from panther import aws_cloudtrail_success
 from panther_base_helpers import deep_get, aws_rule_context
 
 EVENTS = {
-# Monitor for object level events
-    'PutObject',
-    'GetObject',
-    'DeleteObject',
-    'CopyObject',
-    'InitiateMultipartUpload',
-    'UploadPart',
-    'CompleteMultipartUpload',
-    'AbortMultipartUpload',
-    'ListMultipartUploadParts',
-
-# Monitor for bucket level events
-# Also check ListBucket to reveal object enumeration.
-    'ListBucket',
-    'CreateBucket',
-    'DeleteBucket',
-    'PutBucketAcl',
-    'PutBucketPolicy',
-    'PutBucketCors',
-    'PutBucketLifecycle',
-
-#Monitor actions that can execute a script
-    'RunInstances',
-    'SubmitJob',
-    'TerminateJob',
-    'StartAutomationExecution',
-    'StopAutomationExecution'
-
+    # Monitor for object level events
+    "PutObject",
+    "GetObject",
+    "DeleteObject",
+    "CopyObject",
+    "InitiateMultipartUpload",
+    "UploadPart",
+    "CompleteMultipartUpload",
+    "AbortMultipartUpload",
+    "ListMultipartUploadParts",
+    # Monitor for bucket level events
+    # Also check ListBucket to reveal object enumeration.
+    "ListBucket",
+    "CreateBucket",
+    "DeleteBucket",
+    "PutBucketAcl",
+    "PutBucketPolicy",
+    "PutBucketCors",
+    "PutBucketLifecycle",
+    # Monitor actions that can execute a script
+    "RunInstances",
+    "SubmitJob",
+    "TerminateJob",
+    "StartAutomationExecution",
+    "StopAutomationExecution",
 }
+
 
 def rule(event):
     # Filter: Non-S3 events
@@ -45,12 +43,14 @@ def rule(event):
     # Filter: Non "Get" events
     return event.get("eventName", "") in EVENTS
 
+
 def title(event):
     return (
         f"AWS Identity [{deep_get(event, 'userIdentity', 'arn')}] "
         f"executed possible reconnaissance command [event.get('eventName', '')] "
         f"in AWS Account [event.get('recipientAccountId', '')]"
     )
+
 
 def alert(event):
     return aws_rule_context(event)
