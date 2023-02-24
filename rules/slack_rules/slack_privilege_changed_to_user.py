77be9e0bc72638f0e7356050ccea30975d630acd
Andrew Miotke <andrew.miotke@panther.io>
2023-02-24 14:09:01 -0800

    Slack: New rule detecting when a Slack user's role changes to User.
    - A Slack user could have a role such as Owner or Admin or another
      type of role that Slack offers. This rule detects whether an
      account is moved from a non-user role and given the User role.
      Unfortunently Slack's audit logs do not offer 1) an endpoint like
      role_change_from... 2) the role that the account previously held,
      such as the Owner or Admin role.

---
+++ rules/slack_rules/slack_privilege_changed_to_user.py
@@ -0,0 +1,25 @@
+from panther_base_helpers import deep_get, slack_alert_context
+
+
+def rule(event):
+    return event.get("action") == "role_change_to_user"
+
+
+def title(event):
+    username = deep_get(event, "entity", "user", "name", default="<unknown-actor>")
+    email = deep_get(event, "entity", "user", "email", default="<unknown-email>")
+
+    if event.get("action") == "role_change_to_user":
+        return f"{username}'s ({email}) role changed to User"
+
+    return f"Slack account changed on {username} ({email}), event: {event.get('action')}"
+
+
+def severity(event):
+    if event.get("action") != "role_change_to_user":
+        return "Low"
+    return "Medium"
+
+
+def alert_context(event):
+    return slack_alert_context(event)
