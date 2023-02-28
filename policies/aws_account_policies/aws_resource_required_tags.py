d036943ce5d4e9914b8f68c15103e990922f861d
Calvin Kim <calvin.kim@panther.com>
2023-02-28 16:16:43 -0600

    updated required tags policy

--- policies/aws_account_policies/aws_resource_required_tags.py
+++ policies/aws_account_policies/aws_resource_required_tags.py
@@ -1,19 +1,17 @@
 # REQUIRED_TAGS_MAPPINGS maps resource types to a set of tag keys required for that resource.
-# Example: REQUIRED_TAGS_MAPPINGS = {'AWS.EC2.Instance.Snapshot': {'TagOne', 'TagTwo'}}
+# Example: REQUIRED_TAGS_MAPPINGS = {'AWS.EC2.Instance.Snapshot': {'Owner', 'CreatedBy'}}
 # The above example would check all EC2 instances for the presence of tags keyed TagOne and TagTwo
-REQUIRED_TAGS_MAPPINGS = {}
+REQUIRED_TAGS_MAPPINGS = {
+    'AWS.EC2.Instance': {'Owner', 'CreatedBy'} #Example. Do not use in production
+}
 
 
 def policy(resource):
-    if resource["Tags"] is None:
-        return True
-
-    # Check if this resource type has any tag requirements.
-    # If this resource type doesn't have tag requirements, the policy should be updated.
-    if resource["ResourceType"] not in REQUIRED_TAGS_MAPPINGS:
-        return False
-
-    required_tags = REQUIRED_TAGS_MAPPINGS[resource["ResourceType"]]
-    actual_tags = set(tag["Key"] for tag in resource["Tags"])
-
-    return required_tags.issubset(actual_tags)
+    if resource["ResourceType"] in REQUIRED_TAGS_MAPPINGS:
+        required_tags = REQUIRED_TAGS_MAPPINGS[resource.get("ResourceType")]
+        tags = resource.get("Tags")
+        if not tags:
+            tags = {}
+        actual_tags = tags.keys()
+        return required_tags.issubset(actual_tags)
+    return True
