e89e6fc54888e614bbd102391a6f3943ab91b885
Calvin Kim <calvin.kim@panther.com>
2023-02-28 16:41:54 -0600

    magicmock

--- policies/aws_account_policies/aws_resource_required_tags.py
+++ policies/aws_account_policies/aws_resource_required_tags.py
@@ -1,12 +1,19 @@
+import ast
+from unittest.mock import MagicMock
+
 # REQUIRED_TAGS_MAPPINGS maps resource types to a set of tag keys required for that resource.
 # Example: REQUIRED_TAGS_MAPPINGS = {'AWS.EC2.Instance.Snapshot': {'Owner', 'CreatedBy'}}
-# The above example would check all EC2 instances for the presence of tags keyed TagOne and TagTwo
-REQUIRED_TAGS_MAPPINGS = {
-    "AWS.EC2.Instance": {"Owner", "CreatedBy"}  # Example. Do not use in production
-}
+# The above example would check all EC2 instances for the presence of tags keyed Owner and CreatedBy
+
+REQUIRED_TAGS_MAPPINGS = {}
 
 
 def policy(resource):
+    # pylint: disable=not-callable
+    # pylint: disable=global-statement
+    global REQUIRED_TAGS_MAPPINGS
+    if isinstance(REQUIRED_TAGS_MAPPINGS, MagicMock):
+        REQUIRED_TAGS_MAPPINGS = ast.literal_eval(REQUIRED_TAGS_MAPPINGS())
     if resource["ResourceType"] in REQUIRED_TAGS_MAPPINGS:
         required_tags = REQUIRED_TAGS_MAPPINGS[resource.get("ResourceType")]
         tags = resource.get("Tags")
