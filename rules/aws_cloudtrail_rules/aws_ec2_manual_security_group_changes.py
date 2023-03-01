6d30bf09e742e8c4963119aca4255de2e0ce8373
Ed Anderson <ed.anderson@panther.io>
2023-02-28 17:16:22 -0800

    chore: make fmt wanted to reorder a few imports based on panther being renamed panther_default

--- rules/aws_cloudtrail_rules/aws_ec2_manual_security_group_changes.py
+++ rules/aws_cloudtrail_rules/aws_ec2_manual_security_group_changes.py
@@ -1,5 +1,5 @@
-from panther_default import aws_cloudtrail_success
 from panther_base_helpers import aws_rule_context, deep_get, pattern_match_list
+from panther_default import aws_cloudtrail_success
 
 PROD_ACCOUNT_IDS = {"11111111111111", "112233445566"}
 SG_CHANGE_EVENTS = {
