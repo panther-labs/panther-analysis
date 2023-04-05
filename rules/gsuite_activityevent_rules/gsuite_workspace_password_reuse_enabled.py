07a828ba9529c5d554de7be6cd748ddfa9d59f74
dotbeseck <adam.besecker@chime.com>
2023-04-05 19:11:17 -0400

    Gsuite Password Reuse Nonetype error
    When the Newvalue was null this would give a nonetype error. Provided in the yaml is a test

--- rules/gsuite_activityevent_rules/gsuite_workspace_password_reuse_enabled.py
+++ rules/gsuite_activityevent_rules/gsuite_workspace_password_reuse_enabled.py
@@ -2,13 +2,15 @@ from panther_base_helpers import deep_get
 
 
 def rule(event):
-    if deep_get(event, "id", "applicationName", default="").lower() != "admin":
+    if not deep_get(event, "id", "applicationName", default="").lower() == "admin":
         return False
+    
+    new_value =  (deep_get(event, "parameters", "NEW_VALUE", default="") or "").lower()
     if all(
         [
             (event.get("name", "") == "CHANGE_APPLICATION_SETTING"),
             (event.get("type", "") == "APPLICATION_SETTINGS"),
-            (deep_get(event, "parameters", "NEW_VALUE", default="").lower() == "true"),
+            (new_value == "true"),
             (
                 deep_get(event, "parameters", "SETTING_NAME", default="")
                 == "Password Management - Enable password reuse"
