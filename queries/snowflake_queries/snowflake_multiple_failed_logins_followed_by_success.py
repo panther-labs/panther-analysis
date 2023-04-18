6c6bf1cdfbdfae293b3fc80fabe91c021a7101c8
andrea-youwakim <andrea.youwakim@panther.io>
2023-04-18 12:44:21 -0600

    enriching test cases and alert titles to add context

--- queries/snowflake_queries/snowflake_multiple_failed_logins_followed_by_success.py
+++ queries/snowflake_queries/snowflake_multiple_failed_logins_followed_by_success.py
@@ -4,7 +4,10 @@ def rule(_):
 def title(event):
     return (
         f"Username [{event.get('user_name','<USER_NOT_FOUND>')}] from clientIP "
-        f"[{event.get('client_ip','<CLIENT_IP_NOT_FOUND>')}]] "
-        f"registered multiple failed logins followed by a success."
+        f"[{event.get('client_ip','<CLIENT_IP_NOT_FOUND>')}] "
+        f"registered [{event.get('num_fails','<NUM_FAILS_NOT_FOUND>')}] failed logins "
+        f" which began at [{event.get('start_of_unsuccessful_logins_time','<UNSUCCESSFUL_LOGINS_START_TIME_NOT_FOUND>')}] "
+        f"followed by a successful login which occurred at "
+        f"[{event.get('successful_login_time','<SUCCESS_LOGIN_TIME_NOT_FOUND>')}]."
     )
  
