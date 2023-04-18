6f957dd6d438e1a985b48e95f1485629e05196fa
andrea-youwakim <andrea.youwakim@panther.io>
2023-04-18 12:47:59 -0600

    fix: default val in title

--- queries/snowflake_queries/snowflake_external_shares.py
+++ queries/snowflake_queries/snowflake_external_shares.py
@@ -7,7 +7,7 @@ def title(event):
         f"in source region [{event.get('source_region','<SOURCE_REGION_NOT_FOUND>')}] "
         f"to target cloud [{event.get('target_cloud','<TARGET_CLOUD_NOT_FOUND>')}] "
         f"in target region [{event.get('target_region','<TARGET_REGION_NOT_FOUND>')}] "
-        f"with transfer type [{event.get('transfer_type','<TARGET_REGION_NOT_FOUND>')}] "
+        f"with transfer type [{event.get('transfer_type','<TRANSFER_TYPE_NOT_FOUND>')}] "
         f"for [{event.get('bytes_transferred','<BYTES_NOT_FOUND>')}] bytes."
     )
  
