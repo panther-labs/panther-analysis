4e7c712a6e8b878bbd106b67378a4ebfe4e574f1
Calvin Kim <calvin.kim@panther.com>
2023-04-21 09:33:28 -0500

    severity high if transferred externally

--- rules/dropbox_rules/dropbox_ownership_transfer.py
+++ rules/dropbox_rules/dropbox_ownership_transfer.py
@@ -1,4 +1,11 @@
+import json
+from unittest.mock import MagicMock
+
 from panther_base_helpers import deep_get
+
+ALLOWED_DOMAINS = [
+    # "example.com"
+]
 
 
 def rule(event):
@@ -17,3 +24,13 @@ def title(event):
         f"Dropbox: [{actor}] transferred ownership of [{asset}]"
         f"from [{previous_owner}] to [{new_owner}]."
     )
+
+
+def severity(event):
+    global ALLOWED_DOMAINS  # pylint: disable=global-statement
+    if isinstance(ALLOWED_DOMAINS, MagicMock):
+        ALLOWED_DOMAINS = set(json.loads(ALLOWED_DOMAINS()))  # pylint: disable=not-callable
+    new_owner = deep_get(event, "details", "new_owner_email", default="<NEW_OWNER_NOT_FOUND>")
+    if new_owner.split("@")[-1] not in ALLOWED_DOMAINS:
+        return "HIGH"
+    return "LOW"
