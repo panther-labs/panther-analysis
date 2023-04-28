9d5aaf6360367bb617f8d8b0b95c0c3a2c7453dd
jzandona <jason.zandona@runpanther.io>
2023-04-28 14:31:22 -0700

    linting

--- data_models/crowdstrike_fdr_data_model.py
+++ data_models/crowdstrike_fdr_data_model.py
@@ -1,8 +1,10 @@
 from panther_base_helpers import deep_get
 
+
 def get_dns_query(event):
-    # Strip trailing period. Domain Names from Crowdstrike FDR end with a trailing period, such as google.com. 
-    domain = deep_get(event, 'event', 'DomainName', default=None)
+    # Strip trailing period.
+    # Domain Names from Crowdstrike FDR end with a trailing period, such as google.com.
+    domain = deep_get(event, "event", "DomainName", default=None)
     if domain:
         domain = ".".join(domain.rstrip(".").split(".")[-2:]).lower()
     return domain
--- rules/crowdstrike_rules/crowdstrike_connection_to_embargoed_country.py
+++ rules/crowdstrike_rules/crowdstrike_connection_to_embargoed_country.py
@@ -1,30 +1,37 @@
-from panther_base_helpers import deep_get, pattern_match_list, crowdstrike_detection_alert_context
+from panther_base_helpers import crowdstrike_detection_alert_context, deep_get
 
 EMBARGO_COUNTRY_CODES = {
-    "CU", #Cuba
-    "IR", #Iran
-    "KP", #DPRK
-    "SY", #Syria
+    "CU",  # Cuba
+    "IR",  # Iran
+    "KP",  # DPRK
+    "SY",  # Syria
 }
 
+
 def get_enrichment_obj(event):
-    return deep_get(event, 'p_enrichment', 'ipinfo_location',  'p_any_ip_addresses', default=None)
+    return deep_get(event, "p_enrichment", "ipinfo_location", "p_any_ip_addresses", default=None)
+
 
 def rule(event):
     enrichment_obj = get_enrichment_obj(event)
     if enrichment_obj:
-        if [i for i in enrichment_obj if i['country'] in EMBARGO_COUNTRY_CODES]:
+        if [i for i in enrichment_obj if i["country"] in EMBARGO_COUNTRY_CODES]:
             return True
     return False
 
+
 def title(event):
     enrichment_obj = get_enrichment_obj(event)
-    country_code = [i.get('country') for i in enrichment_obj if i['country'] in EMBARGO_COUNTRY_CODES][0]
+    country_code = [
+        i.get("country") for i in enrichment_obj if i["country"] in EMBARGO_COUNTRY_CODES
+    ][0]
     return f"Connection made to embargoed country: {country_code}."
 
+
 def alert_context(event):
-    if event.get('p_log_type') == 'Crowdstrike.FDREvent':
-        return crowdstrike_detection_alert_context(event) | {'p_any_ip_addresses': event.get('p_any_ip_addresses')}
-    else:
-        return { 'p_any_ip_addresses': event.get('p_any_ip_addresses')}
-    
+    if event.get("p_log_type") == "Crowdstrike.FDREvent":
+        return crowdstrike_detection_alert_context(event) | {
+            "p_any_ip_addresses": event.get("p_any_ip_addresses")
+        }
+
+    return {"p_any_ip_addresses": event.get("p_any_ip_addresses")}
