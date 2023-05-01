eaa11cdd58ff03b5dd993aa27820eda0482c793e
jzandona <jason.zandona@runpanther.io>
2023-05-01 12:52:47 -0700

    initial data model

---
+++ data_models/cisco_umbrella_data_model.py
@@ -0,0 +1,8 @@
+from panther_base_helpers import deep_get
+
+def get_dns_query(event):
+    # Strip trailing period. Domain Names from Cisco Umbrella end with a trailing period, such as google.com. 
+    domain = event.get('domain')
+    if domain:
+        domain = domain.rstrip(".")
+    return domain
