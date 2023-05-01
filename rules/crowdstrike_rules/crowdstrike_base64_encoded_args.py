0a5864f4ef6efb2dc81f391c7c10137a79c94ff5
jzandona <jason.zandona@runpanther.io>
2023-05-01 09:03:28 -0700

    test cases and tuning

--- rules/crowdstrike_rules/crowdstrike_base64_encoded_args.py
+++ rules/crowdstrike_rules/crowdstrike_base64_encoded_args.py
@@ -1,7 +1,6 @@
 from panther_base_helpers import crowdstrike_detection_alert_context
 import re
 
-
 def rule(event):
     # List of command line tools to monitor for execution with Base64 encoded arguments
     command_line_tools = {
@@ -15,24 +14,24 @@ def rule(event):
     # Define a regular expression pattern to match Base64 encoded strings
     base64_pattern = re.compile(r'[A-Za-z0-9+/]{10,}[=]{0,2}')
 
-    # Normalize the FileName to lower case for comparison
-    file_name = event.get("FileName", default="").lower()
-    command_line = event.get("CommandLine", default="").lower()
+    # Normalize the process name to lower case for comparison
+    process_name = event.udm('process_name').lower()
+    # Split process path from arguments
+    command_line_args = ' '.join(event.udm("cmd").split(' ')[1:])
 
-    # Check if the FileName matches any of the command line tools
+    # Check if the process name matches any of the command line tools
     # and if Base64 encoded arguments are present in the command line
-    if file_name in command_line_tools and base64_pattern.search(command_line):
+    if process_name in command_line_tools and base64_pattern.search(command_line_args):
         return True
-
     return False
 
 
 def title(event):
-    file_name = event.get("FileName", default="")
-    command_line = event.get("CommandLine", default="")
+    process_name = event.udm('process_name').lower()
+    command_line = event.udm("cmd").lower()
 
-    return f"Execution of Command Line Tool with Base64 Encoded Arguments: {file_name} - {command_line} "
+    return f"Execution of Command Line Tool with Base64 Encoded Arguments: {process_name} - {command_line} "
 
 
 def alert_context(event):
-    return crowdstrike_detection_alert_context
+    return crowdstrike_detection_alert_context(event)
