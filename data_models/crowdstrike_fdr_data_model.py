749b4eb7fe0ecf5301454a53e4bd0d23c334f638
jzandona <jason.zandona@runpanther.io>
2023-05-01 12:53:52 -0700

    corrected method

--- data_models/crowdstrike_fdr_data_model.py
+++ data_models/crowdstrike_fdr_data_model.py
@@ -4,7 +4,7 @@ def get_dns_query(event):
     # Strip trailing period. Domain Names from Crowdstrike FDR end with a trailing period, such as google.com. 
     domain = deep_get(event, 'event', 'DomainName', default=None)
     if domain:
-        domain = ".".join(domain.rstrip(".").split(".")[-2:]).lower()
+        domain = domain.rstrip(".").lower()
     return domain
 
 def get_process_name(event):
