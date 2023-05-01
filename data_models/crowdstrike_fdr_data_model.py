62862c2aae39b0e706e396b0ac8809456f31623c
jzandona <jason.zandona@runpanther.io>
2023-05-01 09:02:39 -0700

    added get_process_name

--- data_models/crowdstrike_fdr_data_model.py
+++ data_models/crowdstrike_fdr_data_model.py
@@ -6,3 +6,13 @@ def get_dns_query(event):
     if domain:
         domain = ".".join(domain.rstrip(".").split(".")[-2:]).lower()
     return domain
+
+def get_process_name(event):
+    platform = event.get('event_platform')
+    #Extract process name from path
+    #Win = \Device\HarddiskVolume2\Windows\System32\winlogon.exe
+    #Lin = /usr/bin/run-parts
+    #Mac = /usr/libexec/xpcproxy
+    if platform == 'Win':
+        return deep_get(event, 'event', 'ImageFileName').split('\\')[-1]
+    return deep_get(event, 'event', 'ImageFileName').split('/')[-1]
