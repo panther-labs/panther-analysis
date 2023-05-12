5e754e9c6a221debf6a200033e93a2403726ebce
Carrie Pascale <carrie.pascale@panther.io>
2023-05-12 14:33:42 -0400

    Adds Crowdstrike suspicious cron rule

---
+++ rules/crowdstrike_rules/crowdstrike_suspicious_cron.py
@@ -0,0 +1,56 @@
+import shlex
+from fnmatch import fnmatch
+
+from panther_base_helpers import (
+    deep_get,
+    filter_crowdstrike_fdr_event_type,
+    crowdstrike_detection_alert_context,
+)
+
+SUSPICIOUS_CRON_CMD_ARGS = {
+    # Running in unexpected locations
+    "/tmp/*",  # nosec
+    # Reaching out to the internet
+    "curl",
+    "dig",
+    "http?://*",
+    "nc",
+    "wget",
+}
+
+SUSPICIOUS_CRON_CMDS = {
+    # Passing arguments into /bin/sh
+    "*|*sh",
+    "*sh -c *",
+}
+
+
+def suspicious_cmd_pairs(command):
+    return any((fnmatch(command, c) for c in SUSPICIOUS_CRON_CMDS))
+
+
+def suspicious_cmd_args(command):
+    command_args = shlex.split(command.replace("'", "\\'"))  # escape single quotes
+    for cmd in command_args:
+        if any((fnmatch(cmd, c) for c in SUSPICIOUS_CRON_CMD_ARGS)):
+            return True
+    return False
+
+
+def rule(event):
+    if filter_crowdstrike_fdr_event_type(event, "ProcessRollup2"):
+        return False
+
+    command = deep_get(event, "event", "CommandLine")
+    if not command:
+        return False
+
+    return any([suspicious_cmd_args(command), suspicious_cmd_pairs(command)])
+
+
+def title(event):
+    return f"Suspicious cron found on [{deep_get(event,'aid'), '<UNKNOWN_HOST>'}]"
+
+
+def alert_context(event):
+    return crowdstrike_detection_alert_context(event)
