6c88020deb03c3017f0765ce46e37b7c0bd5c478
Ed Anderson <ed.anderson@panther.io>
2023-05-31 10:03:02 -0700

    fix: some cache ttls were getting stringified, which leads to dynamodb silently not expiring them

--- rules/slack_rules/slack_application_dos.py
+++ rules/slack_rules/slack_application_dos.py
@@ -53,4 +53,4 @@ def store_reset_info(key, event):
         ],
     )
     # Expire the entry after 24 hours
-    set_key_expiration(key, str((datetime.now() + timedelta(days=1)).timestamp()))
+    set_key_expiration(key, int((datetime.now() + timedelta(days=1)).timestamp()))
