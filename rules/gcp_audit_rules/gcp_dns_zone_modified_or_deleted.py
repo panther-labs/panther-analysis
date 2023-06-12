04963642db70c3634af6e6ca6b6708a05d9e41ca
Evan Gibler <evan.gibler@panther.com>
2023-06-12 17:26:16 -0500

    Add detections for GCP DNS zone operations (#779)

---
+++ rules/gcp_audit_rules/gcp_dns_zone_modified_or_deleted.py
@@ -0,0 +1,23 @@
+from panther_base_helpers import deep_get
+from gcp_base_helpers import gcp_alert_context
+
+
+def rule(event):
+    methods = (
+        "dns.changes.create",
+        "dns.managedZones.delete",
+        "dns.managedZones.patch",
+        "dns.managedZones.update",
+    )
+    return deep_get(event, "protoPayload", "methodName", default="") in methods
+
+
+def title(event):
+    actor = deep_get(event, "protoPayload", "authenticationInfo", "principalEmail", default="")
+    method = deep_get(event, "protoPayload", "methodName", default="")
+    resource = deep_get(event, "protoPayload", "resourceName", default="")
+    return f"[GCP] {actor} performed {method} on {resource}"
+
+
+def alert_context(event):
+    return gcp_alert_context(event)
