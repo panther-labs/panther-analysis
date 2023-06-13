cbffb3b7f88336a8d6fbd6eae5144b627e643cbb
Josh Esbrook <josh@panther.io>
2023-06-13 12:13:24 -0400

    Tines Rules 2x - Job Deletion

---
+++ rules/tines_rules/tines.enqueued_retrying.job.deletion.py
@@ -0,0 +1,8 @@
+def rule(event):
+    return event.get('operation_name') in ["JobsQueuedDeletion", "JobsRetryingDeletion"]
+
+def title(event):
+    operation = event.get('operation_name')
+    user = event.get('user_email')
+    tines_instance = event.get('p_source_label')
+    return f"Tines {operation} by {user} on {tines_instance}"
