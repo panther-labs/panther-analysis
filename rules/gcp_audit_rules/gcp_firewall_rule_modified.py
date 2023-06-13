a535dc8fc9379c2b5943facd33515df63858a00f
Evan Gibler <evan.gibler@panther.com>
2023-06-13 11:26:14 -0500

    Create detection for GCP firewall rule modifications (#785)
    * Create detection for GCP firewall rule modifications
    
    * Address PR comments
    
    * fmt

--- rules/gcp_audit_rules/gcp_firewall_rule_modified.py
+++ rules/gcp_audit_rules/gcp_firewall_rule_modified.py
@@ -1,15 +1,13 @@
+import re
+
 from gcp_base_helpers import gcp_alert_context
 from panther_base_helpers import deep_get
 
 
 def rule(event):
-    methods = (
-        "dns.changes.create",
-        "dns.managedZones.delete",
-        "dns.managedZones.patch",
-        "dns.managedZones.update",
-    )
-    return deep_get(event, "protoPayload", "methodName", default="") in methods
+    method_pattern = r"(?:\w+\.)*v1\.(?:Firewall\.Update)|(compute\.firewalls\.(patch|update))"
+    match = re.search(method_pattern, deep_get(event, "protoPayload", "methodName", default=""))
+    return match is not None
 
 
 def title(event):
@@ -17,7 +15,7 @@ def title(event):
         event, "protoPayload", "authenticationInfo", "principalEmail", default="<ACTOR_NOT_FOUND>"
     )
     resource = deep_get(event, "protoPayload", "resourceName", default="<RESOURCE_NOT_FOUND>")
-    return f"[GCP]: [{actor}] modified managed DNS zone [{resource}]"
+    return f"[GCP]: [{actor}] modified firewall rule on [{resource}]"
 
 
 def alert_context(event):
