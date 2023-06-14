e9374f30a810383a5d31c12414947b8309f95b38
Josh Esbrook <josh@panther.io>
2023-06-14 11:58:11 -0400

    add false case test

--- rules/tines_rules/tines_story_items_destruction.py
+++ rules/tines_rules/tines_story_items_destruction.py
@@ -7,7 +7,7 @@ def rule(event):
     if not filter_include_event(event):
         return False
 
-    return event.get("operation_name") == "StoryItemsDestruction"
+    return deep_get(event, "operation_name") == "StoryItemsDestruction"
 
 
 def title(event):
