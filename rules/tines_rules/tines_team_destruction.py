25017405709b0f01fc59602b835f80b05592d7c0
Josh Esbrook <josh@panther.io>
2023-06-14 11:02:58 -0400

    make fmt

--- rules/tines_rules/tines_team_destruction.py
+++ rules/tines_rules/tines_team_destruction.py
@@ -1,18 +1,21 @@
-from panther_base_helpers import deep_get
 from global_filter_tines import filter_include_event
+from panther_base_helpers import deep_get
 from panther_tines_helpers import tines_alert_context
+
 
 def rule(event):
     if not filter_include_event(event):
         return False
-    
-    return event.get('operation_name') == 'TeamDestruction'
+
+    return event.get("operation_name") == "TeamDestruction"
+
 
 def title(event):
-    operation = deep_get(event, 'operation_name', default='Unknown Operation')
-    user = deep_get(event, 'user_email', default='Unknown User')
-    tines_instance = deep_get(event, 'p_source_label', default='Unknown Tines Instance')
+    operation = deep_get(event, "operation_name", default="Unknown Operation")
+    user = deep_get(event, "user_email", default="Unknown User")
+    tines_instance = deep_get(event, "p_source_label", default="Unknown Tines Instance")
     return f"Tines [{operation}] by [{user}] on [{tines_instance}]"
+
 
 def alert_context(event):
     return tines_alert_context(event)
