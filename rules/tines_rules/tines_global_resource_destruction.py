61508b1247eaa6166e002ad97cb4f88f2fd86e07
Josh Esbrook <josh@panther.io>
2023-06-14 10:34:09 -0400

    deep get for title

--- rules/tines_rules/tines_global_resource_destruction.py
+++ rules/tines_rules/tines_global_resource_destruction.py
@@ -1,8 +1,10 @@
+from panther_base_helpers import deep_get
+
 def rule(event):
     return event.get('operation_name') == 'GlobalResourceDestruction'
 
 def title(event):
-    operation = event.get('operation_name')
-    user = event.get('user_email')
-    tines_instance = event.get('p_source_label')
+    operation = deep_get(event, 'operation_name', default='Unknown Operation')
+    user = deep_get(event, 'user_email', default='Unknown User')
+    tines_instance = deep_get(event, 'p_source_label', default='Unknown Tines Instance')
     return f"Tines [{operation}] by [{user}] on [{tines_instance}]"
