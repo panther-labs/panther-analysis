a80fb096abc3339796409ae857c30b93385b9618
Josh Esbrook <josh@panther.io>
2023-06-14 13:49:19 -0400

    linted

--- rules/tines_rules/tines_global_resource_destruction.py
+++ rules/tines_rules/tines_global_resource_destruction.py
@@ -7,13 +7,17 @@ def rule(event):
     if not filter_include_event(event):
         return False
 
-    return deep_get(event, "operation_name", default="NO_OPERATION_NAME") == "GlobalResourceDestruction"
+    return (
+        deep_get(event, "operation_name", default="NO_OPERATION_NAME")
+        == "GlobalResourceDestruction"
+    )
 
 
 def title(event):
     operation = deep_get(event, "operation_name", default="NO_OPERATION_NAME")
     user = deep_get(event, "user_email", default="NO_USER_EMAIL")
     tines_instance = deep_get(event, "p_source_label", default="NO_SOURCE_LABEL")
+
     return f"Tines [{operation}] by [{user}] on [{tines_instance}]"
 
 
