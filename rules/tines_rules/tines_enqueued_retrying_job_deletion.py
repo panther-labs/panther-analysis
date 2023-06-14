51426f926209b1976a80c8fe413c413ebf20a340
Josh Esbrook <josh@panther.io>
2023-06-14 10:14:42 -0400

    square brackets for var interpretation, fix file name

--- rules/tines_rules/tines_enqueued_retrying_job_deletion.py
+++ rules/tines_rules/tines_enqueued_retrying_job_deletion.py
@@ -5,4 +5,4 @@ def title(event):
     operation = event.get('operation_name')
     user = event.get('user_email')
     tines_instance = event.get('p_source_label')
-    return f"Tines {operation} by {user} on {tines_instance}"
+    return f"Tines [{operation}] by [{user}] on [{tines_instance}]"
