e2e8de98962389f0652dc09c25bd7babca3300b0
Josh Esbrook <josh@panther.io>
2023-06-14 10:45:58 -0400

    add tines alert context

--- rules/tines_rules/tines_story_items_destruction.py
+++ rules/tines_rules/tines_story_items_destruction.py
@@ -1,4 +1,5 @@
 from panther_base_helpers import deep_get
+from panther_tines_helpers import tines_alert_context
 
 def rule(event):
     return event.get('operation_name') == 'StoryItemsDestruction'
@@ -8,3 +9,6 @@ def title(event):
     user = deep_get(event, 'user_email', default='Unknown User')
     tines_instance = deep_get(event, 'p_source_label', default='Unknown Tines Instance')
     return f"Tines [{operation}] by [{user}] on [{tines_instance}]"
+
+def alert_context(event):
+    return tines_alert_context(event)
