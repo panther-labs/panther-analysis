d58dd7ac56c7fcc35c10d6b471d2fc3347975e06
Josh Esbrook <josh@panther.io>
2023-06-14 15:08:43 -0400

    default brackets, title as sentence

--- rules/tines_rules/tines_global_resource_destruction.py
+++ rules/tines_rules/tines_global_resource_destruction.py
@@ -8,17 +8,17 @@ def rule(event):
         return False
 
     return (
-        deep_get(event, "operation_name", default="NO_OPERATION_NAME")
+        deep_get(event, "operation_name", default="<NO_OPERATION_NAME>")
         == "GlobalResourceDestruction"
     )
 
 
 def title(event):
-    operation = deep_get(event, "operation_name", default="NO_OPERATION_NAME")
-    user = deep_get(event, "user_email", default="NO_USER_EMAIL")
-    tines_instance = deep_get(event, "p_source_label", default="NO_SOURCE_LABEL")
+    operation = deep_get(event, "operation_name", default="<NO_OPERATION_NAME>")
+    user = deep_get(event, "user_email", default="<NO_USER_EMAIL>")
+    tines_instance = deep_get(event, "p_source_label", default="<NO_SOURCE_LABEL>ß")
 
-    return f"Tines [{operation}] by [{user}] on [{tines_instance}]"
+    return f"Tines [{operation}] performed by [{user}] on [{tines_instance}]"
 
 
 def alert_context(event):
