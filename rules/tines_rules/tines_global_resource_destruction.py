8f584a00176c7c96ef7a66204e2233a7da9d830e
Josh Esbrook <josh@panther.io>
2023-06-14 10:10:06 -0400

    square brackets for var interpretation

--- rules/tines_rules/tines_global_resource_destruction.py
+++ rules/tines_rules/tines_global_resource_destruction.py
@@ -2,6 +2,7 @@ def rule(event):
     return event.get('operation_name') == 'GlobalResourceDestruction'
 
 def title(event):
+    operation = event.get('operation_name')
     user = event.get('user_email')
     tines_instance = event.get('p_source_label')
-    return f"Tines Global Resource Destruction by {user} on {tines_instance}"
+    return f"Tines [{operation}] by [{user}] on [{tines_instance}]"
