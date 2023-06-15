931d916401bde9c198df75b43f58b976ca518c30
Josh Esbrook <josh@panther.io>
2023-06-15 09:02:32 -0400

    PR requests, added to pack as per reviewer requirement

--- rules/tines_rules/tines_story_jobs_clearance.py
+++ rules/tines_rules/tines_story_jobs_clearance.py
@@ -7,14 +7,15 @@ def rule(event):
     if not filter_include_event(event):
         return False
 
-    return deep_get(event, "operation_name") == "StoryJobsClearance"
+    return deep_get(event, "operation_name", default="<NO_OPERATION_NAME>") == "StoryJobsClearance"
 
 
 def title(event):
-    operation = deep_get(event, "operation_name", default="Unknown Operation")
-    user = deep_get(event, "user_email", default="Unknown User")
-    tines_instance = deep_get(event, "p_source_label", default="Unknown Tines Instance")
-    return f"Tines [{operation}] by [{user}] on [{tines_instance}]"
+    operation = deep_get(event, "operation_name", default="<NO_OPERATION_NAME>")
+    user = deep_get(event, "user_email", default="<NO_USER_EMAIL>")
+    tines_instance = deep_get(event, "p_source_label", default="<NO_SOURCE_LABEL>")
+
+    return f"Tines [{operation}] performed by [{user}] on [{tines_instance}]."
 
 
 def alert_context(event):
