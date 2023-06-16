a508165f60d439007fc0326d38bab3427546ba5a
Alex Mylonas <alex.a.mylonas@gmail.com>
2023-06-16 16:33:17 +0300

    fix: lint

--- rules/auth0_rules/auth0_custom_role_created.py
+++ rules/auth0_rules/auth0_custom_role_created.py
@@ -2,22 +2,26 @@ from global_filter_auth0 import filter_include_event
 from panther_auth0_helpers import auth0_alert_context, is_auth0_config_event
 from panther_base_helpers import deep_get
 
+
 def rule(event):
     if not filter_include_event(event):
         return False
     data_description = deep_get(event, "data", "description", default="<NO_DATA_DESCRIPTION_FOUND>")
-    return all([
+    return all(
+        [
             data_description == "Create a role",
             is_auth0_config_event(event),
         ]
     )
 
-def title (event):
+
+def title(event):
     user = deep_get(
         event, "data", "details", "request", "auth", "user", "email", default="<NO_USER_FOUND>"
     )
-    request_body_name = deep_get(event, "data", "details", "request", "body", "name",
-        default="<NO_REQUEST_NAME_FOUND>")
+    request_body_name = deep_get(
+        event, "data", "details", "request", "body", "name", default="<NO_REQUEST_NAME_FOUND>"
+    )
     request_body_description = deep_get(event, "data", "details", "request", "body", default=[-1])
 
     if "admin" in request_body_description or "admin" in request_body_name:
@@ -27,20 +31,24 @@ def title (event):
 
     p_source_label = deep_get(event, "p_source_label", default="<NO_P_SOURCE_LABEL_FOUND>")
     return (
-		f"Auth0 User [{user}] created a "
-		f"role [{request_body_name}] with [{role_type}] "
-		f"permissions in your tenant [{p_source_label}]."
-	)
+        f"Auth0 User [{user}] created a "
+        f"role [{request_body_name}] with [{role_type}] "
+        f"permissions in your tenant [{p_source_label}]."
+    )
+
 
 def severity(event):
-    request_body_name = deep_get(event, "data", "details", "request", "body", "name", 
-        default="<NO_REQUEST_NAME_FOUND>")
-    request_body_description = deep_get(event, "data", "details", "request", "body", 
-        "description", default=[-1])
+    request_body_name = deep_get(
+        event, "data", "details", "request", "body", "name", default="<NO_REQUEST_NAME_FOUND>"
+    )
+    request_body_description = deep_get(
+        event, "data", "details", "request", "body", "description", default=[-1]
+    )
     if "admin" in request_body_description or "admin" in request_body_name:
         return "MEDIUM"
     else:
         return "LOW"
+
 
 def alert_context(event):
     return auth0_alert_context(event)
