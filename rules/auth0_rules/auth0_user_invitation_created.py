a35d974e25db7a70958ade7fa7959c198b670337
Jim Kalafut <jim.kalafut@panther.com>
2023-06-16 13:22:26 -0700

    Apply suggestions from code review
    Co-authored-by: Ed⁦ <ed.anderson@panther.io>

--- rules/auth0_rules/auth0_user_invitation_created.py
+++ rules/auth0_rules/auth0_user_invitation_created.py
@@ -8,7 +8,10 @@ org_re = re.compile(r"^/api/v2/organizations/[^/\s]+/invitations$")
 
 
 def rule(event):
-    if not filter_include_event(event) or not is_auth0_config_event(event):
+    if not any([
+       filter_include_event(event),
+       is_auth0_config_event(event)
+       ]):
         return False
 
     return invitation_type(event) is not None
@@ -23,13 +26,13 @@ def title(event):
     else:
         raise Exception(f"Unexpected invitation type {inv_type}")
 
-    inviter = deep_get(event, "data", "details", "request", "auth", "user", "email")
-    source = deep_get(event, "p_source_label", default="unknown")
+    inviter = deep_get(event, "data", "details", "request", "auth", "user", "email", default="<NO_INVITER>")
+    source = deep_get(event, "p_source_label", default="<NO_PSOURCE>")
     return f"Auth0 User [{inviter}] invited [{invitee}] to {inv_type.title()} [{source}]]"
 
 
 def invitation_type(event):
-    path = deep_get(event, "data", "details", "request", "path")
+    path = deep_get(event, "data", "details", "request", "path", default="")
 
     if path == "/api/v2/tenants/invitations":
         return "tenant"
