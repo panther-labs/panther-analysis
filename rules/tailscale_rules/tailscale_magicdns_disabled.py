a48c03466c21b2472f4d2e307cc18717328b3d0b
andrea-youwakim <andrea.youwakim@panther.io>
2023-07-19 13:27:11 -0600

    fix: fmt

--- rules/tailscale_rules/tailscale_magicdns_disabled.py
+++ rules/tailscale_rules/tailscale_magicdns_disabled.py
@@ -1,13 +1,16 @@
 from global_filter_tailscale import filter_include_event
-from panther_tailscale_helpers import tailscale_alert_context, is_tailscale_admin_console_event
 from panther_base_helpers import deep_get
+from panther_tailscale_helpers import (
+    is_tailscale_admin_console_event,
+    tailscale_alert_context,
+)
 
 
 def rule(event):
     if not filter_include_event(event):
         return False
     action = deep_get(event, "action", default="<NO_ACTION_FOUND>")
-    target_property = deep_get(event, "target", "property",  default="<NO_TARGET_PROPERTY_FOUND>")
+    target_property = deep_get(event, "target", "property", default="<NO_TARGET_PROPERTY_FOUND>")
     return all(
         [
             action == "DISABLE",
@@ -17,10 +20,9 @@ def rule(event):
     )
 
 
-
 def title(event):
-    user = deep_get(event, "actor", "loginName",  default="<NO_USER_FOUND>")
-    target_id = deep_get(event, "target", "id",  default="<NO_TARGET_ID_FOUND>")
+    user = deep_get(event, "actor", "loginName", default="<NO_USER_FOUND>")
+    target_id = deep_get(event, "target", "id", default="<NO_TARGET_ID_FOUND>")
     return (
         f"Tailscale user [{user}] disabled Magic DNS for "
         f"[{target_id}] in your organization’s tenant."
