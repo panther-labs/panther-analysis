7740c514feab161595970231f05c2f92fc2d9fbc
Pierce Douglas <9985798+piercedouglas@users.noreply.github.com>
2023-09-07 16:46:41 -0600

    Check for GuardDuty sample data

--- rules/aws_guardduty_rules/aws_guardduty_low_sev_findings.py
+++ rules/aws_guardduty_rules/aws_guardduty_low_sev_findings.py
@@ -2,7 +2,9 @@ from panther_base_helpers import aws_guardduty_context
 
 
 def rule(event):
-    return 0.1 <= float(event.get("severity", 0)) <= 3.9
+    if deep_get(event, "additionalInfo", "sample") == True:
+        return False # in case of sample data https://docs.aws.amazon.com/guardduty/latest/ug/sample_findings.html
+    else return 0.1 <= float(event.get("severity", 0)) <= 3.9
 
 
 def dedup(event):
