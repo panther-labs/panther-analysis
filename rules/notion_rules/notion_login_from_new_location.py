33712e909fd1d4df5d0bb4c10c0d22905d1a9647
Ben Airey <benjaminjohnairey@gmail.com>
2023-09-15 17:24:59 -0500

    made pylint happy(er)

--- rules/notion_rules/notion_login_from_new_location.py
+++ rules/notion_rules/notion_login_from_new_location.py
@@ -1,70 +1,67 @@
+import time
 from global_filter_notion import filter_include_event
 from panther_notion_helpers import notion_alert_context
 from panther_oss_helpers import get_string_set, put_string_set, set_key_expiration
 from panther_ipinfo_helpers import IPInfoLocation
 
-import time
-
 # How long (in seconds) to keep previous login locations in cached memory
 DEFAULT_CACHE_PERIOD = 2419200
+
 
 def rule(event):
     if not filter_include_event(event):
         return False
-    
+
     # Only focused on login events
-    if event.deep_walk('type') != 'user.login':
+    if event.deep_walk("type") != "user.login":
         return False
-    
+
     # Get the user's location, via IPInfo
-    global ipinfo_loc
-    ipinfo_loc = IPInfoLocation(event)
-    path_to_ip = 'ip_address'
-    city = ipinfo_loc.city(path_to_ip)
-    region = ipinfo_loc.region(path_to_ip)
-    country = ipinfo_loc.country(path_to_ip)
-    loc_string = '_'.join((city, region, country))
+    # pylint: disable=global-variable-undefined
+    global IPINFO_LOC
+    IPINFO_LOC = IPInfoLocation(event)
+    path_to_ip = "ip_address"
+    city = IPINFO_LOC.city(path_to_ip)
+    region = IPINFO_LOC.region(path_to_ip)
+    country = IPINFO_LOC.country(path_to_ip)
+    loc_string = "_".join((city, region, country))
 
     # Store the login location. The premise is to create a new entry for each combimation of user
-    # and location, and then have those records persist for some length of time (4 weeks by 
+    # and location, and then have those records persist for some length of time (4 weeks by
     # default).
-    user = event.deep_walk('actor', 'id')
+    user = event.deep_walk("actor", "id")
     cache_key = f"{user} {loc_string}"
     # Check if this key already exists
     if get_string_set(cache_key):
         # User has logged in from this location recently. Let's refresh this login key.
-        set_key_expiration(cache_key, time.time()+DEFAULT_CACHE_PERIOD)
-        return False # No need to alert - user has logged in from here before.
-    else:
-        # This is a location the user hasn't recently used
-        put_string_set(cache_key, ["arbitrary value"])
-        set_key_expiration(cache_key, time.time()+DEFAULT_CACHE_PERIOD)
-        return True
+        set_key_expiration(cache_key, time.time() + DEFAULT_CACHE_PERIOD)
+        return False  # No need to alert - user has logged in from here before.
+
+    # Else, his is a location the user hasn't recently used
+    put_string_set(cache_key, ["arbitrary value"])
+    set_key_expiration(cache_key, time.time() + DEFAULT_CACHE_PERIOD)
+    return True
 
 
 def title(event):
-    path_to_ip = 'ip_address'
-    city = ipinfo_loc.city(path_to_ip)
-    region = ipinfo_loc.region(path_to_ip)
-    country = ipinfo_loc.country(path_to_ip)
+    path_to_ip = "ip_address"
+    city = IPINFO_LOC.city(path_to_ip)
+    region = IPINFO_LOC.region(path_to_ip)
+    country = IPINFO_LOC.country(path_to_ip)
 
-    user_email = event.deep_walk('actor', 'person', 'email', default='UNKNWON_EMAIL')
-    return f'[{user_email}] logged in from a new location: {city}, {region}, {country}.'
+    user_email = event.deep_walk("actor", "person", "email", default="UNKNWON_EMAIL")
+    return f"[{user_email}] logged in from a new location: {city}, {region}, {country}."
 
 
 def alert_context(event):
-    path_to_ip = 'ip_address'
-    city = ipinfo_loc.city(path_to_ip)
-    region = ipinfo_loc.region(path_to_ip)
-    country = ipinfo_loc.country(path_to_ip)
-    user_email = user_email = event.deep_walk('actor', 'person', 'email', default='UNKNWON_EMAIL')
+    path_to_ip = "ip_address"
+    city = IPINFO_LOC.city(path_to_ip)
+    region = IPINFO_LOC.region(path_to_ip)
+    country = IPINFO_LOC.country(path_to_ip)
+    user_email = user_email = event.deep_walk("actor", "person", "email", default="UNKNWON_EMAIL")
 
     context = notion_alert_context(event)
-    context['user_email'] = user_email
-    context['location'] = {
-        'city': city,
-        'region': region,
-        'country': country
-    }
+    context["user_email"] = user_email
+    context["location"] = {"city": city, "region": region, "country": country}
 
     return context
--- rules/notion_rules/notion_password_change_after_login.py
+++ rules/notion_rules/notion_password_change_after_login.py
@@ -10,7 +10,8 @@ DEFAULT_PASSWORD_CHANGE_WINDOW_MINUTES = 10
 
 # Prefix for cached key. This ensures we don't accidently tamper with cached data from other
 # detections.
-CACHE_PREFIX = 'Notion.PasswordChangeAfterLogin'
+CACHE_PREFIX = "Notion.PasswordChangeAfterLogin"
+
 
 def rule(event):
     if not filter_include_event(event):
@@ -18,23 +19,24 @@ def rule(event):
 
     # If this is neither a login, nor password change event, then exit
     allowed_event_types = {
-        'user.login',
-        'user.settings.login_method.password_updated',
-        'user.settings.login_method.password_added'
+        "user.login",
+        "user.settings.login_method.password_updated",
+        "user.settings.login_method.password_added",
     }
-    if event.deep_walk('type') not in allowed_event_types:
+    if event.deep_walk("type") not in allowed_event_types:
         return False
 
     # Extract user info
-    userid = event.deep_walk('actor', 'id')
+    userid = event.deep_walk("actor", "id")
     cache_key = f"{CACHE_PREFIX}-{userid}"
 
     # If this is a login event, record it
-    if event.deep_walk('type') == 'user.login':
+    if event.deep_walk("type") == "user.login":
         put_string_set(
             cache_key,
-            [str(event.get('p_event_time'))], # We'll save this for the alert context later
-            time.time()+DEFAULT_PASSWORD_CHANGE_WINDOW_MINUTES*60)
+            [str(event.get("p_event_time"))],  # We'll save this for the alert context later
+            time.time() + DEFAULT_PASSWORD_CHANGE_WINDOW_MINUTES * 60,
+        )
         return False
 
     # If we made it here, then this is a password change event
@@ -42,18 +44,20 @@ def rule(event):
     if last_login := get_string_set(cache_key, force_ttl_check=True):
         # pylint: disable=global-variable-undefined
         global LOGIN_TS
-        LOGIN_TS = list(last_login)[0] # Save the last login timestamp for the alert context
+        LOGIN_TS = list(last_login)[0]  # Save the last login timestamp for the alert context
         return True
     # If they haven't logged in recently, then return false
     return False
 
+
 def title(event):
-    user_email = event.deep_walk('actor', 'person', 'email', default="UNKNOWN EMAIL")
+    user_email = event.deep_walk("actor", "person", "email", default="UNKNOWN EMAIL")
     mins = DEFAULT_PASSWORD_CHANGE_WINDOW_MINUTES
     return f"User [{user_email}] changed their password within [{mins}] minutes of logging in."
+
 
 def alert_context(event):
     context = notion_alert_context(event)
     global LOGIN_TS
-    context['login_timestamp'] = LOGIN_TS
+    context["login_timestamp"] = LOGIN_TS
     return context
