4c8bba15bc43d45f4877d50ba2751a90c3bf62c6
George Papanikolaou <g3orge.app@gmail.com>
2023-09-19 11:12:31 +0300

    Migrate Azure data model and pack as well

--- data_models/azure_signin_data_model.py
+++ data_models/azure_signin_data_model.py
@@ -1,16 +1,16 @@
 import panther_event_type_helpers as event_type
-from panther_azuresignin_helpers import actor_user
+from panther_azuresignin_helpers import actor_user, is_signin_event
 from panther_base_helpers import deep_get
 
 
 def get_event_type(event):
-    operation = deep_get(event, "operationName", default="")
+    if not is_signin_event(event):
+        return None
+
     error_code = deep_get(event, "properties", "status", "errorCode", default=0)
-    if operation == "Sign-in activity":
-        if error_code == 0:
-            return event_type.SUCCESSFUL_LOGIN
-        return event_type.FAILED_LOGIN
-    return None
+    if error_code == 0:
+        return event_type.SUCCESSFUL_LOGIN
+    return event_type.FAILED_LOGIN
 
 
 def get_actor_user(event):
