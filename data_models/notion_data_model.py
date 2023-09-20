cddcdbef5e3a1a4b906cf10459ae07f78ce0cce6
Ben Airey <benjaminjohnairey@gmail.com>
2023-09-20 17:50:37 -0500

    More fixes to data model

--- data_models/notion_data_model.py
+++ data_models/notion_data_model.py
@@ -1,4 +1,5 @@
 import panther_event_type_helpers as event_type
+from panther_base_helpers import deep_get
 
 
 def get_event_type(event):
@@ -13,11 +14,11 @@ def get_event_type(event):
         'user.settings.preferred_name_updated': event_type.USER_ACCOUNT_MODIFIED,
         'user.settings.profile_photo_updated': event_type.USER_ACCOUNT_MODIFIED,
         'workspace.permissions.member_role_updated': event_type.USER_ROLE_MODIFIED
-    }.get(event.deep_get("event", "type"))
+    }.get(deep_get(event, "event", "type"))
 
 
 def get_actor_user(event):
-    actor = event.deep_get("event", "actor", "id", default="UNKNOWN USER")
-    if event.deep_get("event", "actor", "person"):
-        actor = event.deep_get("event", "actor", "person", "email", default="UNKNOWN USER")
+    actor = deep_get(event, "event", "actor", "id", default="UNKNOWN USER")
+    if deep_get(event, "event", "actor", "person"):
+        actor = deep_get(event, "event", "actor", "person", "email", default="UNKNOWN USER")
     return actor
