8227d0ff8be0244358d0e66ac0256cc5f4db5c85
Ben Airey <benjaminjohnairey@gmail.com>
2023-10-05 17:41:52 -0500

    new rule: login from blocked IP

---
+++ rules/notion_rules/notion_login_from_blocked_ip.py
@@ -0,0 +1,19 @@
+from global_filter_notion import filter_include_event
+from panther_notion_helpers import notion_alert_context
+
+
+def rule(event):
+    # Users can specify inline-filters to permit rules based on IPs
+    if not filter_include_event(event):
+        return False
+    return event.deep_get("event", "type", default="<NO_EVENT_TYPE_FOUND>") == "user.login"
+
+
+def title(event):
+    user = event.deep_get("event", "event", "actor", "person", "email", default="<NO_USER_FOUND>")
+    page_id = event.deep_get("event", "details", "target", "page_id", default="<NO_PAGE_ID_FOUND>")
+    return f"Notion User [{user}] exported a multiple pages with page ids [{page_id}]."
+
+
+def alert_context(event):
+    return notion_alert_context(event)
