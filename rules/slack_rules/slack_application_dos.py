91f5b1db052690564ccc7f7479095fefb758e9f3
Max Richmond <46904505+maxrichie5@users.noreply.github.com>
2023-10-23 11:24:24 -0600

    replacing set_key_expiration with epoch_seconds field (#892)
    * using caching module in detections instead of globals
    
    * disabling broad exception since that is old code but a new error
    
    * replacing set_key_expiration with new epoch seconds field
    
    * updates
    
    * updating PDH version
    
    ---------
    
    Co-authored-by: maxrichie5 <maxrichmond@panther.com>

--- rules/slack_rules/slack_application_dos.py
+++ rules/slack_rules/slack_application_dos.py
@@ -1,11 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from json import dumps
 
 from panther_base_helpers import deep_get, slack_alert_context
 from panther_detection_helpers.caching import (
     get_string_set,
     put_string_set,
-    set_key_expiration,
 )
 
 DENIAL_OF_SERVICE_ACTIONS = [
@@ -55,6 +54,5 @@ def store_reset_info(key, event):
                 }
             )
         ],
+        epoch_seconds=event.event_time_epoch() + timedelta(days=1).total_seconds(),
     )
-    # Expire the entry after 24 hours
-    set_key_expiration(key, int((datetime.now() + timedelta(days=1)).timestamp()))
