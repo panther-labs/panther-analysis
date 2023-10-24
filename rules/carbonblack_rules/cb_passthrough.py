69f361d26a2eb14c07588db2693682b293683d47
Ariel Ropek <ariel.ropek@panther.com>
2023-10-24 11:32:57 -0600

    carbonblack passthrough rule draft

---
+++ rules/carbonblack_rules/cb_passthrough.py
@@ -0,0 +1,34 @@
+def rule(event):
+    return event.deep_get("workflow", "changed_by") == "ALERT_CREATION"
+
+
+def title(event):
+    f"{event.get('attack_tactic', 'CB')}:"
+    f"{event.get('device_username', '<no-user-found>')} on "
+    f"{event.get('device_name', '<no-device-found>')}: "
+    f"{event.get('reason', '<no-reason-found>')}"
+
+
+def description(event):
+    return
+
+
+def severity(event):
+    sev = event.get("severity")
+    if sev >= 9:
+        return "CRITICAL"
+    if sev >= 7:
+        return "HIGH"
+    if sev >= 5:
+        return "MEDIUM"
+    if sev >= 3:
+        return "LOW"
+    return "INFO"
+
+
+def reference(event):
+    return event.get("alert_url")
+
+
+def dedup(event):
+    return event.get("id")
