ecf457ca188afd2f42776afdb6358a5adbde2532
Adam Panzer <apanzerj@gmail.com>
2023-10-30 09:01:26 -0700

    Set severity to INFO if making calendar private (#904)
    * Set severity to INFO if making calendar private
    
    https://developers.google.com/admin-sdk/reports/v1/appendix/activity/calendar#change_calendar_acls
    
    None means nobody can see your calendar. This should just be info level.
    
    * Tweak title too
    
    Update title too
    
    * Add test
    
    * make fmt

--- rules/gsuite_activityevent_rules/gsuite_calendar_made_public.py
+++ rules/gsuite_activityevent_rules/gsuite_calendar_made_public.py
@@ -13,5 +13,14 @@ def title(event):
     return (
         f"GSuite calendar "
         f"[{deep_get(event, 'parameters', 'calendar_id', default='<NO_CALENDAR_ID>')}] made "
-        f"public by [{deep_get(event, 'actor', 'email', default='<NO_ACTOR_FOUND>')}]"
+        f"{public_or_private(event)} by "
+        f"[{deep_get(event, 'actor', 'email', default='<NO_ACTOR_FOUND>')}]"
     )
+
+
+def severity(event):
+    return "LOW" if public_or_private(event) == "private" else "MEDIUM"
+
+
+def public_or_private(event):
+    return "private" if deep_get(event, "parameters", "access_level") == "none" else "public"
