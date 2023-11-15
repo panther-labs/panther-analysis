418c763fe4eb5715dcf47a28f74bf3a59f656c94
Ariel Ropek <ariel.ropek@panther.com>
2023-11-15 14:16:14 -0700

    exclude placeholder buckets from policy alerts

--- policies/aws_cloudtrail_policies/aws_cloudtrail_s3_bucket_public.py
+++ policies/aws_cloudtrail_policies/aws_cloudtrail_s3_bucket_public.py
@@ -7,7 +7,49 @@ BAD_PERMISSIONS = {
 }
 
 
+# https://docs.aws.amazon.com/codepipeline/latest/userguide/reference-ct-placeholder-buckets.html
+PLACEHOLDER_BUCKETS = {
+    "codepipeline-cloudtrail-placeholder-bucket-us-east-2",
+    "codepipeline-cloudtrail-placeholder-bucket-us-east-1",
+    "codepipeline-cloudtrail-placeholder-bucket-us-west-1",
+    "codepipeline-cloudtrail-placeholder-bucket-us-west-2",
+    "codepipeline-cloudtrail-placeholder-bucket-ca-central-1",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-central-1",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-west-1",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-west-2",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-west-3",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-north-1",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-east-1",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-south-2",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-southeast-3",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-southeast-4",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-south-1",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-northeast-3-prod",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-northeast-1",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-northeast-2",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-southeast-1",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-southeast-2",
+    "codepipeline-cloudtrail-placeholder-bucket-ap-northeast-1",
+    "codepipeline-cloudtrail-placeholder-bucket-ca-central-1",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-central-1",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-west-1",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-west-2",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-south-1",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-west-3",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-south-2",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-north-1",
+    "codepipeline-cloudtrail-placeholder-bucket-eu-central-2",
+    "codepipeline-cloudtrail-placeholder-bucket-il-central-1",
+    "codepipeline-cloudtrail-placeholder-bucket-me-south-1",
+    "codepipeline-cloudtrail-placeholder-bucket-me-central-1",
+    "codepipeline-cloudtrail-placeholder-bucket-sa-east-1"
+}
+
+
 def policy(resource):
+    if resource["S3BucketName"] in PLACEHOLDER_BUCKETS:
+        return True
+
     bucket_arn = "arn:aws:s3:::" + resource["S3BucketName"]
 
     try:
