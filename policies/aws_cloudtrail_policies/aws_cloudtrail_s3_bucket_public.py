46c08abc6d14c9e4b8614ee173aa609e310b2fbb
egibs <keybase@egibs.xyz>
2023-11-15 15:08:45 -0600

    Add EXCLUDED_BUCKET_NAMES set for aws_cloudtrail_s3_bucket_public.py policy

--- policies/aws_cloudtrail_policies/aws_cloudtrail_s3_bucket_public.py
+++ policies/aws_cloudtrail_policies/aws_cloudtrail_s3_bucket_public.py
@@ -1,9 +1,14 @@
 from panther_base_helpers import deep_get
+from panther_default import aws_regions
 from panther_oss_helpers import BadLookup, resource_lookup
 
 BAD_PERMISSIONS = {
     "http://acs.amazonaws.com/groups/global/AuthenticatedUsers",
     "http://acs.amazonaws.com/groups/global/AllUsers",
+}
+
+EXCLUDED_BUCKET_NAMES = {
+    f"codepipeline-cloudtrail-placeholder-bucket-{region}" for region in aws_regions()
 }
 
 
@@ -16,7 +21,9 @@ def policy(resource):
         return True
 
     for grant in bucket["Grants"] or []:
-        if deep_get(grant, "Grantee", "URI") in BAD_PERMISSIONS:
+        if deep_get(grant, "Grantee", "URI") in BAD_PERMISSIONS and not any(
+            bucket_name in bucket_arn for bucket_name in EXCLUDED_BUCKET_NAMES
+        ):
             return False
 
     return True
