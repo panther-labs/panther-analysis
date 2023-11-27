1f8841b559ba1015ba94370dbe0c32ed9a6e58cc
Jonathan Lassoff <jof@thejof.com>
2023-11-27 09:03:53 -0800

    Teleport: Update Rules (#966)
    * Teleport: Update Rules
    
    * Update global_helpers/panther_default.py
    
    * Update global_helpers/panther_default.py
    
    * Update rules/gravitational_teleport_rules/teleport_long_lived_certs.py
    
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
    
    * Update rules/gravitational_teleport_rules/teleport_long_lived_certs.py
    
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
    
    * Update rules/gravitational_teleport_rules/teleport_long_lived_certs.py
    
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
    
    * Update rules/gravitational_teleport_rules/teleport_long_lived_certs.py
    
    * Update rules/gravitational_teleport_rules/teleport_long_lived_certs.py
    
    * Update rules/gravitational_teleport_rules/teleport_long_lived_certs.py
    
    * Update rules/gravitational_teleport_rules/teleport_long_lived_certs.py
    
    appease fmt
    
    * Update rules/gravitational_teleport_rules/teleport_long_lived_certs.py
    
    ---------
    
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>

--- rules/gravitational_teleport_rules/teleport_auth_errors.py
+++ rules/gravitational_teleport_rules/teleport_auth_errors.py
@@ -5,5 +5,6 @@ def rule(event):
 def title(event):
     return (
         f"A high volume of SSH errors was detected from user "
-        f"[{event.get('user', '<UNKNOWN_USER>')}]"
+        f"[{event.get('user', '<UNKNOWN_USER>')}] "
+        f"on [{event.get('cluster_name', '<UNKNOWN_CLUSTER>')}]"
     )
