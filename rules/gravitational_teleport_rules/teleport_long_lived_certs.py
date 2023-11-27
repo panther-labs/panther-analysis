f2eee0c87b993671a2b795cbc2d881100086dca0
Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
2023-11-27 09:48:32 -0700

    Update rules/gravitational_teleport_rules/teleport_long_lived_certs.py

--- rules/gravitational_teleport_rules/teleport_long_lived_certs.py
+++ rules/gravitational_teleport_rules/teleport_long_lived_certs.py
@@ -73,7 +73,7 @@ def validity_interval(event):
 def title(event):
     identity = event.deep_get('identity', 'user', default='<Cert with no User!?>')
     return (
-        f"A Certificate for  [{event.deep_get('identity', 'user', default='<Cert with no User!?>')}] "
+        f"A Certificate for [{identity}] "
         f"on [{event.get('cluster_name', '<UNKNOWN_CLUSTER>')}] "
         f"has been issued for an unusually long time: {validity_interval(event)!r} "
     )
