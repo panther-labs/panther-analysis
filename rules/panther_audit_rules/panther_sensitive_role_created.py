1c0bdfc1c571038c0b822324ed970f2348f10251
Ben Airey <benjaminjohnairey@gmail.com>
2023-12-05 17:59:18 -0600

    add handling for dynamic params
    Panther Audit rules now handle cases where audit logs have the `dynamic`
    input field, and cases where they don't (legacy). Also updated some test
    cases to include the field.

--- rules/panther_audit_rules/panther_sensitive_role_created.py
+++ rules/panther_audit_rules/panther_sensitive_role_created.py
@@ -17,7 +17,10 @@ PANTHER_ROLE_ACTIONS = [
 def rule(event):
     if event.udm("event_type") not in PANTHER_ROLE_ACTIONS:
         return False
-    role_permissions = set(deep_get(event, "actionParams", "input", "permissions", default=""))
+    permissions = deep_get(event, "actionParams", "dynamic", "input", "permissions")
+    if permissions == None:
+        deep_get(event, "actionParams", "input", "permissions", default="")
+    role_permissions = set(permissions)
 
     return (
         len(set(PANTHER_ADMIN_PERMISSIONS).intersection(role_permissions)) > 0
@@ -26,9 +29,12 @@ def rule(event):
 
 
 def title(event):
+    role_name = deep_get(event, "actionParams", "dynamic", "input" ,"name")
+    if role_name == None:
+        role_name = deep_get(event, "actionParams", "input" ,"name", default="<UNKNWON ROLE>")
     return (
         f"Role with Admin Permissions created by {event.udm('actor_user')}"
-        f"Role Name: {deep_get(event, 'actionParams', 'input' ,'name')}"
+        f"Role Name: {role_name}"
     )
 
 
