e60b7c75bc6864a879f27a9686c776be62a02ef1
Evan Gibler <evan.gibler@panther.com>
2024-02-13 12:30:36 -0600

    [sync] GitHub Data Model Admin Actions update (#79) (#1104)
    * action = 'team.add_repository' and perm = 'admin'
    
    * fmt
    
    ---------
    
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>

--- data_models/github_data_model.py
+++ data_models/github_data_model.py
@@ -6,14 +6,25 @@ ADMIN_EVENTS = {
     "team.promote_maintainer",
 }
 
+CONDITIONAL_ADMIN_EVENTS = {
+    "team.add_repository",
+}
+
 
 def get_admin_role(event):
     action = event.get("action", "")
+    permission = event.get("permission", "")
+    if action in CONDITIONAL_ADMIN_EVENTS and permission == "admin":
+        return action
     return action if action in ADMIN_EVENTS else "<UNKNOWN_ADMIN_ROLE>"
 
 
 def get_event_type(event):
-    if event.get("action", "") in ADMIN_EVENTS:
+    action = event.get("action", "")
+    permission = event.get("permission", "")
+    if action in ADMIN_EVENTS:
+        return event_type.ADMIN_ROLE_ASSIGNED
+    if action in CONDITIONAL_ADMIN_EVENTS and permission == "admin":
         return event_type.ADMIN_ROLE_ASSIGNED
     if event.get("action", "") == "org.disable_two_factor_requirement":
         return event_type.MFA_DISABLED
