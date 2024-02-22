cc32ff1e78a23c29ae5100cbb91439c6167c12cf
egibs <keybase@egibs.xyz>
2024-02-22 07:45:06 -0600

    Simplify script with modified command from #1109

--- format.sh
+++ format.sh
@@ -1,12 +1,4 @@
+# Small script that removes trailing whitespace from files in the repository
 #!/bin/sh
 
-# Small script that removes trailing whitespace from files in the repository
-
-OS=$(uname)
-for extension in "*.py" "*.yml"; do
-    case $OS in
-        Darwin) find . -type f -not -path '.git' -iname $extension -print0 | xargs -0 sed -i '' -E "s/[[:space:]]*\$//";;
-        Linux) find . -type f -not -path '.git' -iname $extension -print0 | xargs -0 sed -i -E "s/[[:space:]]*\$//";;
-        *) echo "Unsupported OS: $OSTYPE"; exit 1;;
-    esac
-done
+find .[a-z]* * -type f -print0 | grep -zZv '^.git/' | xargs -0 perl -pi -e 's/[ \t]+$//'
