5545344634ec023f69a7f6c82201f7c2f97f1ce3
egibs <keybase@egibs.xyz>
2024-02-27 09:58:25 -0600

    PR suggestions

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
 # Small script that removes trailing whitespace from files in the repository
 #!/bin/sh
 
-find .[a-z]* * -type f -print0 | grep -zZv '^.git/' | xargs -0 perl -pi -e 's/[ \t]+$//'
+find .[a-z]* * -type f -print0 | grep -zZv '^.git/' | xargs -0 perl -pi -e 's/[\x09\x20]+$//'
