f47ea5967af24945773c32920c5dfbc8d74a40d1
akozlovets098 <an.kozlovets@gmail.com>
2024-03-04 11:41:46 +0200

    Convert k8s rules from SDYAML to python

---
+++ rules/gcp_k8s_rules/gcp_k8s_cron_job_created_or_modified.py
@@ -0,0 +1,28 @@
+from gcp_base_helpers import gcp_alert_context
+from panther_base_helpers import deep_walk, deep_get
+
+
+def rule(event):
+    authorization_info = deep_walk(event, "protoPayload", "authorizationInfo")
+    if not authorization_info:
+        return False
+    for auth in authorization_info:
+        if (
+            auth.get("permission") in ["io.k8s.batch.v1.cronjobs.create", "io.k8s.batch.v1.cronjobs.update"]
+            and auth.get("granted") is True
+        ):
+            return True
+    return False
+
+def title(event):
+    actor = deep_get(
+        event, "protoPayload", "authenticationInfo", "principalEmail", default="<ACTOR_NOT_FOUND>"
+    )
+    operation = deep_get(event, "protoPayload", "methodName", default="<OPERATION_NOT_FOUND>")
+    project_id = deep_get(event, "resource", "labels", "project_id", default="<PROJECT_NOT_FOUND>")
+
+    return f"[GCP]: [{actor}] performed [{operation}] on project [{project_id}]"
+
+
+def alert_context(event):
+    return gcp_alert_context(event)
---
+++ rules/gcp_k8s_rules/gcp_k8s_pod_attached_to_node_host_network.py
@@ -0,0 +1,37 @@
+from gcp_base_helpers import gcp_alert_context
+from panther_base_helpers import deep_get, deep_walk
+
+
+def rule(event):
+    if deep_get(event, "protoPayload", "methodName") not in (
+        "io.k8s.core.v1.pods.create",
+        "io.k8s.core.v1.pods.update",
+        "io.k8s.core.v1.pods.patch"
+    ):
+        return False
+
+    host_network = deep_walk(
+        event, "protoPayload", "request", "spec", "hostNetwork"
+    )
+    if host_network is not True:
+        return False
+
+    return True
+
+
+
+def title(event):
+    actor = deep_get(
+        event, "protoPayload", "authenticationInfo", "principalEmail", default="<ACTOR_NOT_FOUND>"
+    )
+    pod_name = deep_get(event, "protoPayload", "resourceName", default="<RESOURCE_NOT_FOUND>")
+    project_id = deep_get(event, "resource", "labels", "project_id", default="<PROJECT_NOT_FOUND>")
+
+    return (
+        f"[GCP]: [{actor}] created or modified pod which is attached to the host's network "
+        f"in project [{project_id}]"
+    )
+
+
+def alert_context(event):
+    return gcp_alert_context(event)
