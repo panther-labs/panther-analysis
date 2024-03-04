9178ef7c9f6a3d0a7adb88943951d6dd20221168
akozlovets098 <an.kozlovets@gmail.com>
2024-03-04 11:15:45 +0200

    GCP rule consistency check

--- rules/gcp_k8s_rules/gcp_k8s_ioc_activity.py
+++ rules/gcp_k8s_rules/gcp_k8s_ioc_activity.py
@@ -1,3 +1,4 @@
+from gcp_base_helpers import gcp_alert_context
 from panther_base_helpers import deep_get
 
 
@@ -7,3 +8,19 @@ def rule(event):
     ):
         return True
     return False
+
+
+def title(event):
+    actor = deep_get(
+        event, "protoPayload", "authenticationInfo", "principalEmail", default="<ACTOR_NOT_FOUND>"
+    )
+    operation = deep_get(event, "protoPayload", "methodName", default="<OPERATION_NOT_FOUND>")
+    project_id = deep_get(event, "resource", "labels", "project_id", default="<PROJECT_NOT_FOUND>")
+
+    return f"[GCP]: [{actor}] performed [{operation}] on project [{project_id}]"
+
+
+def alert_context(event):
+    context = gcp_alert_context(event)
+    context["tor_exit_nodes"] = deep_get(event, "p_enrichment", "tor_exit_nodes")
+    return context
--- rules/gcp_k8s_rules/gcp_k8s_pod_using_host_pid_namespace.py
+++ rules/gcp_k8s_rules/gcp_k8s_pod_using_host_pid_namespace.py
@@ -1,3 +1,4 @@
+from gcp_base_helpers import gcp_alert_context
 from panther_base_helpers import deep_get
 
 METHODS_TO_CHECK = [
@@ -14,3 +15,15 @@ def rule(event):
     if (request_host_pid is True or response_host_pid is True) and method in METHODS_TO_CHECK:
         return True
     return False
+
+def title(event):
+    actor = deep_get(
+        event, "protoPayload", "authenticationInfo", "principalEmail", default="<ACTOR_NOT_FOUND>"
+    )
+    project_id = deep_get(event, "resource", "labels", "project_id", default="<PROJECT_NOT_FOUND>")
+
+    return f"[GCP]: [{actor}] created or modified pod using the host PID namespace in project [{project_id}]"
+
+
+def alert_context(event):
+    return gcp_alert_context(event)
