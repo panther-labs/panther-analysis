8e5b3d2130f90dbbe3f87399e9ea1b6734940cbc
akozlovets098 <95437895+akozlovets098@users.noreply.github.com>
2024-03-04 18:57:42 +0200

    GCP rule consistency check (#1133)
    * GCP rule consistency check
    
    * GCP rule consistency check - linter

--- rules/gcp_audit_rules/gcp_iam_roles_update_privilege_escalation.py
+++ rules/gcp_audit_rules/gcp_iam_roles_update_privilege_escalation.py
@@ -1,4 +1,5 @@
-from panther_base_helpers import deep_walk
+from gcp_base_helpers import gcp_alert_context
+from panther_base_helpers import deep_walk, deep_get
 
 
 def rule(event):
@@ -7,3 +8,17 @@ def rule(event):
         if auth.get("permission") == "iam.roles.update" and auth.get("granted") is True:
             return True
     return False
+
+
+def title(event):
+    actor = deep_get(
+        event, "protoPayload", "authenticationInfo", "principalEmail", default="<ACTOR_NOT_FOUND>"
+    )
+    operation = deep_get(event, "protoPayload", "methodName", default="<OPERATION_NOT_FOUND>")
+    project_id = deep_get(event, "resource", "labels", "project_id", default="<PROJECT_NOT_FOUND>")
+
+    return f"[GCP]: [{actor}] performed [{operation}] on project [{project_id}]"
+
+
+def alert_context(event):
+    return gcp_alert_context(event)
