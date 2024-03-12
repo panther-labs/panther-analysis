afa4dfbc3828842b1f37408562145d83872e66e8
Ariel Ropek <ariel.ropek@panther.com>
2024-03-12 11:40:01 -0600

    base64 rules use python funtions not regex

--- rules/crowdstrike_rules/crowdstrike_base64_encoded_args.py
+++ rules/crowdstrike_rules/crowdstrike_base64_encoded_args.py
@@ -1,6 +1,9 @@
-import re
+from base64 import b64decode
+from binascii import Error as AsciiError
 
 from panther_base_helpers import crowdstrike_process_alert_context
+
+DECODED = ""
 
 
 def rule(event):
@@ -13,37 +16,45 @@ def rule(event):
         "rundll32.exe",
     }
 
-    # Filter by CS event type
-    if event.get("fdr_event_type") != "ProcessRollup2":
+    # Filter by CS event type, Windows platform, and process name
+    if not all(
+        [
+            event.get("fdr_event_type") == "ProcessRollup2",
+            event.get("event_platform") == "Win",
+            event.udm("process_name").lower() in command_line_tools,
+        ]
+    ):
         return False
 
-    # Define a regular expression pattern to match Base64 encoded strings
+    # Split arguments from process path
+    command_line_args = event.udm("cmd")
+    command_line_args = command_line_args.replace('"', "")
+    command_line_args = command_line_args.replace("'", "")
+    command_line_args = command_line_args.split(" ")[1:]
 
-    if event.get("event_platform") == "Win":
-        base64_pattern = re.compile(
-            r"^(\W|)(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?(\W|)$"
-        )
+    # Check if Base64 encoded arguments are present in the command line
+    for arg in command_line_args:
+        try:
+            # Check if the matched string can be decoded back into ASCII
+            global DECODED
+            DECODED = b64decode(arg).decode("ascii")
+            if len(DECODED) > 0:
+                return True
+        except AsciiError:
+            continue
+        except UnicodeDecodeError:
+            continue
 
-        # Normalize the process name to lower case for comparison
-        process_name = event.udm("process_name").lower()
-        # Split process path from arguments
-        command_line_args = event.udm("cmd").split(" ")[1:]
-
-        # Check if the process name matches any of the command line tools
-        # and if Base64 encoded arguments are present in the command line
-        if process_name in command_line_tools:
-            for arg in command_line_args:
-                if base64_pattern.search(arg):
-                    return True
     return False
 
 
 def title(event):
     process_name = event.udm("process_name").lower()
-    command_line = event.udm("cmd").lower()
-
+    command_line = event.udm("cmd")
     return f"Crowdstrike: Execution with base64 encoded args: [{process_name}] - [{command_line}]"
 
 
 def alert_context(event):
-    return crowdstrike_process_alert_context(event)
+    context = crowdstrike_process_alert_context(event)
+    context["decoded arg"] = DECODED
+    return context
--- rules/standard_rules/standard_dns_base64.py
+++ rules/standard_rules/standard_dns_base64.py
@@ -1,26 +1,35 @@
-import re
+from base64 import b64decode
+from binascii import Error as AsciiError
 
 from panther_base_helpers import defang_ioc
 
+DECODED = ""
+
 
 def rule(event):
-    # Return True to match the log event and trigger an alert.
+    args = event.udm("dns_query").split(".")
 
-    # To reduce FPs, don't match against leading subdomains that are only 4 characters in length.
-    # Four base64 characters can only represent 1-3 characters in length.
-    subdomain_len = len(event.udm("dns_query").split(".")[0])
+    for arg in args:
+        try:
+            # Check if the matched string can be decoded back into ASCII
+            global DECODED
+            DECODED = b64decode(arg).decode("ascii")
+            if len(DECODED) > 0:
+                return True
+        except AsciiError:
+            continue
+        except UnicodeDecodeError:
+            continue
 
-    if subdomain_len < 5:
-        return False
-
-    # Search for valid Base64 strings
-    base64_pattern = re.compile(
-        r"^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?\."
-    )
-
-    return bool(base64_pattern.search(event.udm("dns_query")))
+    return False
 
 
 def title(event):
     defang_query = defang_ioc(event.udm("dns_query"))
     return f'Base64 encoded query detected from [{event.udm("source_ip")}], [{defang_query}]'
+
+
+def alert_context(event):
+    context = {}
+    context["decoded url part"] = DECODED
+    return context
