5e6f01edb2ecb6524dbd9b243ffb156fa38b52fe
Oleh Melenevskyi <oleh.melenevskyi@ext.runpanther.io>
2024-03-20 08:54:59 +0200

    Add detection for multiple emails for gitlab password reset

---
+++ rules/gitlab_rules/gitlab_audit_password_reset_multiple_emails.py
@@ -0,0 +1,23 @@
+import json
+
+
+def rule(event):
+    custom_message = event.deep_get("detail", "custom_message", default="")
+    emails_raw = event.deep_get("detail", "target_details", default="")
+
+    if custom_message != "Ask for password reset":
+        return False
+
+    try:
+        emails = json.loads(emails_raw)
+    except json.decoder.JSONDecodeError:
+        return False
+
+    if len(emails) > 1:
+        return True
+    return False
+
+
+def title(event):
+    emails = event.deep_get("detail", "target_details", default="")
+    return f"Someone tried to reset your password with multiple emails :{emails}"
