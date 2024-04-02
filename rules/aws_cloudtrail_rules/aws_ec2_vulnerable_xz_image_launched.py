c16e03fd637db75e206faf3a3453793f98d6abb1
egibs <keybase@egibs.xyz>
2024-04-02 12:21:33 -0500

    Add CloudTrail Rule to detect vulnerable EC2 AMIs re: CVE-2024-3094

---
+++ rules/aws_cloudtrail_rules/aws_ec2_vulnerable_xz_image_launched.py
@@ -0,0 +1,51 @@
+from panther_aws_ami import XZ_AMI_LIST
+from panther_base_helpers import aws_rule_context
+from panther_default import aws_cloudtrail_success
+
+# AMIs published by Fedora between 2024-03-26 and 2024-04-02
+# OpenSUSE and Kali do not have any recent [public] AMIs that would be affected
+
+
+def rule(event):
+    if not aws_cloudtrail_success(event) or event.get("eventName") != "RunInstances":
+        return False
+
+    amis_launched = event.deep_walk(
+        "responseElements",
+        "instancesSet",
+        "items",
+        "imageId",
+        default="<AMI ID not found>",
+        return_val="all",
+    )
+    # convert to a list if only one item is returned
+    if not isinstance(amis_launched, list):
+        amis_launched = [amis_launched]
+    if any(ami in XZ_AMI_LIST for ami in amis_launched):
+        return True
+
+    return False
+
+
+def title(event):
+    amis_launched = event.deep_walk(
+        "responseElements",
+        "instancesSet",
+        "items",
+        "imageId",
+        default="<AMI ID not found>",
+        return_val="all",
+    )
+    instance_ids = event.deep_walk(
+        "responseElements",
+        "instancesSet",
+        "items",
+        "instanceId",
+        default="<Instance ID not found>",
+        return_val="all",
+    )
+    return f"Instance {instance_ids} launched with vulnerable AMI: {amis_launched}"
+
+
+def alert_context(event):
+    return aws_rule_context(event)
