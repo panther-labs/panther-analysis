557cc0d287990c864b15d224044c2d44e44ed31d
Ariel Ropek <ariel.ropek@panther.com>
2024-04-02 13:49:49 -0600

    reference default packs instead of custom query

--- rules/osquery_rules/osquery_linux_mac_vulnerable_xz_liblzma.py
+++ rules/osquery_rules/osquery_linux_mac_vulnerable_xz_liblzma.py
@@ -1,52 +1,26 @@
-# SELECT 'Homebrew Package' AS source, name, version,
-#   CASE
-#     WHEN version LIKE '5.6.0%' OR version LIKE '5.6.1%' THEN 'Potentially Vulnerable'
-#     ELSE 'Most likely not vulnerable'
-#   END AS status
-# FROM homebrew_packages
-# WHERE name = 'xz' OR name = 'liblzma';
-
-# SELECT 'DEB Package' AS source, name, version,
-#   CASE
-#     WHEN version LIKE '5.6.0%' OR version LIKE '5.6.1%' THEN 'Potentially Vulnerable'
-#     ELSE 'Most likely not vulnerable'
-#   END AS status
-# FROM deb_packages
-# WHERE name = 'xz-utils' OR name = 'xz-libs' OR name = 'liblzma' OR name LIKE 'liblzma%'
-# UNION
-# SELECT 'RPM Package' AS source, name, version,
-#   CASE
-#     WHEN version LIKE '5.6.0%' OR version LIKE '5.6.1%' THEN 'Potentially Vulnerable'
-#     ELSE 'Most likely not vulnerable'
-#   END AS status
-# FROM rpm_packages
-# WHERE name = 'xz-utils' OR name = 'xz-libs' OR name = 'liblzma' OR name LIKE 'liblzma%';
-
+QUERY_NAMES = {
+    "pack_vuln-management_homebrew_packages",
+    "pack_vuln-management_deb_packages",
+    "pack_vuln-management_rpm_packages",
+}
 VULNERABLE_PACKAGES = {"xz", "liblzma", "xz-libs", "xz-utils"}
 VULNERABLE_VERSIONS = {"5.6.0", "5.6.1"}
 
 
 def rule(event):
-    if event.get("action") != "added":
-        return False
-    name = event.deep_get("columns", "name", default="")
+    package = event.deep_get("columns", "name", default="")
     version = event.deep_get("columns", "version", default="")
-    status = event.deep_get("columns", "status", default="")
-
-    if name not in VULNERABLE_PACKAGES and not name.startswith("liblzma"):
-        return False
-    if (
-        any(version.startswith(v) for v in VULNERABLE_VERSIONS)
-        and status == "Potentially vulnerable"
-    ):
-        return True
-    return False
+    return all(
+        [
+            event.get("name") in QUERY_NAMES,
+            (package in VULNERABLE_PACKAGES or package.startswith("liblzma")),
+            any(version.startswith(v) for v in VULNERABLE_VERSIONS),
+        ]
+    )
 
 
 def title(event):
     host = event.get("hostIdentifier")
     name = event.deep_get("columns", "name", default="")
     version = event.deep_get("columns", "version", default="")
-    status = event.deep_get("columns", "status", default="")
-    alert_title = f"[CVE-2024-3094] {name} {version} {status} on {host}"
-    return alert_title
+    return f"[CVE-2024-3094] {name} {version} Potentially vulnerable on {host}"
