a56d1824fbe04ad29e40bb427f08d5163bcde78d
jzandona <jason.zandona@runpanther.io>
2024-04-17 08:42:52 -0700

    linting

--- rules/appomni_rules/appomni_alert_passthrough.py
+++ rules/appomni_rules/appomni_alert_passthrough.py
@@ -1,17 +1,19 @@
 from panther_base_helpers import deep_get
 
+
 def rule(event):
     # Only alert where event.kind == "alert"
     if deep_get(event, "event", "kind") == "alert":
         return True
     return False
 
-def title(event):    
+
+def title(event):
     # Create title that includes severity and message
     sev_dict = {0: "Critical", 1: "High", 2: "Medium", 3: "Low", 4: "Informational"}
     sev = sev_dict[deep_get(event, "event", "severity")]
-    
-    # Use type service in title if only one field, label as 'Multiple Services' if more than one. 
+
+    # Use type service in title if only one field, label as 'Multiple Services' if more than one.
     if len(deep_get(event, "related", "services", "type")) == 1:
         service = deep_get(event, "related", "services", "type")[0]
     else:
@@ -19,17 +21,18 @@ def title(event):
 
     return f'[{sev}] - {service} - {event.get("message")}'
 
+
 def severity(event):
     # Update Panther alert severity based on severity from AppOmni Alert
     sev = {0: "Critical", 1: "High", 2: "Medium", 3: "Low", 4: "Informational"}
-    return sev[deep_get(event, "event", "severity")] 
+    return sev[deep_get(event, "event", "severity")]
+
 
 def dedup(event):
-    # Use the unique EventID for this alert to make sure we alert each time a new AppOmni alert is logged
+    # Dedup by the events alert id, make sure we alert each time a new AppOmni alert is logged
     return f'Event ID: {deep_get(event, "appomni", "event", "id")}'
 
+
 def alert_context(event):
-    # Return a dictionary with threat and related data to be included in the alert sent to the alert destination
-    return { "threat": deep_get(event, "rule", "threat"),
-             "related": deep_get(event, "related")
-           }
+    # 'Threat' and 'related' data to be included in the alert sent to the alert destination
+    return {"threat": deep_get(event, "rule", "threat"), "related": deep_get(event, "related")}
