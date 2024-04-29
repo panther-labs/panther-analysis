9f33dcc680ea21b5b0d61b775e17ac9ee67c5b51
Ariel Ropek <ariel.ropek@panther.com>
2024-04-29 12:04:33 -0600

    use event.deep_get and remove InlineFilters

--- rules/appomni_rules/appomni_alert_passthrough.py
+++ rules/appomni_rules/appomni_alert_passthrough.py
@@ -1,9 +1,6 @@
-from panther_base_helpers import deep_get
-
-
 def rule(event):
     # Only alert where event.kind == "alert"
-    if deep_get(event, "event", "kind") == "alert":
+    if event.deep_get("event", "kind") == "alert":
         return True
     return False
 
@@ -11,11 +8,11 @@ def rule(event):
 def title(event):
     # Create title that includes severity and message
     sev_dict = {0: "Critical", 1: "High", 2: "Medium", 3: "Low", 4: "Informational"}
-    sev = sev_dict[deep_get(event, "event", "severity")]
+    sev = sev_dict[event.deep_get("event", "severity")]
 
     # Use type service in title if only one field, label as 'Multiple Services' if more than one.
-    if len(deep_get(event, "related", "services", "type")) == 1:
-        service = deep_get(event, "related", "services", "type")[0]
+    if len(event.deep_get("related", "services", "type")) == 1:
+        service = event.deep_get("related", "services", "type")[0]
     else:
         service = "Multiple Services"
 
@@ -25,14 +22,14 @@ def title(event):
 def severity(event):
     # Update Panther alert severity based on severity from AppOmni Alert
     sev = {0: "Critical", 1: "High", 2: "Medium", 3: "Low", 4: "Informational"}
-    return sev[deep_get(event, "event", "severity")]
+    return sev[event.deep_get("event", "severity")]
 
 
 def dedup(event):
     # Dedup by the events alert id, make sure we alert each time a new AppOmni alert is logged
-    return f'Event ID: {deep_get(event, "appomni", "event", "id")}'
+    return f'Event ID: {event.deep_get("appomni", "event", "id")}'
 
 
 def alert_context(event):
     # 'Threat' and 'related' data to be included in the alert sent to the alert destination
-    return {"threat": deep_get(event, "rule", "threat"), "related": deep_get(event, "related")}
+    return {"threat": event.deep_get("rule", "threat"), "related": event.deep_get("related")}
