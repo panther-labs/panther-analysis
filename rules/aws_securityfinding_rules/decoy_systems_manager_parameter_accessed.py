70d93b36b9b806cc5c52acb43f5ff0064b86a927
JPhenglavong <jphenglavong@gmail.com>
2024-06-05 10:07:31 -0700

    AWS Honeypot Detections threat-306
    AWS Security Finding rules on decoy AWS resources:
    https://aws.amazon.com/blogs/security/how-to-detect-suspicious-activity-in-your-aws-account-by-using-private-decoy-resources/

---
+++ rules/aws_securityfinding_rules/decoy_systems_manager_parameter_accessed.py
@@ -0,0 +1,29 @@
+def rule(event):
+    # List of suspicious API events
+    #NOTE: There may be more API events that's not listed
+    suspicious_api_events = ["Decrypt"]
+
+    # Return True if the API value is in the list of suspicious API events
+    if event["GeneratorId"] == "ssm.amazonaws.com":
+        # Extract the API value from the event
+        api_value = event["Action"]["AwsApiCallAction"]["Api"]
+
+        return api_value in suspicious_api_events
+    return False
+
+def title(event):
+    # (Optional) Return a string which will be shown as the alert title.
+    # If no 'dedup' function is defined, the return value of this method will act as deduplication string.
+
+    # NOTE: Not sure if the offending actor Id will always be in the 0th index of Resources
+    # It's possible to just return the Title as a whole string
+    secret = event['Resources'][0]['Id']
+    return f"Suspicious activity detected accessing private decoy Systems Manager parameter {secret}"
+
+# def dedup(event):
+    #  (Optional) Return a string which will be used to deduplicate similar alerts.
+    # return ''
+
+# def alert_context(event):
+    #  (Optional) Return a dictionary with additional data to be included in the alert sent to the SNS/SQS/Webhook destination
+    # return {'key':'value'}
