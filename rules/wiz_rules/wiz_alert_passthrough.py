df4f6a0154223f624d5d60fcc4288c52ee026af3
akozlovets098 <an.kozlovets@gmail.com>
2024-06-05 17:07:11 +0300

    THREAT-315 Wiz Alert passthrough

---
+++ rules/wiz_rules/wiz_alert_passthrough.py
@@ -0,0 +1,28 @@
+def rule(event):
+    return event.get("status") == "OPEN"
+
+
+def title(event):
+    return (
+        f"[Wiz Alert]: {event.deep_get('sourceRule', 'name', default='ALERT_DESCRIPTION_NOT_FOUND')}"
+    )
+
+
+def severity(event):
+    return event.get("severity")
+
+
+def dedup(event):
+    return event.get("id")
+
+
+def alert_context(event):
+    return {
+        "id": event.get("id", "<ID_NOT_FOUND>"),
+        "type": event.get("type", "<TYPE_NOT_FOUND>"),
+        "description": event.deep_get("sourceRule", "controlDescription", default="<DESCRIPTION_NOT_FOUND>"),
+        "resolution_recommendation": event.deep_get("sourceRule", "resolutionRecommendation", default="<RECOMMENDATION_NOT_FOUND>"),
+        "severity": event.get("severity", "<SEVERITY_NOT_FOUND>"),
+        "entity_snapshot": event.get("entitySnapshot", {}),
+    }
+
