8948aed9e9d3572406368f83940eceaf46167031
jphenglavong <johnny.phenglavong@panther.io>
2024-06-06 14:04:15 -0700

    reformatted and linted

--- rules/aws_securityfinding_rules/decoy_secret_accessed.py
+++ rules/aws_securityfinding_rules/decoy_secret_accessed.py
@@ -1,6 +1,6 @@
 def rule(event):
     # List of suspicious API events
-    #NOTE: There may be more API events that's not listed
+    # NOTE: There may be more API events that's not listed
     suspicious_api_events = ["Decrypt", "Encrypt", "GenerateDataKey"]
 
     # Return True if the API value is in the list of suspicious API events
@@ -11,19 +11,21 @@ def rule(event):
         return api_value in suspicious_api_events
     return False
 
+
 def title(event):
     # (Optional) Return a string which will be shown as the alert title.
     # If no 'dedup' function is defined, the return value of this method will act as dedup string.
 
     # NOTE: Not sure if the offending actor Id will always be in the 0th index of Resources
     # It's possible to just return the Title as a whole string
-    secret = event['Resources'][0]['Id']
+    secret = event["Resources"][0]["Id"]
     return f"Suspicious activity detected accessing private decoy secret {secret}"
 
+
 # def dedup(event):
-    #  (Optional) Return a string which will be used to deduplicate similar alerts.
-    # return ''
+#  (Optional) Return a string which will be used to deduplicate similar alerts.
+# return ''
 
 # def alert_context(event):
-    # (Optional) Return a dict with  data to be included in the alert sent to the SNS/SQS/Webhook
-    # return {'key':'value'}
+# (Optional) Return a dict with  data to be included in the alert sent to the SNS/SQS/Webhook
+# return {'key':'value'}
