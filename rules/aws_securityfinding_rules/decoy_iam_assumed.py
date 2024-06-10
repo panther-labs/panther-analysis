7f7b6dd8976bce12bc49b9ab2d2b9de115c5a726
JPhenglavong <johnny.phenglavong@panther.io>
2024-06-10 10:09:37 -0700

    AWS Honeypot Detections threat-306 (#1252)
    * AWS Honeypot Detections threat-306
    
    AWS Security Finding rules on decoy AWS resources:
    https://aws.amazon.com/blogs/security/how-to-detect-suspicious-activity-in-your-aws-account-by-using-private-decoy-resources/
    
    * Update decoy_dynamodb_accessed.py
    
    * Update decoy_iam_assumed.py
    
    * Update decoy_s3_accessed.py
    
    * Update decoy_secret_accessed.py
    
    * Update decoy_systems_manager_parameter_accessed.py
    
    * Update decoy_dynamodb_accessed.py
    
    * Update decoy_iam_assumed.py
    
    * Update decoy_s3_accessed.py
    
    * Update decoy_secret_accessed.py
    
    * Update decoy_systems_manager_parameter_accessed.py
    
    * Update decoy_systems_manager_parameter_accessed.py
    
    * Update decoy_systems_manager_parameter_accessed.py
    
    * Update decoy_secret_accessed.py
    
    * Update decoy_s3_accessed.py
    
    * Update decoy_iam_assumed.py
    
    * Update decoy_dynamodb_accessed.py
    
    * Update decoy_systems_manager_parameter_accessed.py
    
    * reformatted and linted
    
    * removed unused methods
    
    * fixed trailing lines
    
    * add decoy rules as a pack
    
    ---------
    
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>

---
+++ rules/aws_securityfinding_rules/decoy_iam_assumed.py
@@ -0,0 +1,22 @@
+def rule(event):
+    # List of suspicious API events
+    # NOTE: There may be more API events that's not listed
+    suspicious_api_events = ["AssumeRole"]
+
+    # Return True if the API value is in the list of suspicious API events
+    if event["GeneratorId"] == "sts.amazonaws.com":
+        # Extract the API value from the event
+        api_value = event["Action"]["AwsApiCallAction"]["Api"]
+
+        return api_value in suspicious_api_events
+    return False
+
+
+def title(event):
+    # (Optional) Return a string which will be shown as the alert title.
+    # If no 'dedup' function is defined, the return value of this method will act as dedup string.
+
+    # NOTE: Not sure if the offending actor Id will always be in the 0th index of Resources
+    # It's possible to just return the Title as a whole string
+    secret = event["Resources"][0]["Id"]
+    return f"Suspicious activity detected accessing private decoy IAM role {secret}"
