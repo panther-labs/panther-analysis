f715e863924da678a0b6973e87f1dff5c9907b7a
akozlovets098 <95437895+akozlovets098@users.noreply.github.com>
2024-06-10 18:20:51 +0300

    OCSF data model, VPC/DNS (#1214)
    * Deprecate GreyNoise detections (#1205)
    
    * Deprecate GreyNoise detections
    
    * Update rules/aws_cloudtrail_rules/aws_s3_activity_greynoise.yml
    
    * Update rules/cloudflare_rules/cloudflare_firewall_suspicious_event_greynoise.yml
    
    * Update cloudflare_httpreq_bot_high_volume_greynoise.yml
    
    ---------
    
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
    
    * fix - Notion Login From New Location - NoneType error (#1206)
    
    * fix - Notion Login From New Location - NoneType error
    
    * fix - Notion Login From New Location - NoneType error - linter fix
    
    * remove codeowners (#1208)
    
    * fix - GCP rules - AttributeError (#1210)
    
    * fix - GCP rules - AttributeError
    
    * fix - GCP rules - AttributeError - linter fix
    
    * MITRE ATT&CK Mappings for MS Rules (#1209)
    
    * added MITRE mappings for microsoft rules
    
    * fixed formatting on some helper files
    
    ---------
    
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
    
    * traildiscover enrichment with managed schema (#1177)
    
    * traildiscover enrichment with managed schema
    
    * Add npm install in dockerfile (#1172)
    
    * add npm install in dockerfile
    
    * Remove Python optimizations; add prettier to PATH
    
    ---------
    
    Co-authored-by: egibs <keybase@egibs.xyz>
    
    * schema name: TrailDiscover.CloudTrail
    
    * Fix Dockerfile; add Workflow to test image
    
    * updated data set
    
    * Add MongoDB.2FA.Disabled rule (#1190)
    
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
    
    * lint and fmt
    
    * fmt
    
    * add OCSF selector
    
    * additional OCSF mappings
    
    * Fix Pipfile
    
    * Rebase changes
    
    ---------
    
    Co-authored-by: Panos Sakkos <panos.sakkos@panther.com>
    Co-authored-by: egibs <keybase@egibs.xyz>
    Co-authored-by: Oleh Melenevskyi <767472+melenevskyi@users.noreply.github.com>
    
    * Update PAT to 0.46.0 (#1216)
    
    * THREAT-278 OCSF data model, VPC
    
    ---------
    
    Co-authored-by: Oleh Melenevskyi <767472+melenevskyi@users.noreply.github.com>
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
    Co-authored-by: Panos Sakkos <panos.sakkos@panther.com>
    Co-authored-by: ben-githubs <38414634+ben-githubs@users.noreply.github.com>
    Co-authored-by: egibs <keybase@egibs.xyz>
    Co-authored-by: Evan Gibler <evan.gibler@panther.com>

--- rules/aws_vpc_flow_rules/aws_vpc_unapproved_outbound_dns.py
+++ rules/aws_vpc_flow_rules/aws_vpc_unapproved_outbound_dns.py
@@ -13,17 +13,21 @@ def rule(event):
     # Common DNS ports, for better security use an application layer aware network monitor
     #
     # Defaults to True (no alert) if 'dstport' key is not present
-    if event.get("dstport") != 53 and event.get("dstport") != 5353:
+    if event.udm("destination_port") != 53 and event.udm("destination_port") != 5353:
         return False
 
     # Only monitor traffic that is originating internally
     #
     # Defaults to True (no alert) if 'srcaddr' key is not present
-    if ip_network(event.get("srcaddr", "0.0.0.0/32")).is_global:
+    source_ip = event.udm("source_ip") or "0.0.0.0/32"
+    if ip_network(source_ip).is_global:
         return False
 
     # No clean way to default to False (no alert), so explicitly check for key
-    return "dstaddr" in event and event.get("dstaddr") not in APPROVED_DNS_SERVERS
+    return (
+        bool(event.udm("destination_ip"))
+        and event.udm("destination_ip") not in APPROVED_DNS_SERVERS
+    )
 
 
 def alert_context(event):
