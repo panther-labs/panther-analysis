71a72cdde2566283157f931e206556dfad59d45d
akozlovets098 <an.kozlovets@gmail.com>
2024-06-25 14:31:08 +0300

    THREAT-315 Wiz Alert passthrough - updated according to comments

--- rules/wiz_rules/wiz_alert_passthrough.py
+++ rules/wiz_rules/wiz_alert_passthrough.py
@@ -1,3 +1,6 @@
+from panther_base_helpers import deep_get
+
+
 def rule(event):
     return event.get("status") == "OPEN"
 
@@ -17,16 +20,25 @@ def dedup(event):
     return event.get("id")
 
 
+def description(event):
+    return event.deep_get("sourceRule", "controlDescription", default="<DESCRIPTION_NOT_FOUND>")
+
+
+def runbook(event):
+    return event.deep_get(
+        "sourceRule", "resolutionRecommendation", default="<RECOMMENDATION_NOT_FOUND>"
+    )
+
+
 def alert_context(event):
+    security_subcategories = event.deep_get("sourceRule", "securitySubCategories", default=[{}])
     return {
         "id": event.get("id", "<ID_NOT_FOUND>"),
         "type": event.get("type", "<TYPE_NOT_FOUND>"),
-        "description": event.deep_get(
-            "sourceRule", "controlDescription", default="<DESCRIPTION_NOT_FOUND>"
-        ),
-        "resolution_recommendation": event.deep_get(
-            "sourceRule", "resolutionRecommendation", default="<RECOMMENDATION_NOT_FOUND>"
-        ),
-        "severity": event.get("severity", "<SEVERITY_NOT_FOUND>"),
         "entity_snapshot": event.get("entitySnapshot", {}),
+        "mitre_attack_categories": [
+            subcategory
+            for subcategory in security_subcategories
+            if deep_get(subcategory, "category", "framework", "name") == "MITRE ATT&CK Matrix"
+        ],
     }
