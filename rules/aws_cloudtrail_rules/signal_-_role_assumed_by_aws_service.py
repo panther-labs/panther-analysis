d931a30a3fd46bb13433e5915584eeb09b9216cd
Ariel Ropek <ariel.ropek@panther.com>
2024-07-03 16:20:36 -0600

    aws potentially compromised service role

---
+++ rules/aws_cloudtrail_rules/signal_-_role_assumed_by_aws_service.py
@@ -0,0 +1,10 @@
+def rule(event):
+    # Return True to match the log event and trigger an alert.
+    aws_service = event.deep_get("userIdentity", "type") == "AWSService"
+    return all(
+        [
+            event.get("eventName") == "AssumeRole",
+            event.deep_get("requestParameters", "roleArn"),
+            aws_service
+        ]
+    )
