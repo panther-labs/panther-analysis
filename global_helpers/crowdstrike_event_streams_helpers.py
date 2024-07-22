f5b63ff287713e142773a547edc7337b2e34960d
Ben Airey <benjaminjohnairey@gmail.com>
2024-07-22 16:58:10 -0500

    abstract "turn audit keys to dictionary" away from "alert context"

--- global_helpers/crowdstrike_event_streams_helpers.py
+++ global_helpers/crowdstrike_event_streams_helpers.py
@@ -2,6 +2,9 @@ from panther_base_helpers import key_value_list_to_dict
 
 
 def cs_alert_context(event):
+    return audit_keys_dict(event)
+
+def audit_keys_dict(event):
     return key_value_list_to_dict(
         event.deep_get("event", "AuditKeyValues", default=[]), "Key", "ValueString"
     )
