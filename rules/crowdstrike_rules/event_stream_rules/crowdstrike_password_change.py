b2eb2afc403d8e168512fa9451147b2683897e7e
Ben Airey <benjaminjohnairey@gmail.com>
2024-08-13 13:57:30 -0500

    edit: update Crowdstrike.UserPasswordChange with dynamic severity

--- rules/crowdstrike_rules/event_stream_rules/crowdstrike_password_change.py
+++ rules/crowdstrike_rules/event_stream_rules/crowdstrike_password_change.py
@@ -24,5 +24,20 @@ def title(event):
     return f"[{actor}] changed the password of [{target}]"
 
 
+def severity(event):
+    # Downgrade sev if password changed by same uer
+
+    audit_keys = key_value_list_to_dict(
+        event.deep_get("event", "AuditKeyValues"), "Key", "ValueString"
+    )
+    target = audit_keys.get("target_name", "UNKNOWN USER")
+    actor = event.deep_get("event", "UserId")
+
+    if target == actor:
+        return "INFO"
+
+    return "DEFAULT"
+
+
 def alert_context(event):
     return cs_alert_context(event)
