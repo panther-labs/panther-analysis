3ed786f5afd7610d4129d0cae30febd825401348
Ben Airey <benjaminjohnairey@gmail.com>
2024-08-19 11:31:24 -0500

    add: panther_country_helpers Added a new helper (panther_country_helpers) to contain code relating to getting contextual information from country codes. Moved the existing functions out out panther_base_helpers and expanded on them. Also refactored the rogue state sign-in rule to use the new helper.

--- rules/standard_rules/sign_in_from_rogue_state.py
+++ rules/standard_rules/sign_in_from_rogue_state.py
@@ -1,37 +1,34 @@
+import panther_country_helpers as countries
 import panther_event_type_helpers as event_type
-from panther_base_helpers import expand_country_code
 
 # Configuration Required:
 #   Configure the below list of rogue states according to your needs/experience
 #   Refer to the link below to find the alpha-2 code corresponding to your country
 #   https://www.iban.com/country-codes
-ROGUE_STATES = {
-    "CN",
-    "IR",
-    "RU"
-}
+ROGUE_STATES = {"CN", "IR", "RU"}
+
 
 def rule(event):
     # Only evaluate successful logins
     if event.udm("event_type") != event_type.SUCCESSFUL_LOGIN:
         return False
-    
+
     # Get contry of request origin and compare to identified rogue state list
-    return bool(is_rogue_state(get_country(event)))
+    return bool(is_rogue_state(get_country(event).alpha2))
 
 
 def title(event):
     log_type = event.get("p_log_type")
     country = get_country(event)
     account_name = get_account_name(event)
-    return f"{log_type}: Sign-In for account {account_name} from Rogue State '{expand_country_code(country)}'"
+    return f"{log_type}: Sign-In for account {account_name} from Rogue State '{country.name}'"
 
 
 def alert_context(event):
     return {
         "source_ip": event.udm("source_ip"),
-        "country": get_country(event),
-        "account_name": get_account_name(event)
+        "country": get_country(event).name,
+        "account_name": get_account_name(event),
     }
 
 
@@ -39,17 +36,17 @@ def get_country(event) -> str:
     """Returns the country code from an event's IPinfo data."""
     location_data = event.deep_get("p_enrichment", "ipinfo_location", event.udm_path("source_ip"))
     if not location_data:
-        return "" # Ignore event if we have no enrichment to analyze
-    return location_data.get("country").upper()
+        return ""  # Ignore event if we have no enrichment to analyze
+    return countries.get_country(location_data.get("country").upper())
 
 
 def get_account_name(event) -> str:
-    """ Returns the account name. """
+    """Returns the account name."""
     if account_name := event.deep_get("p_udm", "user", "email"):
         return account_name
-    elif account_name := event.deep_get("p_udm", "user", "name"):
+    if account_name := event.deep_get("p_udm", "user", "name"):
         return account_name
-    elif account_name := event.udm("actor_user"):
+    if account_name := event.udm("actor_user"):
         return account_name
     return "UNKNWON ACCOUNT"
 
