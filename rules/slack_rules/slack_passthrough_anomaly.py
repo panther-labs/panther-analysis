9d387125f43247e93720f421a182a2029f96a5aa
Ariel Ropek <ariel.ropek@panther.com>
2024-08-27 08:51:11 -0600

    update slack passthrough anomaly rules

--- rules/slack_rules/slack_passthrough_anomaly.py
+++ rules/slack_rules/slack_passthrough_anomaly.py
@@ -1,6 +1,6 @@
 from panther_base_helpers import slack_alert_context
 
-SERIOUS_ANAOMALIES = {"excessive_malware_uploads", "session_fingerprint", "unexpected_admin_action"}
+ELEVATED_ANOMALIES = {"excessive_malware_uploads", "session_fingerprint", "unexpected_admin_action"}
 
 
 def rule(event):
@@ -10,12 +10,12 @@ def rule(event):
 def severity(event):
     # Return "MEDIUM" for some more serious anomalies
     reasons = event.deep_get("details", "reason", default=[])
-    if set(reasons) & SERIOUS_ANAOMALIES:
+    if set(reasons) & ELEVATED_ANOMALIES:
         return "MEDIUM"
     return "DEFAULT"
 
 
 def alert_context(event):
     context = slack_alert_context(event)
-    context.update({"details": event.get("details", {}), "context": event.get("context", {})})
+    context |= {"details": event.get("details", {}), "context": event.get("context", {})}
     return context
