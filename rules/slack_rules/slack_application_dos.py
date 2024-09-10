c7fcece699ea93dc3c5587982cea43197780a278
akozlovets098 <an.kozlovets@gmail.com>
2024-09-10 13:06:35 +0300

    Slack.AuditLogs.ApplicationDoS -> threshold rule

--- rules/slack_rules/slack_application_dos.py
+++ rules/slack_rules/slack_application_dos.py
@@ -1,8 +1,4 @@
-from datetime import timedelta
-from json import dumps
-
 from panther_base_helpers import deep_get, slack_alert_context
-from panther_detection_helpers.caching import get_string_set, put_string_set
 
 DENIAL_OF_SERVICE_ACTIONS = [
     "bulk_session_reset_by_admin",
@@ -16,40 +12,12 @@ def rule(event):
     if event.get("action") not in DENIAL_OF_SERVICE_ACTIONS:
         return False
 
-    # Generate a unique cache key for each user
-    user_key = gen_key(event)
-
-    # Retrieve prior entries from the cache, if any
-    last_reset = get_string_set(user_key)
-
-    # Store the reset info for future use
-    store_reset_info(user_key, event)
-
-    # If this is the first reset for the user, don't alert
-    if not last_reset:
-        return False
-
     return True
+
+
+def dedup(event):
+    return f"Slack.AuditLogs.ApplicationDoS{deep_get(event, 'entity', 'user', 'name')}"
 
 
 def alert_context(event):
     return slack_alert_context(event)
-
-
-def gen_key(event):
-    return f"Slack.AuditLogs.ApplicationDoS{deep_get(event, 'entity', 'user', 'name')}"
-
-
-def store_reset_info(key, event):
-    # Map the user to the most recent reset
-    put_string_set(
-        key,
-        [
-            dumps(
-                {
-                    "time": event.get("p_event_time"),
-                }
-            )
-        ],
-        epoch_seconds=event.event_time_epoch() + timedelta(days=1).total_seconds(),
-    )
