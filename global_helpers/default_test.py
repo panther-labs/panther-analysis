8099f396dde964329e95a74684ee0e48ea08270e
Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
2024-09-25 14:52:35 -0600

    Fix issue 439: AccountId case (#1365)

--- global_helpers/default_test.py
+++ global_helpers/default_test.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(__file__))
 import panther_default as p_d  # pylint: disable=C0413
 
 
-class TestAWSKeyAccountID(unittest.TestCase):
+class TestAWSKeyAccountId(unittest.TestCase):
     def test_aws_key_account_id(self):
         aws_key_id = "ASIAY34FZKBOKMUTVV7A"
         account_id = p_d.aws_key_account_id(aws_key_id)
