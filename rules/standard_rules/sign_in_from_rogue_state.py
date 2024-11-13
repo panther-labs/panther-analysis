3ced396251686b613b59f486e4da95a8f0db34e7
ben-githubs <38414634+ben-githubs@users.noreply.github.com>
2024-11-13 09:44:09 -0600

    THREAT-318: Standard.SignInFromRogueState (#1426)
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>

---
+++ rules/standard_rules/sign_in_from_rogue_state.py
@@ -0,0 +1,62 @@
+import panther_event_type_helpers as event_type
+import pycountry
+
+# Configuration Required:
+#   Configure the below list of rogue states according to your needs/experience
+#   Refer to the link below to find the alpha-2 code corresponding to your country
+#   https://www.iban.com/country-codes
+ROGUE_STATES = {"CN", "IR", "RU"}
+
+
+def rule(event):
+    # Only evaluate successful logins
+    if event.udm("event_type") != event_type.SUCCESSFUL_LOGIN:
+        return False
+
+    # Ignore events with no IP data
+    if not event.udm("source_ip"):
+        return False
+
+    # Get contry of request origin and compare to identified rogue state list
+    return bool(is_rogue_state(get_country(event).alpha_2))
+
+
+def title(event):
+    log_type = event.get("p_log_type")
+    country = get_country(event)
+    account_name = get_account_name(event)
+    return f"{log_type}: Sign-In for account {account_name} from Rogue State '{country.name}'"
+
+
+def alert_context(event):
+    return {
+        "source_ip": event.udm("source_ip"),
+        "country": get_country(event).name,
+        "account_name": get_account_name(event),
+    }
+
+
+def get_country(event) -> str:
+    """Returns the country code from an event's IPinfo data."""
+    location_data = event.deep_get("p_enrichment", "ipinfo_location", event.udm_path("source_ip"))
+    if not location_data:
+        return ""  # Ignore event if we have no enrichment to analyze
+    return pycountry.countries.get(alpha_2=location_data.get("country").upper())
+
+
+def get_account_name(event) -> str:
+    """Returns the account name."""
+    if account_name := event.deep_get("p_udm", "user", "email"):
+        return account_name
+    if account_name := event.deep_get("p_udm", "user", "name"):
+        return account_name
+    if account_name := event.udm("actor_user"):
+        return account_name
+    return "UNKNWON ACCOUNT"
+
+
+def is_rogue_state(country_code: str) -> bool:
+    """Returns whether the country code provided belongs to an identified rogue state."""
+    # This function makes it easy for us to use unit test mocks to ensure altering the ROGUE_STATES
+    #   dict doesn't break our test suite.
+    return country_code in ROGUE_STATES
