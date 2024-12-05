2187efcc456b7754485375400150edf175534444
Ben Airey <ben.airey@panther.com>
2024-12-05 11:15:47 -0600

    added reference function, type hints, and tags

--- rules/crowdstrike_rules/event_stream_rules/crowdstrike_detection_summary.py
+++ rules/crowdstrike_rules/event_stream_rules/crowdstrike_detection_summary.py
@@ -1,24 +1,25 @@
+from panther_core import PantherEvent
 from panther_crowdstrike_event_streams_helpers import cs_alert_context
 
 
-def rule(event):
+def rule(event: PantherEvent):
     return event.deep_get("metadata", "eventType") == "EppDetectionSummaryEvent"
 
 
-def title(event):
+def title(event: PantherEvent):
     alert_title = event.deep_get("event", "Name", default="New CrowdStrike Detection")
     alert_desc = event.deep_get("event", "Description")
     return f"{alert_title}: {alert_desc}" if alert_desc else alert_title
 
 
-def dedup(event):
+def dedup(event: PantherEvent):
     if alert_id := event.deep_get("event", "CompositeId"):
         return alert_id
     # Else, fall back on title string
     return title(event)
 
 
-def severity(event):
+def severity(event: PantherEvent):
     # First, try returning the severity based on the SeverityName
     sevname = event.deep_get("event", "SeverityName").upper()
     allowed_values = ("INFO", "LOW", "MEDIUM", "HIGH", "CRITICAL")
@@ -34,7 +35,11 @@ def severity(event):
     )
 
 
-def alert_context(event):
+def reference(event: PantherEvent):
+    return event.deep_get("event", "FalconHostLink", default="")
+
+
+def alert_context(event: PantherEvent):
     context = cs_alert_context(event)
     context.update(
         {"FalconLink": event.deep_get("event", "FalconHostLink", default="<NO LINK PROVIDED>")}
