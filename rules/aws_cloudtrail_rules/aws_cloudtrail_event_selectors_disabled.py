2495608dae81d7816eb7d87996cd9e5c7f98ddc0
ben-githubs <38414634+ben-githubs@users.noreply.github.com>
2024-12-06 09:55:10 -0600

    THREAT-420: Stratus AWS Logging Detections (#1437)
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>

---
+++ rules/aws_cloudtrail_rules/aws_cloudtrail_event_selectors_disabled.py
@@ -0,0 +1,35 @@
+from panther_aws_helpers import aws_cloudtrail_success, aws_rule_context, lookup_aws_account_name
+
+# API calls that are indicative of CloudTrail changes
+CLOUDTRAIL_EDIT_SELECTORS = {"PutEventSelectors"}
+
+
+def rule(event):
+    if not (aws_cloudtrail_success(event) and event.get("eventName") in CLOUDTRAIL_EDIT_SELECTORS):
+        return False
+
+    # Check if management events are included for each selector.
+    #    deep_walk only returns a list if there's more than 1 entry in the nested array, so we must
+    #    enforce it to be a list.
+    includes = event.deep_walk("requestParameters", "eventSelectors", "includeManagementEvents")
+    if not isinstance(includes, list):
+        includes = [includes]
+
+    # Return False all the management events are included, else return True and raise alert
+    return not all(includes)
+
+
+def dedup(event):
+    # Merge on the CloudTrail ARN
+    return event.deep_get("requestParameters", "trailName", default="<UNKNOWN_NAME>")
+
+
+def title(event):
+    return (
+        f"Management events have been exluded from CloudTrail [{dedup(event)}] in account "
+        f"[{lookup_aws_account_name(event.get('recipientAccountId'))}]"
+    )
+
+
+def alert_context(event):
+    return aws_rule_context(event)
