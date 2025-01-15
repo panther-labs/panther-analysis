0c1b9c18412f5b211060ece784ab9a4be820518c
Ariel <ariel.ropek@panther.com>
2025-01-15 14:09:06 -0700

    deep_get refactor

--- policies/aws_iam_policies/aws_iam_role_github_actions_trust.py
+++ policies/aws_iam_policies/aws_iam_role_github_actions_trust.py
@@ -1,5 +1,8 @@
+from panther_base_helpers import deep_get
+
+
 def policy(resource):
-    assume_role_policy = resource.get("AssumeRolePolicyDocument", {}).get("Statement", [])
+    assume_role_policy = deep_get(resource, "AssumeRolePolicyDocument", "Statement", default=[])
     is_valid = False
 
     for statement in assume_role_policy:
@@ -8,7 +11,7 @@ def policy(resource):
         ) != "Allow" or "sts:AssumeRoleWithWebIdentity" not in statement.get("Action", []):
             continue
 
-        principal = statement.get("Principal", {}).get("Federated")
+        principal = deep_get(statement, "Principal", "Federated")
         if not principal or principal == "*":
             return False
         if "oidc-provider/token.actions.githubusercontent.com" not in principal:
@@ -16,10 +19,12 @@ def policy(resource):
 
         # Validate the conditions only if the Principal is valid for GitHub Actions
         conditions = statement.get("Condition", {})
-        audience = conditions.get("StringEquals", {}).get("token.actions.githubusercontent.com:aud")
-        subject = conditions.get("StringLike", {}).get(
-            "token.actions.githubusercontent.com:sub", ""
-        ) or conditions.get("StringEquals", {}).get("token.actions.githubusercontent.com:sub", "")
+        audience = deep_get(conditions, "StringEquals", "token.actions.githubusercontent.com:aud")
+        subject = deep_get(
+            conditions, "StringLike", "token.actions.githubusercontent.com:sub", default=""
+        ) or deep_get(
+            conditions, "StringEquals", "token.actions.githubusercontent.com:sub", default=""
+        )
 
         if (
             audience != "sts.amazonaws.com"
