148cb529bdcfdd2bb50ddcd173567c2371847d01
Ben Airey <ben.airey@panther.com>
2025-01-20 10:22:11 -0600

    add rule: AWS.CloudTrail.AttemptToLeaveOrg

---
+++ rules/aws_cloudtrail_rules/aws_cloudtrail_attempt_to_leave_org.py
@@ -0,0 +1,26 @@
+from panther_aws_helpers import aws_cloudtrail_success, aws_rule_context, lookup_aws_account_name
+from panther_core import PantherEvent
+
+
+def rule(event: PantherEvent) -> bool:
+    return event.get("eventName") == "LeaveOrganization"
+
+
+def title(event: PantherEvent) -> str:
+    account_name = lookup_aws_account_name(event.get("recipientAccountId"))
+    actor = event.udm("actor_user")
+    # Return a more informative message if the attempt was unsuccessful
+    if not aws_cloudtrail_success(event):
+        return f"Failed attempt to remove {account_name} from your AWS organization by {actor}"
+    return f"Account {account_name} has been removed from your AWS organization by {actor}"
+
+
+def severity(event: PantherEvent) -> str:
+    # Downgrade to HIGH if attempt is unsuccessful
+    if not aws_cloudtrail_success(event):
+        return "HIGH"
+    return "DEFAULT"
+
+
+def alert_context(event: PantherEvent) -> dict:
+    return aws_rule_context(event)
