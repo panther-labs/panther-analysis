175bd80485b1f769908900623e680981a904392d
akozlovets098 <anastasiia.kozlovets@ext.runpanther.io>
2025-01-20 13:37:00 +0200

    THREAT-423 Stratus AWS Snapshot Sharing Detections

--- rules/aws_cloudtrail_rules/aws_ami_modified_for_public_access.py
+++ rules/aws_cloudtrail_rules/aws_ami_modified_for_public_access.py
@@ -1,4 +1,7 @@
+from panther_detection_helpers.caching import check_account_age
+
 from panther_aws_helpers import aws_cloudtrail_success, aws_rule_context
+
 
 
 def rule(event):
@@ -7,11 +10,13 @@ def rule(event):
         return False
 
     added_perms = event.deep_get(
-        "requestParameters", "launchPermission", "add", "items", default=[]
+        "requestParameters", "launchPermission", "add", "items", default=[{}]
     )
 
     for item in added_perms:
         if item.get("group") == "all":
+            return True
+        if check_account_age(item.get("userId")):  # checking if the account is new
             return True
 
     return False
