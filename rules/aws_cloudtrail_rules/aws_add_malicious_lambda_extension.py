e88e62a8f9adac679db658b7f95534044121d885
akozlovets098 <anastasiia.kozlovets@ext.runpanther.io>
2025-01-22 15:50:25 +0200

    THREAT-425 Stratus AWS Lambda Detections

---
+++ rules/aws_cloudtrail_rules/aws_add_malicious_lambda_extension.py
@@ -0,0 +1,26 @@
+from panther_aws_helpers import aws_cloudtrail_success, aws_rule_context
+
+
+def rule(event):
+    if (
+        aws_cloudtrail_success(event)
+        and event.get("eventSource") == "lambda.amazonaws.com"
+        and event.get("eventName") == "UpdateFunctionConfiguration20150331v2"
+        and event.deep_get("responseElements", "layers")
+    ):
+        return True
+    return False
+
+
+def title(event):
+    lambda_name = event.deep_get(
+        "responseElements", "functionName", default="LAMBDA_NAME_NOT_FOUND"
+    )
+    return (
+        f"[AWS.CloudTrail] User [{event.udm('actor_user', default='USER_NOT_FOUND')}] "
+        f"updated Lambda function configuration with layers for [{lambda_name}] aaa {event.deep_get('responseParameters', 'layers')}"
+    )
+
+
+def alert_context(event):
+    return aws_rule_context(event)
---
+++ rules/aws_cloudtrail_rules/aws_backdoor_lambda_function.py
@@ -0,0 +1,28 @@
+from panther_aws_helpers import aws_cloudtrail_success, aws_rule_context
+
+ADD_PERMISSION_EVENTS = {
+    "AddPermission20150331",
+    "AddPermission20150331v2",
+}
+
+
+def rule(event):
+    return (
+        aws_cloudtrail_success(event)
+        and event.get("eventSource") == "lambda.amazonaws.com"
+        and event.get("eventName") in ADD_PERMISSION_EVENTS
+    )
+
+
+def title(event):
+    lambda_name = event.deep_get(
+        "responseElements", "functionName", default="LAMBDA_NAME_NOT_FOUND"
+    )
+    return (
+        f"[AWS.CloudTrail] User [{event.udm('actor_user', default='USER_NOT_FOUND')}] "
+        f"added permission to Lambda function [{lambda_name}]"
+    )
+
+
+def alert_context(event):
+    return aws_rule_context(event)
