46bd7907cea7fd67fd15e1ab6f5f295f88ca1427
Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
2025-01-30 08:49:14 -0700

    Update rules/aws_cloudtrail_rules/aws_add_malicious_lambda_extension.py

--- rules/aws_cloudtrail_rules/aws_add_malicious_lambda_extension.py
+++ rules/aws_cloudtrail_rules/aws_add_malicious_lambda_extension.py
@@ -17,7 +17,7 @@ def title(event):
         "responseElements", "functionName", default="LAMBDA_NAME_NOT_FOUND"
     )
     return (
-        f"[AWS.CloudTrail] User [{event.udm('actor_user', default='USER_NOT_FOUND')}] "
+        f"[AWS.CloudTrail] User [{event.udm('actor_user')}] "
         f"updated Lambda function configuration with layers for [{lambda_name}]"
     )
 
