fc2a3afb6252f77a76a06864976e290b303617c5
ben-githubs <38414634+ben-githubs@users.noreply.github.com>
2025-01-31 09:31:14 -0600

    Stratus AWS: Misc Detections (#1485)

---
+++ rules/aws_cloudtrail_rules/aws_cloudtrail_ses_check_send_quota.py
@@ -0,0 +1,12 @@
+from panther_aws_helpers import aws_rule_context
+from panther_core import PantherEvent
+
+
+def rule(event: PantherEvent) -> bool:
+    return event.get("eventName") == "GetSendQuota"
+
+
+def alert_context(event: PantherEvent) -> dict:
+    context = aws_rule_context(event)
+    context["accountRegion"] = f"{event.get('recipientAccountId')}_{event.get('eventRegion')}"
+    return context
