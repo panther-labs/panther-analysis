e4062e4d81fce0d3eb84f5592ef945ee20867a34
joongi92 <144375655+joongi92@users.noreply.github.com>
2025-02-10 15:40:28 -0500

    Adding Azure EntraID rules (#1463)
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>
    Co-authored-by: ben-githubs <38414634+ben-githubs@users.noreply.github.com>
    Co-authored-by: Ariel <ariel.ropek@panther.com>

---
+++ rules/azure_signin_rules/azure_policy_changed.py
@@ -0,0 +1,32 @@
+from panther_msft_helpers import azure_rule_context, azure_success
+
+POLICY_OPERATION = "policy"
+
+IGNORE_ACTIONS = ["Add", "Added"]
+
+
+def rule(event):
+    operation = event.get("operationName", default="")
+    if not azure_success(event) or not operation.endswith(POLICY_OPERATION):
+        return False
+    # Ignore added policies
+    if any((operation.startswith(ignore) for ignore in IGNORE_ACTIONS)):
+        return False
+
+    return True
+
+
+def title(event):
+    operation_name = event.get("operationName", default="<UNKNOWN OPERATION>")
+    actor_name = event.deep_get(
+        "properties", "initiatedBy", "user", "userPrincipalName", default="<UNKNOWN ACTOR>"
+    )
+    policy = event.deep_walk(
+        "properties", "targetResources", "displayName", default="<UNKNOWN POLICY>"
+    )
+
+    return f"{operation_name} by {actor_name} on the policy {policy}"
+
+
+def alert_context(event):
+    return azure_rule_context(event)
