29afd155d6fbe10793990e2cb103a1de322df5aa
akozlovets098 <anastasiia.kozlovets@ext.runpanther.io>
2025-03-03 14:35:14 +0200

    THREAT-422 Stratus AWS S3 Detections

---
+++ rules/aws_cloudtrail_rules/aws_s3_suspicious_files_activities.py
@@ -0,0 +1,27 @@
+from panther_aws_helpers import aws_cloudtrail_success, aws_rule_context
+
+SUSPICIOUS_EVENTS = {
+    "DeleteObject",
+    "DeleteObjects",
+    "GetObject",
+    "CopyObject",
+}
+
+
+def rule(event):
+    return (
+        aws_cloudtrail_success(event)
+        and event.get("eventSource") == "s3.amazonaws.com"
+        and event.get("eventName") in SUSPICIOUS_EVENTS
+    )
+
+
+def title(event):
+    return (
+        f"[AWS.CloudTrail] User [{event.udm('actor_user')}] "
+        f"performed [{event.get('eventName')}] on [{event.deep_get('requestParameters', 'bucketName')}] bucket"
+    )
+
+
+def alert_context(event):
+    return aws_rule_context(event)
