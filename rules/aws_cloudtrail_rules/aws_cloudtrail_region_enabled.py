4aadd21bf74ae17fef27d0e2bbd999bf5432ae9a
akozlovets098 <anastasiia.kozlovets@ext.runpanther.io>
2025-03-25 13:53:40 +0200

    THREAT-467 AWS Region Enabled - rule

---
+++ rules/aws_cloudtrail_rules/aws_cloudtrail_region_enabled.py
@@ -0,0 +1,13 @@
+from panther_aws_helpers import aws_rule_context
+
+
+def rule(event):
+    return event.get("eventName") == "EnableRegion"
+
+
+def title(event):
+    return f"AWS CloudTrail region [{event.deep_get('requestParameters', 'RegionName')}] enabled by user [{event.udm('actor_user')}]"
+
+
+def alert_context(event):
+    return aws_rule_context(event)
