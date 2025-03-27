facc8e93ea0f9c119360604dcf228a837a45c10c
akozlovets098 <anastasiia.kozlovets@ext.runpanther.io>
2025-03-27 13:59:02 +0200

    THREAT-477 [Tuning] aws_ec2_multi_instance_connect.py

--- rules/aws_cloudtrail_rules/aws_ec2_multi_instance_connect.py
+++ rules/aws_cloudtrail_rules/aws_ec2_multi_instance_connect.py
@@ -1,9 +1,24 @@
+import json
+
 from panther_aws_helpers import aws_cloudtrail_success, aws_rule_context, lookup_aws_account_name
 from panther_core import PantherEvent
+from panther_detection_helpers.caching import get_string_set, put_string_set
 
 
 def rule(event: PantherEvent) -> bool:
-    return aws_cloudtrail_success(event) and event.get("eventName") == "SendSSHPublicKey"
+    if not (aws_cloudtrail_success(event) and event.get("eventName") == "SendSSHPublicKey"):
+        return False
+
+    key = event.deep_get("requestParameters", "sSHPublicKey")
+    cached_instance_ids = get_cached_instance_ids(key)
+    if len(cached_instance_ids) == 0:
+        return False
+    target_instance_id = event.deep_get("requestParameters", "instanceId")
+    if target_instance_id not in cached_instance_ids:
+        cached_instance_ids.add(target_instance_id)
+        put_string_set(key, cached_instance_ids, epoch_seconds=3600)
+        return True
+    return False
 
 
 def title(event: PantherEvent) -> str:
@@ -23,3 +38,13 @@ def alert_context(event: PantherEvent) -> dict:
         "requestParameters", "instanceId", default="<UNKNOWN EC2 INSTANCE ID>"
     )
     return context
+
+
+def get_cached_instance_ids(key: str) -> set[str]:
+    """Get any previously cached parameter names. Included automatic converstion from string in
+    the case of a unit test mock."""
+    cached_ids = get_string_set(key, force_ttl_check=True)
+    if isinstance(cached_ids, str):
+        # This is a unit test
+        cached_ids = set(json.loads(cached_ids))
+    return cached_ids
