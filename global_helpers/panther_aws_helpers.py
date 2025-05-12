708d37c013da4b3afc1ebb9f9d1fdb117d002d54
pageinsec <36606674+pageinsec@users.noreply.github.com>
2025-05-12 09:20:29 -0500

    Fixup: AWS get_actor_user (#1600)
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>

--- global_helpers/panther_aws_helpers.py
+++ global_helpers/panther_aws_helpers.py
@@ -252,6 +252,11 @@ def get_actor_user(event):
             "userName",
             default=f"Unknown{user_type}",
         )
+        # If SSO assumed role, the session username is generic, return name from principalId
+        if actor_user.startswith("AWSReservedSSO_"):
+            actor_user = (
+                deep_get(event, "userIdentity", "principalId", default=f"Unknown{user_type}")
+            ).split(":")[1]
     elif user_type == "IdentityCenterUser":
         actor_user = deep_get(
             event, "additionalEventData", "UserName", default=f"Unknown{user_type}"
