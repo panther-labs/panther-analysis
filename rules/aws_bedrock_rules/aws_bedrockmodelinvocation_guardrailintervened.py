38e6749186cb3aa16cf5af3b9844a85549ddb74c
Christina Aravantinou <christina.aravantinou@panther.io>
2025-06-24 16:19:44 +0300

    Update aws_bedrockmodelinvocation_guardrailintervened.py

--- rules/aws_bedrock_rules/aws_bedrockmodelinvocation_guardrailintervened.py
+++ rules/aws_bedrock_rules/aws_bedrockmodelinvocation_guardrailintervened.py
@@ -1,30 +1,35 @@
 def rule(event):
-    if (event.get("operation") != "InvokeModel" and event.get("operation") != "Converse"):
+    if event.get("operation") != "InvokeModel" and event.get("operation") != "Converse":
         return False
 
-    stop_reason = event.deep_get("output","outputBodyJSON","stopReason",default="<UNKNOWN REASON>")
-    action_reason = event.deep_get(
-    "output", 
-    "outputBodyJSON", 
-    "amazon-bedrock-trace", 
-    "guardrail", 
-    "actionReason", 
-    default="<UNKNOWN ACTION REASON>"
+    stop_reason = event.deep_get(
+        "output", "outputBodyJSON", "stopReason", default="<UNKNOWN REASON>"
     )
-    return stop_reason =="guardrail_intervened" or action_reason.startswith("Guardrail blocked")
+    action_reason = event.deep_get(
+        "output",
+        "outputBodyJSON",
+        "amazon-bedrock-trace",
+        "guardrail",
+        "actionReason",
+        default="<UNKNOWN ACTION REASON>",
+    )
+    return stop_reason == "guardrail_intervened" or action_reason.startswith("Guardrail blocked")
+
 
 def title(event):
     model_id = event.get("modelId")
     operation_name = event.get("operation")
     account_id = event.get("accountId")
-    stop_reason = event.deep_get("output","outputBodyJSON","stopReason",default="<UNKNOWN REASON>")
+    stop_reason = event.deep_get(
+        "output", "outputBodyJSON", "stopReason", default="<UNKNOWN REASON>"
+    )
     action_reason = event.deep_get(
-    "output",
-    "outputBodyJSON",
-    "amazon-bedrock-trace",
-    "guardrail",
-    "actionReason",
-    default="<UNKNOWN ACTION REASON>"
+        "output",
+        "outputBodyJSON",
+        "amazon-bedrock-trace",
+        "guardrail",
+        "actionReason",
+        default="<UNKNOWN ACTION REASON>",
     )
     if action_reason == "<UNKNOWN ACTION REASON>":
         return (
