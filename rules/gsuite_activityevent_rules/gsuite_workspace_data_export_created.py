bbcbdd0945738a0e38ea6e1a8aff3b6ce2d9f3bc
flip-z <flipz@flipz.rocks>
2025-10-07 16:43:24 -0400

    Add Gsuite AlertContext Helpers (#1732)
    Co-authored-by: Ariel Ropek <79653153+arielkr256@users.noreply.github.com>

--- rules/gsuite_activityevent_rules/gsuite_workspace_data_export_created.py
+++ rules/gsuite_activityevent_rules/gsuite_workspace_data_export_created.py
@@ -1,3 +1,6 @@
+from panther_gsuite_helpers import gsuite_activityevent_alert_context
+
+
 def rule(event):
     return event.get("name", "").startswith("CUSTOMER_TAKEOUT_")
 
@@ -8,3 +11,7 @@ def title(event):
         f"[{event.get('name', '<NO_EVENT_NAME>')}] "
         f"performed by [{event.deep_get('actor', 'email', default='<NO_ACTOR_FOUND>')}]"
     )
+
+
+def alert_context(event):
+    return gsuite_activityevent_alert_context(event)
