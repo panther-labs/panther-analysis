c3ce13587a0905e36e28177744706fd03c4da7bc
ari <141658119+alessandrarizzo@users.noreply.github.com>
2025-10-09 11:20:12 -0400

    Apply suggestions

--- rules/auth0_rules/auth0_push_notification_fatigue.py
+++ rules/auth0_rules/auth0_push_notification_fatigue.py
@@ -1,36 +1,16 @@
-import json
-
 from panther_auth0_helpers import auth0_alert_context, is_auth0_config_event
-from panther_detection_helpers.caching import add_to_string_set
-
-# Determine how many push notifications must be sent before triggering an alert.
-PUSH_FATIGUE_THRESHOLD = 5
-
-RULE_ID = "Auth0.PushNotification.Fatigue"
-WITHIN_TIMEFRAME_MINUTES = 1
 
 
 def rule(event):
 
     data_type = event.deep_get("data", "type", default="<NO_DATA_TYPE_FOUND>")
-    if data_type != "gd_send_pn":
-        return False
-    user = event.deep_get(
-        "data", "details", "request", "auth", "user", "email", default="<NO_USER_FOUND>"
-    )
-    key = f"{RULE_ID}-{user}"
-    unique_alerts = add_to_string_set(key, WITHIN_TIMEFRAME_MINUTES * 60)
 
-    if isinstance(unique_alerts, str):
-        unique_alerts = json.loads(unique_alerts)
-    if len(unique_alerts) >= PUSH_FATIGUE_THRESHOLD:
-        return all(
-            [
-                unique_alerts,
-                is_auth0_config_event(event),
-            ]
-        )
-    return False
+    return all(
+        [
+            data_type == "gd_send_pn",
+            is_auth0_config_event(event),
+        ]
+    )
 
 
 def title(event):
