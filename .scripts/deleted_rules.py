84cb2191697a04f0d6b3f3e9435c1682b28c5b23
Max Richmond <46904505+maxrichie5@users.noreply.github.com>
2025-12-08 12:31:51 -0700

    Updating delete rules script to use latest code (#1834)

--- .scripts/deleted_rules.py
+++ .scripts/deleted_rules.py
@@ -1,28 +1,30 @@
-""" Checks to see if an Analysis item was removed from the repo, and whether it was added to the
-deprecated.txt file. """
+"""Checks to see if an Analysis item was removed from the repo, and whether it was added to the
+deprecated.txt file."""
 
-import argparse
 import logging
-import os
 import re
 import subprocess
+from typing import Optional
 
 import panther_analysis_tool.command.bulk_delete as pat_delete
 import panther_analysis_tool.util as pat_util
+import typer
 
 diff_pattern = re.compile(r'^[+-](?:RuleID|PolicyID|QueryName):\s*"?(.+?)["\n]')
 
 
 def get_deleted_ids() -> set[str]:
     # Run git diff, get output
-    result = subprocess.run(["git", "diff", "origin/develop", "HEAD"], capture_output=True)
+    result = subprocess.run(
+        ["git", "diff", "origin/develop", "HEAD"], capture_output=True
+    )
     if result.stderr:
         raise Exception(result.stderr.decode("utf-8"))
 
     # Track specific IDs that are added and deleted
     added_ids = set()
     deleted_ids = set()
-    
+
     for line in result.stdout.decode("utf-8").split("\n"):
         if m := diff_pattern.match(line):
             id_value = m.group(1)
@@ -41,43 +43,38 @@ def get_deprecated_ids() -> set[str]:
         return set(f.read().split("\n"))
 
 
-def check(_):
+def check():
+    """Check if any items have been removed and not added to deprecated.txt"""
     if ids := get_deleted_ids() - get_deprecated_ids():
         print("❌ The following rule IDs may have been deleted:")
         for id_ in ids:
             print(f"\t{id_}")
-        exit(1)
+        raise typer.Exit(code=1)
     else:
         print("✅ No unaccounted deletions found! You're in the clear! 👍")
 
 
-def remove(args):
-    api_token = args.api_token or os.environ.get("PANTHER_API_TOKEN")
-    api_host = args.api_host or os.environ.get("PANTHER_API_HOST")
-
-    if not (api_token and api_host):
-        opts = []
-        if not api_token:
-            print("No API token was found or provided!")
-            opts.append("--api-token")
-        if not api_host:
-            print("No API host was found or provided!")
-            opts.append("--api-host")
-        print(f"You can pass API credentials using {' and '.join(opts)} in your command.")
-        exit(1)
+def remove(
+    api_token: Optional[str] = typer.Option(
+        None, help="Panther API token", envvar="PANTHER_API_TOKEN"
+    ),
+    api_host: Optional[str] = typer.Option(
+        None, help="Panther API host", envvar="PANTHER_API_HOST"
+    ),
+):
+    """Delete the entries listed in deprecated.txt"""
 
     ids = list(get_deprecated_ids())
-
-    pat_args = argparse.Namespace(
-        analysis_id=ids, query_id=[], confirm_bypass=True, api_token=api_token, api_host=api_host
-    )
 
     logging.basicConfig(
         format="[%(levelname)s][%(name)s]: %(message)s",
         level=logging.INFO,
     )
 
-    return_code, out = pat_util.func_with_api_backend(pat_delete.run)(pat_args)
+    return_code, out = pat_delete.run(
+        backend=pat_util.get_api_backend(api_token, api_host),
+        args=pat_delete.BulkDeleteArgs(analysis_id=ids, query_id=[], confirm=False),
+    )
 
     if return_code == 1:
         if out:
@@ -88,21 +85,10 @@ def remove(args):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(title="subcommands")
-
-    check_help = "Check if any items have been removed and not added to deprecated.txt"
-    parser_check = subparsers.add_parser("check", help=check_help)
-    parser_check.set_defaults(func=check)
-
-    remove_help = "Delete the entires listed in deprecated.txt"
-    parser_remove = subparsers.add_parser("remove", help=remove_help)
-    parser_remove.add_argument("--api-token", type=str, required=False)
-    parser_remove.add_argument("--api-host", type=str, required=False)
-    parser_remove.set_defaults(func=remove)
-
-    args = parser.parse_args()
-    args.func(args)
+    app = typer.Typer()
+    app.command()(check)
+    app.command()(remove)
+    app()
 
 
 if __name__ == "__main__":
