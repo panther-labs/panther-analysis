2cc65439eb7163e0e287b641e4b92e72b1a2add8
zaynahsmith-dasilva <zaynah.smith-dasilva@panther.com>
2026-02-10 10:22:15 -0500

    Improve threat activity types for ProofPoint Passthrough rule (#1940)

--- rules/sentinelone_rules/sentinelone_threats.py
+++ rules/sentinelone_rules/sentinelone_threats.py
@@ -1,8 +1,20 @@
 NEW_THREAT_ACTIVITYTYPES = [
+    # Malicious Threats - Not Mitigated
     19,  # New Malicious Threat Not Mitigated
-    4108,  # New Malicious Threat Not Mitigated
+    4108,  # New Malicious Threat Not Mitigated (detected by SentinelOne Cloud)
+    # Suspicious Threats - Not Mitigated
     4003,  # New Suspicious Threat Not Mitigated
-    4109,  # New Suspicious Threat Not Mitigated
+    4109,  # New Suspicious Threat Not Mitigated (detected by SentinelOne Cloud)
+    # New Threats - Mitigated/Blocked
+    18,  # New Threat Mitigated
+    20,  # New Threat Preemptive Block
+    # Automated Threat Intelligence Detections
+    4106,  # STAR Active Response Marked Event As Malicious
+    4107,  # STAR Active Response Marked Event As Suspicious
+    4110,  # Singularity Threat Intelligence Engine Marked Event As Malicious
+    4111,  # Watchtower Cloud Detection Engine Marked Event As Threat
+    4112,  # Watchtower Cloud Detection Engine Marked Event As Suspicious
+    4113,  # Singularity Threat Intelligence Engine Marked Event As Suspicious
 ]
 
 
@@ -13,13 +25,13 @@ def rule(event):
 def title(event):
     return (
         f"SentinelOne - [{event.deep_get('data', 'confidencelevel', default='')}] level threat "
-        f"[{event.deep_get('data', 'filedisplayname' ,default='NO FILE NAME')}] detected on "
-        f"[{event.deep_get('data', 'computername', default= 'NO COMPUTER NAME')}]."
+        f"[{event.deep_get('data', 'filedisplayname', default='NO FILE NAME')}] detected on "
+        f"[{event.deep_get('data', 'computername', default='NO COMPUTER NAME')}]."
     )
 
 
 def dedup(event):
-    return f"s1threat:{event.get('id','')}"
+    return f"s1threat:{event.get('id', '')}"
 
 
 def severity(event):
