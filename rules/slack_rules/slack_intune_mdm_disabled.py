fe4a75152e664349795151134d6f45b219638d7f
zaynahsmith-dasilva <zaynah.smith-dasilva@panther.com>
2026-02-10 10:24:06 -0500

    Update rules/slack_rules/slack_intune_mdm_disabled.py
    Co-authored-by: ari <141658119+alessandrarizzo@users.noreply.github.com>

--- rules/slack_rules/slack_intune_mdm_disabled.py
+++ rules/slack_rules/slack_intune_mdm_disabled.py
@@ -9,7 +9,7 @@ def title(event):
     actor = event.deep_get("actor", "user", "email", default="<UNKNOWN_ACTOR>")
     workspace = event.deep_get("context", "location", "domain", default="<UNKNOWN_WORKSPACE>")
     return (
-        f"CRITICAL: Microsoft Intune MDM disabled for Slack workspace [{workspace}] "
+        f"Microsoft Intune: MDM disabled for Slack workspace [{workspace}] "
         f"by [{actor}] - Mobile security controls removed"
     )
 
