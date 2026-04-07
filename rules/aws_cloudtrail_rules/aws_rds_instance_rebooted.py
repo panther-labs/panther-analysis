3b1a4790b31b8b23e04dd8501d405a7c6dab1818
ari <141658119+alessandrarizzo@users.noreply.github.com>
2026-04-07 16:26:58 -0400

    fix

--- rules/aws_cloudtrail_rules/aws_rds_instance_rebooted.py
+++ rules/aws_cloudtrail_rules/aws_rds_instance_rebooted.py
@@ -1,4 +1,4 @@
-from panther_aws_helpers import aws_rds_context
+from panther_aws_helpers import aws_rule_context
 
 
 def rule(event):
@@ -29,6 +29,11 @@ def title(event):
 
 
 def alert_context(event):
-    context = aws_rds_context(event)
+    context = aws_rule_context(event)
+    context["resource_identifier"] = (
+        event.deep_get("requestParameters", "dBInstanceIdentifier")
+        or event.deep_get("requestParameters", "dBClusterIdentifier")
+        or event.deep_get("requestParameters", "dBShardGroupIdentifier", default="N/A")
+    )
     context["force_failover"] = event.deep_get("requestParameters", "forceFailover", default="N/A")
     return context
