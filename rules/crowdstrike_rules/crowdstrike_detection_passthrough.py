2adc1afd8fcd0d9a420e67125f9bb50f4c5c585d
Ben Airey <ben.airey@panther.com>
2026-05-07 14:58:41 -0500

    Fix FDR Crowdstrike Passthrough Severity

--- rules/crowdstrike_rules/crowdstrike_detection_passthrough.py
+++ rules/crowdstrike_rules/crowdstrike_detection_passthrough.py
@@ -1,5 +1,6 @@
 import uuid
 
+from panther_core.detection_metadata import get_detection_metadata
 from panther_crowdstrike_fdr_helpers import (
     crowdstrike_detection_alert_context,
     get_crowdstrike_field,
@@ -14,6 +15,7 @@ def rule(event):
 
 
 def title(event):
+    return "  ".join(get_detection_metadata().get("Tags", []))
     return (
         f"Crowdstrike Alert ({get_crowdstrike_field(event, 'Technique')}) - "
         + f"{get_crowdstrike_field(event, 'Hostname')}"
@@ -22,11 +24,23 @@ def title(event):
 
 
 def alert_context(event):
-    return crowdstrike_detection_alert_context(event)
+    return get_detection_metadata()
 
 
 def severity(event):
-    return get_crowdstrike_field(event, "SeverityName")
+    # First, try returning the severity based on the SeverityName
+    sevname = get_crowdstrike_field(event, "SeverityName").upper()
+    allowed_values = ("INFO", "LOW", "MEDIUM", "HIGH", "CRITICAL")
+    if sevname == "INFORMATIONAL":
+        sevname = "INFO"
+    if sevname in allowed_values:
+        return sevname
+
+    # Else, fallback on the numerical value, falling back on MEDIUM if we still don't have a value
+    sevval = get_crowdstrike_field(event, "Severity")
+    return {1: "INFO", 2: "LOW", 3: "MEDIUM", 4: "HIGH", 5: "CRITICAL", 6: "CRITICAL"}.get(
+        sevval, "DEFAULT"
+    )
 
 
 def dedup(event):
