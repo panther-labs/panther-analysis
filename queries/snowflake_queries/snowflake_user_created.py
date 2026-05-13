c5e6a8bc20875e3cbcfcfb181641fe946c3f6b2e
zaynahsmith-dasilva <zaynah.smith-dasilva@panther.com>
2026-05-13 16:50:36 -0400

    fix

--- queries/snowflake_queries/snowflake_user_created.py
+++ queries/snowflake_queries/snowflake_user_created.py
@@ -1,8 +1,6 @@
 import re
 
-_CREATE_USER_RE = re.compile(
-    r"CREATE\s+USER\s+(?:IF\s+NOT\s+EXISTS\s+)?(\S+)", re.IGNORECASE
-)
+_CREATE_USER_RE = re.compile(r"CREATE\s+USER\s+(?:IF\s+NOT\s+EXISTS\s+)?(\S+)", re.IGNORECASE)
 
 
 def rule(_):
@@ -13,6 +11,5 @@ def title(event):
     match = _CREATE_USER_RE.search(event.get("query_text", ""))
     username = match.group(1) if match else "<UNKNOWN_USER>"
     return (
-        f"Snowflake user [{username}] created by "
-        f"[{event.get('user_name', '<UNKNOWN_ADMIN>')}]"
+        f"Snowflake user [{username}] created by " f"[{event.get('user_name', '<UNKNOWN_ADMIN>')}]"
     )
