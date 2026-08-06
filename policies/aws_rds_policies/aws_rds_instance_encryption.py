006e5815927a758d4ce84475ccbbd4854619707c
zaynahsmith-dasilva <zaynah.smith-dasilva@panther.com>
2026-08-06 11:11:34 -0400

    fix

--- policies/aws_rds_policies/aws_rds_instance_encryption.py
+++ policies/aws_rds_policies/aws_rds_instance_encryption.py
@@ -1,2 +1,9 @@
 def policy(resource):
-    return resource["KmsKeyId"] is not None
+    # Aurora manages storage encryption at the cluster level. Since Feb 2024, AWS encrypts
+    # all new Aurora clusters by default with an AWS owned KMS key, which never populates
+    # KmsKeyId (or StorageEncrypted) on the instance resource, causing false positives here.
+    # Ref: https://aws.amazon.com/blogs/database/use-default-encryption-at-rest-for-new-amazon-aurora-clusters/  # pylint: disable=line-too-long
+    if resource.get("StorageType") == "aurora":
+        return True
+
+    return resource.get("KmsKeyId") is not None
